de1305a07c03ef984132ab1350f2249a1922452a
Jon Corbet <corbet@eol.ucar.edu>
1990-03-02 16:11:07 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/rdsslibs@262 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ ui/ui_window.h
@@ -0,0 +1,13 @@
+/* $Id: ui_window.h,v 1.1 1990-03-02 16:11:07 corbet Exp $ */
+/*
+ * Definitions for windowing code.
+ */
+
+/*
+ * Widget types.
+ */
+# define WT_LIST	0
+# define WT_CMENU	1
+# define WT_FRAME	2
+# define WT_STACK	3
+# define WT_MENUBAR	4
