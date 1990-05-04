01fd4e097847388931bda51b5fdf64105aa67e9b
Chris Burghart <burghart@ucar.edu>
1990-05-04 13:42:32 +0000

    function theta_w() changed name to theta_e()
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/rdsslibs@339 3f9b6c58-9310-455a-aec6-8b30269ab333

--- util/met_formulas.h
+++ util/met_formulas.h
@@ -2,12 +2,15 @@
  * Forward declarations
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  89/07/11  11:26:35  burghart
+ * Changed routine declarations from float to double
+ * 
  * Revision 1.1  89/03/16  15:12:35  burghart
  * Initial revision
  * 
  */
 # ifndef VMS
-	double	w_sat (), e_w (), t_mr (), theta_to_t (), theta_w (), t_sat ();
+	double	w_sat (), e_w (), t_mr (), theta_to_t (), theta_e (), t_sat ();
 	double	square (), ten_to_the (), lcl_pres (), lcl_temp ();
 	double	theta_dry (), dewpoint (), e_from_dp (), t_v ();
 # else
@@ -15,7 +18,7 @@
 	double	e_w (double t);
 	double	t_mr (double p, double w);
 	double	theta_to_t (double pt, double p);
-	double	theta_w (double t, double p);
+	double	theta_e (double t, double p);
 	double	t_sat (double ept, double p);
 	double	square (double x);
 	double	ten_to_the (double x);
