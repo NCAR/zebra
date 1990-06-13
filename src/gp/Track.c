b9689fdfedb9305d951a0ef63de8805f37864e24
Jon Corbet <corbet@eol.ucar.edu>
1990-06-13 15:40:11 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@394 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/gp/Track.c
@@ -0,0 +1,23 @@
+/*
+ * Track drawing routines.
+ */
+static char *rcsid = "$Id: Track.c,v 1.1 1990-06-13 15:40:11 corbet Exp $";
+
+
+# include <X11/Intrinsic.h>
+# include "../include/defs.h"
+# include "../include/pd.h"
+# include "GraphProc.h"
+
+
+
+
+
+
+
+
+void
+tr_DrawCAPTrack (comp, update)
+char *comp;
+bool update;
+{ }
