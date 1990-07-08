83ffa42b15e913b2ed06840e69d6ada19e9e179a
Jon Corbet <corbet@eol.ucar.edu>
1990-07-08 12:54:56 +0000

    Ardent shipl.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@407 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/gp/MovieControl.c
+++ src/gp/MovieControl.c
@@ -1,7 +1,7 @@
 /*
  * Movie control functions.
  */
-static char *rcsid = "$Id: MovieControl.c,v 1.2 1990-06-25 14:54:48 corbet Exp $";
+static char *rcsid = "$Id: MovieControl.c,v 1.3 1990-07-08 12:54:56 corbet Exp $";
 
 # include <X11/Intrinsic.h>
 # include <X11/StringDefs.h>
@@ -11,6 +11,7 @@ static char *rcsid = "$Id: MovieControl.c,v 1.2 1990-06-25 14:54:48 corbet Exp $
 # include <X11/Xaw/Label.h>
 # include <X11/Xaw/Cardinals.h>
 # include <X11/Xaw/AsciiText.h>
+# include <X11/Xaw/Scrollbar.h>
 
 # include "../include/defs.h"
 # include "../include/message.h"
@@ -26,8 +27,9 @@ static char *rcsid = "$Id: MovieControl.c,v 1.2 1990-06-25 14:54:48 corbet Exp $
  * Globals.
  */
 # define ATSLEN	80	/* Length for AsciiText strings		*/
-static char Minutes[ATSLEN], Endt[ATSLEN], Frate[ATSLEN];
-static Widget StatusLabel;
+static char Minutes[ATSLEN], Endt[ATSLEN], Frate[ATSLEN], Fskip[ATSLEN];
+static Widget StatusLabel;	/* Where the current status goes	*/
+static Widget Indicator;
 
 /*
  * The actual movie control parameters.
@@ -36,7 +38,8 @@ static Widget StatusLabel;
 static time Mtimes[MAXFRAME];
 static int CurrentFrame;
 static int MovieSlot = -1;
-static int Nframes;
+static int Nframes = 0;
+static int TimeSkip = 1;	/* Minutes between frames		*/
 static int Rate;
 
 /*
@@ -47,23 +50,31 @@ static int Rate;
 	static bool mc_SetupParams (void);
 	static void mc_SetStatus (char *);
 	static bool mc_GetFrameTimes (time *, int);
-	static void mc_MovieRun ();
-	static void mc_MovieStop ();
-	static void mc_MovieRT ();
+	void mc_MovieRun ();
+	void mc_MovieStop ();
+	void mc_MovieRT ();
 	static void mc_GenFrames (void);
-	static void mc_DoNextFrame (void);
+	static void mc_DoNextFrame (int *);
 	static void mc_NextFrame (void);
+	static void mc_SetIndicator (int);
+	static void mc_ScrollCB (Widget, XtPointer, XtPointer);
+	static void mc_Jump (Widget, XtPointer, XtPointer);
+	static void mc_GenNFrame (int *);
 # else
 	static bool mc_SetupParams ();
 	static void mc_SetStatus ();
 	static bool mc_GetFrameTimes ();
 	static Widget mc_MWCreate ();
-	static void mc_MovieRun ();
-	static void mc_MovieStop ();
-	static void mc_MovieRT ();
+	void mc_MovieRun ();
+	void mc_MovieStop ();
+	void mc_MovieRT ();
 	static void mc_GenFrames ();
 	static void mc_NextFrame ();
 	static void mc_DoNextFrame ();
+	static void mc_SetIndicator ();
+	static void mc_ScrollCB ();
+	static void mc_Jump ();
+	static void mc_GenNFrame ();
 # endif
 
 
@@ -109,7 +120,8 @@ XtAppContext appc;
 	}
 	if (! pda_Search (Pd, "global", "frame-rate", NULL, Frate,SYMT_STRING))
 		strcpy (Frate, "2");
-
+	if (! pda_Search (Pd, "global", "frame-skip", NULL, Fskip,SYMT_STRING))
+		strcpy (Fskip, "3");
 /*
  * Create a form widget to hold everything.
  */
@@ -172,7 +184,7 @@ XtAppContext appc;
 	XtSetArg (args[n], XtNinsertPosition, 0); n++;
 	XtSetArg (args[n], XtNlength, ATSLEN); n++;
 	XtSetArg (args[n], XtNresize, XawtextResizeNever); n++;
-	XtSetArg (args[n], XtNwidth, 50); n++;
+	XtSetArg (args[n], XtNwidth, 30); n++;
 	XtSetArg (args[n], XtNheight, 20); n++;
 	XtSetArg (args[n], XtNstring, Minutes); n++;
 	XtSetArg (args[n], XtNtype, XawAsciiString); n++;
@@ -230,7 +242,7 @@ XtAppContext appc;
 	XtSetArg (args[n], XtNinsertPosition, 0); n++;
 	XtSetArg (args[n], XtNlength, ATSLEN); n++;
 	XtSetArg (args[n], XtNresize, XawtextResizeNever); n++;
-	XtSetArg (args[n], XtNwidth, 140); n++;
+	XtSetArg (args[n], XtNwidth, 30); n++;
 	XtSetArg (args[n], XtNheight, 20); n++;
 	XtSetArg (args[n], XtNstring, Frate); n++;
 	XtSetArg (args[n], XtNtype, XawAsciiString); n++;
@@ -240,13 +252,52 @@ XtAppContext appc;
 	w = XtCreateManagedWidget ("moviefr", asciiTextWidgetClass, form,
 		args, n);
 /*
+ * More label.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNfromHoriz, w); n++;
+	XtSetArg (args[n], XtNfromVert, above); n++;
+	XtSetArg (args[n], XtNlabel, "Frame skip: "); n++;
+	XtSetArg (args[n], XtNborderWidth, 0); n++;
+	w = XtCreateManagedWidget ("Movieskpl", labelWidgetClass, form,
+		args, n);
+/*
+ * The frame rate text widget.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNfromHoriz, w); n++;
+	XtSetArg (args[n], XtNfromVert, above); n++;
+	XtSetArg (args[n], XtNdisplayPosition, 0); n++;
+	XtSetArg (args[n], XtNinsertPosition, 0); n++;
+	XtSetArg (args[n], XtNlength, ATSLEN); n++;
+	XtSetArg (args[n], XtNresize, XawtextResizeNever); n++;
+	XtSetArg (args[n], XtNwidth, 30); n++;
+	XtSetArg (args[n], XtNheight, 20); n++;
+	XtSetArg (args[n], XtNstring, Fskip); n++;
+	XtSetArg (args[n], XtNtype, XawAsciiString); n++;
+	XtSetArg (args[n], XtNuseStringInPlace, True); n++;
+	XtSetArg (args[n], XtNleftMargin, 5); n++;
+	XtSetArg (args[n], XtNeditType, XawtextAppend); n++;
+	w = XtCreateManagedWidget ("moviefs", asciiTextWidgetClass, form,
+		args, n);
+/*
+ * More label.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNfromHoriz, w); n++;
+	XtSetArg (args[n], XtNfromVert, above); n++;
+	XtSetArg (args[n], XtNlabel, "minutes."); n++;
+	XtSetArg (args[n], XtNborderWidth, 0); n++;
+	w = XtCreateManagedWidget ("Movieskpm", labelWidgetClass, form,
+		args, n);
+/*
  * The status line:
  */
 	above = label;
 	n = 0;
 	XtSetArg (args[n], XtNfromHoriz, NULL); n++;
 	XtSetArg (args[n], XtNfromVert, above); n++;
-	XtSetArg (args[n], XtNlabel, "Status"); n++;
+	XtSetArg (args[n], XtNlabel, "Status:"); n++;
 	XtSetArg (args[n], XtNborderWidth, 0); n++;
 	label = w = XtCreateManagedWidget ("Moviest", labelWidgetClass, form,
 		args, n);
@@ -262,7 +313,19 @@ XtAppContext appc;
 	XtSetArg (args[n], XtNjustify, XtJustifyLeft); n++;
 	StatusLabel = XtCreateManagedWidget ("movieStatus", labelWidgetClass,
 		form, args,n);
-
+/*
+ * Also a scrollbar to indicate what is happening.
+ */
+	n = 0;
+	XtSetArg (args[n], XtNfromHoriz, label); n++;
+	XtSetArg (args[n], XtNfromVert, label); n++;
+	XtSetArg (args[n], XtNwidth, 300); n++;
+	XtSetArg (args[n], XtNsensitive, True); n++;
+	XtSetArg (args[n], XtNorientation, XtorientHorizontal); n++;
+	Indicator = XtCreateManagedWidget ("movieSB", scrollbarWidgetClass,
+		form, args, n);
+	XtAddCallback (Indicator, XtNscrollProc, mc_ScrollCB, 0);
+	XtAddCallback (Indicator, XtNjumpProc, mc_Jump, 0);
 	return (form);
 }
 
@@ -270,13 +333,12 @@ XtAppContext appc;
 
 
 
-static void
+void
 mc_MovieRun ()
 /*
  * Run the movie.
  */
 {
-	float sec;
 /*
  * Figure out our parameters.
  */
@@ -287,25 +349,16 @@ mc_MovieRun ()
 		return;
 	}
 /*
- * If everything worked until now, cancel any current timer events.
+ * If everything worked until now, cancel any current timer events and drop
+ * out of real time mode.
  */
 	tl_AllCancel ();
+	PlotMode = History;
 /*
- * Generate each movie frame.
+ * Start the process of generating the movie frames.
  */
+	fc_MarkFrames (Mtimes, Nframes);
 	mc_GenFrames ();
-/*
- * Display the first frame.
- */
-	mc_SetStatus ("Running");
-	CurrentFrame = 0;
-	mc_NextFrame ();
-/*
- * Start the timer to do the rest.
- */
-	sec = 1.0/(float) Rate;
-	MovieSlot = tl_AddRelativeEvent (mc_NextFrame, 0, (int) (sec*INCFRAC),
-		(int) (sec*INCFRAC));
 }
 
 
@@ -320,6 +373,7 @@ mc_SetupParams ()
 {
 	int minutes;
 	union usy_value v;
+	char fskipk[ATSLEN];
 /*
  * Figure out what is in the control widget now.
  */
@@ -338,12 +392,20 @@ mc_SetupParams ()
 		mc_SetStatus ("Unable to understand frame rate");
 		return (FALSE);
 	}
+	if (! sscanf (Fskip, "%d", &TimeSkip))
+	{
+		mc_SetStatus ("Unable to understand frame skip");
+		return (FALSE);
+	}
 /*
  * Store these values in the PD.
  */
-	pd_Store (Pd, "global", "movie-minutes", &minutes, SYMT_INT);
-	pd_Store (Pd, "global", "movie-end-time", &v.us_v_date, SYMT_DATE);
-	pd_Store (Pd, "global", "frame-rate", &Rate, SYMT_INT);
+	pd_Store (Pd, "global", "movie-minutes", (char *) &minutes, SYMT_INT);
+	pd_Store (Pd, "global", "movie-end-time", (char *) &v.us_v_date,
+			SYMT_DATE);
+	pd_Store (Pd, "global", "frame-rate", (char *) &Rate, SYMT_INT);
+	sprintf (fskipk, "%dm", TimeSkip);
+	pd_Store (Pd, "global", "frame-skip", fskipk, SYMT_STRING);
 /*
  * Now calculate our frame times.
  */
@@ -375,19 +437,68 @@ mc_GenFrames ()
 		FrameCount = Nframes;
 		XtSetArg (args[0], XtNframeCount, Nframes);
 		XtSetValues (Graphics, args, ONE);
+		pd_Store (Pd, "global", "time-frames", (char *)&Nframes,
+			SYMT_INT);
 	}
-	fc_InvalidateCache ();
+	/* fc_InvalidateCache (); */
 /*
- * Now plot each frame, thus loading it into the cache.
+ * Start the process of generating the frames.  This is done through the
+ * event queue, allowing for event processing and stopping the whole process
+ * if necessary.
  */
-	for (f = 0; f < Nframes; f++)
-	{
-		msg_ELog (EF_DEBUG, "Gen frame at %d %d", Mtimes[f].ds_yymmdd,
-			Mtimes[f].ds_hhmmss);
-		PlotTime = Mtimes[f];
-		px_PlotExec ("global");
-	}
+	f = 0;
+	Eq_AddEvent (PDisplay, mc_GenNFrame, &f, sizeof (int), Override);
 }
+
+
+
+
+
+static void
+mc_GenNFrame (which)
+int *which;
+/*
+ * Generate the WHICHth movie frame.
+ */
+{
+	char msg[50];
+	int next;
+	float sec;
+/*
+ * Update the message.
+ */
+	sprintf (msg, "Generating frames: %d/%d", *which, Nframes);
+	mc_SetStatus (msg);
+/*
+ * Do the frame.
+ */
+	CurrentFrame = *which;
+	mc_SetIndicator (*which);
+	PlotTime = Mtimes[*which];
+	px_PlotExec ("global");
+/*
+ * If we're not done, arrange to have the next frame done.
+ */
+	if ((next = *which + 1) < Nframes)
+	{
+		Eq_AddEvent (PDisplay, mc_GenNFrame, &next, sizeof (int),
+			Augment);
+		return;
+	}
+/*
+ * Otherwise now it's time to start the movie sequence process.
+ */
+	mc_SetStatus ("Running");
+	CurrentFrame = 0;
+	mc_NextFrame ();
+/*
+ * Start the timer to do the rest.
+ */
+	sec = 1.0/(float) Rate;
+	MovieSlot = tl_AddRelativeEvent (mc_NextFrame, 0, (int) (sec*INCFRAC),
+		(int) (sec*INCFRAC));
+}
+
 
 
 
@@ -400,31 +511,20 @@ int minutes;
  * Calculate the frame times for this movie.
  */
 {
-	char ctrigger[80];
-	int trigger, incr, f, seconds;
+	int incr, f, seconds;
 	time t;
-/*
- * Find our trigger condition.  This code for now assumes a time trigger
- * only.  That will eventually have to change.
- */
-	if (! pda_Search (Pd, "global", "trigger", 0, ctrigger, SYMT_STRING) ||
-		! (trigger = pc_TimeTrigger (ctrigger)))
-	{
-		mc_SetStatus ("Missing or bad TRIGGER");
-		return (FALSE);
-	}
 /*
  * Now fix up the plot time.
  */
 	seconds = (end->ds_hhmmss/10000)*60*60 +
 		  ((end->ds_hhmmss/100) % 100)*60 + (end->ds_hhmmss % 100);
-	seconds -= seconds % trigger;
+	seconds -= seconds % 60;
 	end->ds_hhmmss = (seconds/3600)*10000 + ((seconds/60) % 60)*100 +
 				seconds % 60;
 /*
  * Figure out how many frames we have.
  */
-	Nframes = (minutes*60)/trigger + 1;
+	Nframes = minutes/TimeSkip + 1;
 	if (Nframes > MAXFRAME)
 	{
 		mc_SetStatus ("Too many frames");
@@ -433,7 +533,7 @@ int minutes;
 /*
  * Go through and figure out each frame time.
  */
-	incr = (trigger/60)*100 + (trigger % 60);
+	incr = TimeSkip*100;
 	t = *end;
 	for (f = Nframes - 1; f >= 0; f--)
 	{
@@ -447,30 +547,44 @@ int minutes;
 
 
 
-static void
+void
 mc_MovieStop ()
 /*
  * Stop the movie.
  */
 {
-	mc_SetStatus ("Stopped");
+	mc_SetStatus ("Stopped (history mode)");
+/*
+ * If there are timer events active, get rid of them.
+ */
 	if (MovieSlot >= 0)
 	{
 		tl_Cancel (MovieSlot);
 		MovieSlot = -1;
 	}
+/*
+ * Get rid of any event queue entries relating to frame generation.
+ */
+	Eq_ZapProc (PDisplay, mc_GenNFrame);
+/*
+ * Throw the system into history mode, showing the current frame.
+ */
+	HistoryMode (&Mtimes[CurrentFrame]);
 }
 
 
 
 
-static void
+void
 mc_MovieRT ()
 /*
  * Stop the movie and go back to real time mode.
  */
 {
+	mc_MovieStop ();
 	mc_SetStatus ("Inactive (real time)");
+	pd_Store (Pd, "global", "plot-mode", "real-time", SYMT_STRING);
+	pc_PlotHandler ();
 }
 
 
@@ -489,6 +603,7 @@ char *string;
 	XtSetArg (args[0], XtNlabel, string);
 	XtSetValues (StatusLabel, args, ONE);
 	eq_sync ();
+	xtEvent (0);	/* XXX */
 }
 
 
@@ -501,19 +616,124 @@ mc_NextFrame ()
  * Arrange to have the next frame displayed.
  */
 {
-	Eq_AddEvent (PDisplay, mc_DoNextFrame, NULL, 0, Bounce);
+	Eq_AddEvent (PDisplay, mc_DoNextFrame, &CurrentFrame, sizeof (int), 
+		Bounce);
+	if (++CurrentFrame >= Nframes)
+		CurrentFrame = 0;
 }
 
 
 
 static void
-mc_DoNextFrame ()
+mc_DoNextFrame (frame)
+int *frame;
 /*
  * Actually display the next frame in the movie.
  */
 {
-	PlotTime = Mtimes[CurrentFrame];
-	if (++CurrentFrame >= Nframes)
-		CurrentFrame = 0;
+	PlotTime = Mtimes[*frame];
+	mc_SetIndicator (*frame);
 	px_PlotExec ("global");
+}
+
+
+
+
+
+
+static void
+mc_SetIndicator (frame)
+int frame;
+/*
+ * Set the indicator to show that we're on this frame.
+ */
+{
+	float width = 1.0/Nframes;
+
+	XawScrollbarSetThumb (Indicator, frame*width, width);
+	eq_sync ();
+}
+
+
+
+
+void
+mc_Dial (motion)
+int motion;
+/*
+ * Deal with dial motion.
+ */
+{
+/*
+ * Insist on having some stuff around.
+ */
+	if (Nframes <= 0)
+	{
+		msg_ELog (EF_PROBLEM, "No movie frames saved!");
+		return;
+	}
+/*
+ * Do the change.
+ */
+	if ((CurrentFrame += motion) < 0)
+		CurrentFrame += Nframes;
+	else if (CurrentFrame >= Nframes)
+		CurrentFrame -= Nframes;
+	Eq_AddEvent (PDisplay, mc_DoNextFrame, &CurrentFrame, sizeof (int), 
+		Augment);
+}
+
+
+
+
+
+static void
+mc_ScrollCB (sb, junk, position)
+Widget sb;
+XtPointer junk, position;
+/*
+ * The scrollbar callback.
+ */
+{
+	int ipos = (int) position;
+
+	mc_Dial (ipos < 0 ? 1 : -1);
+}
+
+
+
+
+static void
+mc_Jump (sb, junk, pct)
+Widget sb;
+XtPointer junk, pct;
+/*
+ * Implement jump scrolling.
+ */
+{
+	int frame = Nframes * (* (float *) pct);
+/*
+ * Make sure something exists.
+ */
+	if (Nframes <= 0)
+	{
+		msg_ELog (EF_PROBLEM, "No movie information saved");
+		return;
+	}
+/*
+ * Range checking.
+ */
+	if (frame < 0)
+		frame = 0;
+	else if (frame >= Nframes)
+		frame = Nframes - 1;
+/*
+ * Redisplay only if they have moved far enough to get a different frame.
+ */
+	if (frame != CurrentFrame)
+	{
+		CurrentFrame = frame;
+		Eq_AddEvent (PDisplay, mc_DoNextFrame, &CurrentFrame,
+			sizeof (int), Override);
+	}
 }
