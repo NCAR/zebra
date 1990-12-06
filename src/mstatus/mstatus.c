667f5c605c1abca6c273e70e460ae07fabc32f0a
Jon Corbet <corbet@eol.ucar.edu>
1990-12-06 11:04:33 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@565 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/mstatus/mstatus.c
@@ -0,0 +1,28 @@
+/*
+ * Message server zapper.
+ */
+# include "message.h"
+
+main ()
+{
+	int handler ();
+	struct mh_template tm;
+
+	msg_connect (handler, "Status reporter");
+	tm.mh_type = MH_STATS;
+	msg_send (MSG_MGR_NAME, MT_MESSAGE, 0, &tm, sizeof (tm));
+	msg_await ();
+}
+
+
+
+handler (msg)
+struct message *msg;
+{
+	if (msg->m_proto != MT_MESSAGE)
+		return;
+	if (msg->m_len == 0)
+		return (1);
+	printf ("%s\n", msg->m_data);
+	return (0);
+}
