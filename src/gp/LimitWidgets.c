ca83f4de217741332f77e2f16153590c1db9005d
Jon Corbet <corbet@eol.ucar.edu>
1991-01-30 21:00:23 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@634 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/gp/LimitWidgets.c
@@ -0,0 +1,9 @@
+/*
+ * Widgets for changing plot limits.
+ */
+static char *rcsid = "$Id: LimitWidgets.c,v 1.1 1991-01-30 21:00:23 corbet Exp $";
+
+# include "../include/defs.h"
+# include "../include/message.h"
+# include "GraphProc.h"
+# include <ui_window.h>
