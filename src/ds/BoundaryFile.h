7b13484f2308eded8239f5e392a5f93f6e93ff30
Jon Corbet <corbet@eol.ucar.edu>
1991-02-21 16:48:45 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@663 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/ds/BoundaryFile.h
@@ -0,0 +1,4 @@
+/*
+ * The structures defining a boundary file.
+ */
+/* $Id: BoundaryFile.h,v 1.1 1991-02-21 16:48:45 corbet Exp $ */
---
+++ src/ds/DFA_Boundary.c
@@ -0,0 +1,4 @@
+/*
+ * Deal with Boundary-format files.
+ */
+static char *rcsid = "$Id: DFA_Boundary.c,v 1.1 1991-02-21 16:47:13 corbet Exp $";
