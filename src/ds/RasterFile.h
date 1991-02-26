f10c2da1bb2b4cc9c47cab7aabdcffee9277a9bd
Jon Corbet <corbet@eol.ucar.edu>
1991-02-26 18:46:31 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@666 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/ds/RasterFile.h
@@ -0,0 +1,5 @@
+/* $Id: RasterFile.h,v 1.1 1991-02-26 18:46:31 corbet Exp $ */
+/*
+ * This is the file for FCC native raster files.  Note that these are not
+ * (necessarily) *image* files -- they are simply a form of gridded data.
+ */
