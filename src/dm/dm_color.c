bd0babf0a5a2715ada13b95fe145d245f1e9e321
Jon Corbet <corbet@eol.ucar.edu>
1991-09-12 01:30:31 +0000

    Added the stupid copyright notice.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@909 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/dm/dm_color.c
+++ src/dm/dm_color.c
@@ -1,7 +1,24 @@
 /*
  * Color table routines.
  */
-static char *rcsid = "$Id: dm_color.c,v 2.0 1991-07-18 23:11:58 corbet Exp $";
+static char *rcsid = "$Id: dm_color.c,v 2.1 1991-09-12 01:30:31 corbet Exp $";
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
 
 # include <X11/Xlib.h>
 # include <ui.h>
