e92da416c22b25381f7002093752e7e66ed3c67d
Jon Corbet <corbet@eol.ucar.edu>
1991-09-12 01:33:01 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@910 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/lib/copyright.h
@@ -0,0 +1,17 @@
+static char *Copyright =
+"		Copyright (C) 1987,88,89,90,91 by UCAR\n\
+	University Corporation for Atmospheric Research\n\
+		   All rights reserved\n\
+\n\
+No part of this work covered by the copyrights herein may be reproduced\n\
+or used in any form or by any means -- graphic, electronic, or mechanical,\n\
+including photocopying, recording, taping, or information storage and\n\
+retrieval systems -- without permission of the copyright owner.\n\
+\n\
+This software and any accompanying written materials are provided \"as is\"\n\
+without warranty of any kind.  UCAR expressly disclaims all warranties of\n\
+any kind, either express or implied, including but not limited to the\n\
+implied warranties of merchantibility and fitness for a particular purpose.\n\
+UCAR does not indemnify any infringement of copyright, patent, or trademark\n\
+through use or modification of this software.  UCAR does not provide \n\
+maintenance or updates for its software.\n";
