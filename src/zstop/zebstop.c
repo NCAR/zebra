fc4de6a3fbf65a007fac3a2a5584a4e96b58dea9
Jon Corbet <corbet@eol.ucar.edu>
1991-09-12 02:02:09 +0000

    Added the copyright notice.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@914 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/zstop/zebstop.c
+++ src/zstop/zebstop.c
@@ -1,7 +1,27 @@
 /*
  * Message server zapper.
  */
+static char *rcsid = "$Id: zebstop.c,v 2.1 1991-09-12 02:02:09 corbet Exp $";
+
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
 # include "message.h"
+# include <copyright.h>
 
 main ()
 {
