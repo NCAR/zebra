5746ad8723ed3174b02d4d65d206110821cb9363
Chris Burghart <burghart@ucar.edu>
1991-09-16 22:28:15 +0000

    added copyright stuff
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@934 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ingest/radar/Input.c
+++ src/ingest/radar/Input.c
@@ -1,7 +1,24 @@
 /*
  * Deal with getting radar beams into the system.
  */
-static char *rcsid = "$Id: Input.c,v 2.0 1991-07-18 23:18:20 corbet Exp $";
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+static char *rcsid = "$Id: Input.c,v 2.1 1991-09-16 22:15:47 burghart Exp $";
 
 # include <sys/types.h>
 # include <sys/time.h>
--- src/ingest/radar/radar_ingest.c
+++ src/ingest/radar/radar_ingest.c
@@ -1,8 +1,27 @@
 /*
  * Ingest radar data and rasterize it.
  */
-static char *rcsid = "$Id: radar_ingest.c,v 2.0 1991-07-18 23:18:20 corbet Exp $";
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
 
+static char *rcsid = "$Id: radar_ingest.c,v 2.1 1991-09-16 22:21:25 burghart Exp $";
+
+# include <copyright.h>
 # include <errno.h>
 # include <sys/time.h>
 # include <sys/resource.h>
