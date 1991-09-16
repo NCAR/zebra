7aefb4cad8abc73a38fb3145d794184948304779
Chris Burghart <burghart@ucar.edu>
1991-09-16 21:57:38 +0000

    added copyright stuff
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@931 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ingest/aircraft/ac_ingest.h
+++ src/ingest/aircraft/ac_ingest.h
@@ -1,3 +1,20 @@
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
 # include <ui.h>
 # include <defs.h>
 # include <message.h>
@@ -7,7 +24,7 @@
 # define STRLEN	30
 # define BADVAL -32768
 # define BUFLEN	1000
-# define MAXOURS 10
+# define MAXOURS 50
 # define KM_PER_NM (6080.0 / (5280.0 * 0.621))
 # define M_PER_FT 0.30480
 # define A_FEW 20
@@ -52,13 +69,14 @@ typedef struct ourac
  */
 char	BlackBox[STRLEN], DialOut[STRLEN], OurAircraft[BUFLEN];
 char	*Command = "1";
-char	*Startup = "g f2000";
+char	*Startup = "g f20000";
 char	*Kill = "6";
 char	*SendAll = "2";
 char	Ac_Platform[STRLEN];
 int	BaudRate; 
 float	RangeRes, AzimuthRes;
 float	RadarLat, RadarLon;
+float	AltMin, AltMax, LatMin, LatMax, LonMin, LonMax;
 
 /*
  * File descriptor of device file where black box is.
