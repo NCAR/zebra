d5bf9a6cc20d7d7cf1d12350072bf29ac7f5a7c1
Chris Burghart <burghart@ucar.edu>
1991-09-17 16:29:01 +0000

    Added and removed variables, added copyright stuff
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@945 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/Optimizer/globals.h
+++ src/Optimizer/globals.h
@@ -1,9 +1,27 @@
 /*
  * Global definitions
- * $Id: globals.h,v 1.1 1991-06-16 17:12:09 burghart Exp $
+ * $Id: globals.h,v 1.2 1991-09-17 16:29:01 burghart Exp $
+ */
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
  */
 
 # include <math.h>
+# include <message.h>
 # include <ui_param.h>		/* For bool definition */
 
 /*
@@ -21,14 +39,14 @@
 # define MAX(x,y)	((x) > (y) ? (x) : (y))
 
 /*
+ * Maximum number of radars we can handle
+ */
+# define MAXRAD	10
+
+/*
  * Dummy time value for generating the fastest possible scan
  */
 # define TIME_ASAP	-999.0
-
-/*
- * Configuration name
- */
-extern char	ConfigName[];
 
 /*
  * User-specified desired spatial resolution and minimum beam separation
@@ -47,11 +65,16 @@ extern float	Vol_bot, Vol_top;
 extern float	Vol_time;
 
 /*
- * Are we connected to the message handler?
- * Is the data store available?
+ * Current option and option limits
  */
-extern bool	Msg;
-extern bool	Ds;
+# define NO_OPT	-999
+int	Opt;
+int	MinOpt, MaxOpt;
+
+/*
+ * The slow volume scan time for each option
+ */
+float	Slowtime[];
 
 /*
  * Step to use for generating alternate resolutions
