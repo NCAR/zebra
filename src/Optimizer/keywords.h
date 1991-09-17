58376b255c3d4fa333635f03e4419e515d8f2bc8
Chris Burghart <burghart@ucar.edu>
1991-09-17 16:30:08 +0000

    added copyright stuff
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@946 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/Optimizer/keywords.h
+++ src/Optimizer/keywords.h
@@ -1,3 +1,25 @@
+/*
+ * Optimizer UI keyword definitions
+ * $Id: keywords.h,v 1.2 1991-09-17 16:30:08 burghart Exp $
+ */
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+
 # define KW_SUGAR	0
 # define KW_DISPLAY	1
 # define KW_VOLUME	2
