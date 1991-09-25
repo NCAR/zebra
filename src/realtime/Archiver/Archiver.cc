e726163c9773ce8caae53fb78c15861be5aae180
Chris Burghart <burghart@ucar.edu>
1991-09-25 22:17:04 +0000

    added copyright stuff
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@962 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/realtime/Archiver/Archiver.cc
+++ src/realtime/Archiver/Archiver.cc
@@ -4,6 +4,27 @@
  * Basically: we wake up every so often, look at all the files in the system,
  * and write them out to tape in tar format.
  */
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+
+static char *rcsid = "$Id: Archiver.cc,v 1.2 1991-09-25 22:15:39 burghart Exp $";
+
+# include <copyright.h>
 # include <stdio.h>
 # include <fcntl.h>
 # include <errno.h>
