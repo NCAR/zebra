5fc2fbbe95f49fe53941c2d3e03121cbddeb866e
Deborah Gracio <dkgracio>
1991-09-26 22:56:39 +0000

    Added copyright notice. ,.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@988 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ds/commands.h
+++ src/ds/commands.h
@@ -1,7 +1,25 @@
 /*
- * $Id: commands.h,v 2.0 1991-07-18 22:53:23 corbet Exp $
+ * $Id: commands.h,v 2.1 1991-09-26 22:56:39 gracio Exp $
  *
  * Command keywords for the data store daemon.
+ */
+
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
  */
 # define DK_PLATFORM	 1
 # define DK_ENDPLATFORM  2
