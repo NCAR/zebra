7ba26e8423ada6692d709a2a4a95a9cc69850f63
Deborah Gracio <dkgracio>
1991-09-26 22:43:21 +0000

    Added copyright message
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@983 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ds/DataStore.h
+++ src/ds/DataStore.h
@@ -1,7 +1,25 @@
 /*
- * $Id: DataStore.h,v 2.1 1991-09-24 21:16:52 martin Exp $
+ * $Id: DataStore.h,v 2.2 1991-09-26 22:43:21 gracio Exp $
  *
  * Public data store definitions.
+ */
+
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
  */
 
 # define MAXFIELD	30	/* Max fields in one data object	*/
