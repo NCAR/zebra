74fbab4eb59a8c43202e01c761f82d8442bedada
Deborah Gracio <dkgracio>
1991-09-26 23:12:59 +0000

    Added copyright notice.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@992 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ds/RasterFile.h
+++ src/ds/RasterFile.h
@@ -1,9 +1,27 @@
-/* $Id: RasterFile.h,v 2.0 1991-07-18 22:53:23 corbet Exp $ */
+/* $Id: RasterFile.h,v 2.1 1991-09-26 22:53:44 gracio Exp $ */
+
 /*
  * This is the file for FCC native raster files.  Note that these are not
  * (necessarily) *image* files -- they are simply a form of gridded data.
  */
 
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
 /*
  * Fields are described by the following:
  */
--- src/ds/d_Notify.c
+++ src/ds/d_Notify.c
@@ -1,7 +1,25 @@
 /*
  * The application notification module.
  */
-static char *rcsid = "$Id: d_Notify.c,v 2.0 1991-07-18 22:55:54 corbet Exp $";
+
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+static char *rcsid = "$Id: d_Notify.c,v 2.1 1991-09-26 22:58:59 gracio Exp $";
 
 # include "../include/defs.h"
 # include "../include/message.h"
