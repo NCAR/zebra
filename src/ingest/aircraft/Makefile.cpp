15b7aa4c42733a612e6e993569567ef6f9ddaed3
Jon Corbet <corbet@eol.ucar.edu>
1991-10-24 22:55:14 +0000

    parameterization.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@1043 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ingest/aircraft/Makefile.cpp
+++ src/ingest/aircraft/Makefile.cpp
@@ -15,21 +15,19 @@
  * through use or modification of this software.  UCAR does not provide 
  * maintenance or updates for its software.
  */
-MFVERSION="$Id: Makefile.cpp,v 1.2 1991-09-26 17:59:54 gracio Exp $"
+MFVERSION="$Id: Makefile.cpp,v 1.3 1991-10-24 22:55:14 corbet Exp $"
+
+# include "../../include/config.h"
 
 # ifdef sun
 /*
  * Sun options
  */
-CC=gcc
-CFLAGS=  -g -O -I$(ZEBHOME)/fcc/include -I$(ZEBHOME)/rdss/include
-LIBS= $(ZEBHOME)/fcc/lib/libfcc.a -lnetcdf -lrdss -lXaw -lXt -lXmu -lXext -lX11 -ltermcap -lm
+CC=CCompiler
+CFLAGS=CCOptions -I$(FCCINC) -I$(RDSSINC)
+LIBS= ZebLibrary -lnetcdf -lrdss -lXaw -lXmu -lXt -lXext -lX11 -ltermcap -lm
 # endif
 
-
-BINDIR=../../bin
-LIBDIR=../../lib
-HDIR=../../include
 
 OBJS = ac_ingest.o
 OBJS2 = ac_status.o
@@ -37,10 +35,10 @@ OBJS2 = ac_status.o
 all:	ac_ingest ac_status ac_ingest.lf ac_status.lf 
 
 install:	ac_ingest ac_ingest.lf ac_status ac_status.lf
-	install -c ac_ingest $(BINDIR)
-	install -c ac_status $(BINDIR)
-	install -c -m 0444 ac_ingest.lf $(LIBDIR)
-	install -c -m 0444 ac_status.lf $(LIBDIR)
+	install -c ac_ingest D_BINDIR
+	install -c ac_status D_BINDIR
+	install -c -m 0444 ac_ingest.lf D_LIBDIR
+	install -c -m 0444 ac_status.lf D_LIBDIR
 
 include:
 
@@ -57,7 +55,7 @@ ac_status.lf: ac_status.state
 	uic < make-sw-lf
 
 clean:
-	rm -f *~ ac_ingest *.o Makefile.bak
+	rm -f *~ ac_ingest *.o Makefile.bak ac_status *.lf
 
 Makefile: mf
 
@@ -65,7 +63,7 @@ mf:
 	mv Makefile Makefile~
 	cp Makefile.cpp Makefile.c
 	echo "# DO NOT EDIT -- EDIT Makefile.cpp INSTEAD" > Makefile
-	cc -E Makefile.c >> Makefile
+	cc -E -DMAKING_MAKEFILE Makefile.c | cat -s >> Makefile
 	rm -f Makefile.c
 	make depend
 
