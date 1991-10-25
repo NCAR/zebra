d8099a16093f44f587894350afbe1fc4c5f87b20
Kris Gould <kris@eol.ucar.edu>
1991-10-25 18:05:28 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@1050 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/lib/vector.c
@@ -0,0 +1,75 @@
+/*
+ * Vector drawing routine.
+ */
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+
+# include <X11/Intrinsic.h>
+# include <math.h>
+
+# define ARROWANG .2618 /* PI/12 */
+
+
+void
+draw_vector(W, D, Gcontext, x, y, u, v, unit)
+Display *W;
+Drawable D;
+GC Gcontext;
+int x, y;
+double u, v, unit;
+/*
+ *  Draw a vector.
+ */
+{
+	float dx, dy;
+	int xend, yend;
+	float veclen, vecang, ang;
+/*
+ *  Draw the shaft of the vector.
+ */
+	dx = u * unit;
+	dy = -v * unit;
+
+	xend = (int)(x + dx + 0.5);
+	yend = (int)(y + dy + 0.5);
+
+	XDrawLine(W, D, Gcontext, x, y, xend, yend);
+/*
+ *  If the vector has any length, put on the arrow head.
+ */
+	if(dx != 0 || dy !=0)
+	{
+		vecang = atan2(v,u);
+		veclen = hypot(u,v);
+
+		ang = vecang + ARROWANG;
+		dx = 0.4 * veclen * unit * cos(ang);
+		dy = -0.4 * veclen * unit * sin(ang);
+
+		XDrawLine(W, D, Gcontext, xend, yend,
+			(int)(xend - dx), (int)(yend - dy));
+
+		ang = vecang - ARROWANG;
+		dx = 0.4 * veclen * unit * cos(ang);
+		dy = -0.4 * veclen * unit * sin(ang);
+
+		XDrawLine(W, D, Gcontext, xend, yend,
+			(int)(xend - dx), (int)(yend - dy));
+	}
+}
+
