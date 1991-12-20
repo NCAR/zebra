d2e21e0a12ee565953de45e6adbfa45241fe8929
Jon Corbet <corbet@eol.ucar.edu>
1991-12-20 17:48:56 +0000

    Parameterization stuff -- libraries.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@1211 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ds/Makefile.cpp
+++ src/ds/Makefile.cpp
@@ -1,65 +1,61 @@
-MFVERSION="$Id: Makefile.cpp,v 1.6 1991-11-27 21:49:37 corbet Exp $"
+MFVERSION="$Id: Makefile.cpp,v 1.7 1991-12-20 17:48:56 corbet Exp $"
 
 # include "../include/config.h"
 
-# ifdef sun
-/*
- * Sun options
- */
 CC=CCompiler
-CFLAGS= CCOptions -I$(FCCINC) -I$(RDSSINC)
-LIBS=ZebLibrary -lrdss -ltermcap -lnetcdf -lm
-XLIBS=-lXaw -lXmu -lXt -lXext -lX11
-# endif
+CFLAGS= CCOptions IncludeDirs
+LIBS=ZebLibrary MiscLibs CDFLibrary
+XLIBS=XLibraries
 
 
-
+/* DSOBJS = Daemon-specific object modules */
 DSOBJS= Daemon.o d_SharedMemory.o d_DataTables.o d_Config.o d_Notify.o
+/* OBJS, SRCS = normal DS library code */
 OBJS = Appl.o SharedMemory.o DataFileAccess.o DFA_NetCDF.o GetList.o \
-	DFA_Boundary.o DFA_Raster.o
+	DFA_Boundary.o DFA_Raster.o Fields.o
 SRCS = Appl.c SharedMemory.c DataFileAccess.c DFA_NetCDF.c GetList.c \
-	DFA_Boundary.c DFA_Raster.c
+	DFA_Boundary.c DFA_Raster.c Fields.c
 
-# if BUILD_NETXFR
-all:	dsDaemon dsDaemon.lf $(OBJS) dsdump dsdelete prt_Notify \
-		NetXfr NetXfr.lf Archiver LastData dsdwidget
+/* DCOBJS, DCSRCS =  data chunk modules */
+DCOBJS = ConvertDObj.o DataChunk.o dc_Boundary.o dc_IRGrid.o dc_Image.o \
+	dc_MetData.o dc_RGrid.o dc_Scalar.o dc_Transp.o dc_Attr.o
+DCSRCS = ConvertDObj.c DataChunk.c dc_Boundary.c dc_IRGrid.c dc_Image.c \
+	dc_MetData.c dc_RGrid.c dc_Scalar.c dc_Transp.c dc_Attr.c
 
-install:	dsDaemon dsDaemon.lf $(OBJS) dsdelete include prt_Notify \
-		NetXfr NetXfr.lf dsdump Archiver LastData dsdwidget
+
+all::	dsDaemon dsDaemon.lf $(OBJS) $(DCOBJS) dsdump dsdelete prt_Notify \
+	dsdwidget
+
+install::	dsDaemon dsDaemon.lf $(OBJS) dsdelete include prt_Notify \
+		dsdump dsdwidget $(DCOBJS)
 	install -c dsDaemon D_BINDIR
-	install -c Archiver D_BINDIR
-	install -c LastData D_BINDIR
-	install -c -m 04555 -o root NetXfr D_BINDIR
 	install -c -s dsdelete D_BINDIR
 	install -c -s prt_Notify D_BINDIR
 	install -c -s dsdump D_BINDIR
 	install -c -s dsdwidget D_BINDIR
 	install -c -m 0444 dsDaemon.lf D_LIBDIR
+	ar ruv ZebLibrary $(OBJS) $(DCOBJS)
+	ranlib ZebLibrary
+
+# if RT_DS_TOOLS
+all::	NetXfr Archiver LastData
+
+install::	NetXfr NetXfr.lf Archiver LastData
 	install -c -m 0444 NetXfr.lf D_LIBDIR
-	ar ruv ZebLibrary $(OBJS)
-	ranlib ZebLibrary
-
-# else
-all:	dsDaemon dsDaemon.lf $(OBJS) dsdump dsdelete prt_Notify \
-		Archiver LastData dsdwidget
-
-install:	dsDaemon dsDaemon.lf $(OBJS) dsdelete include prt_Notify \
-		dsdump Archiver LastData dsdwidget
-	install -c dsDaemon D_BINDIR
+	install -c -m 04555 -o root NetXfr D_BINDIR
 	install -c Archiver D_BINDIR
-	install -c LastData D_BINDIR
-	install -c -s dsdelete D_BINDIR
-	install -c -s prt_Notify D_BINDIR
-	install -c -s dsdump D_BINDIR
-	install -c -s dsdwidget D_BINDIR
-	install -c -m 0444 dsDaemon.lf D_LIBDIR
-	ar ruv ZebLibrary $(OBJS)
-	ranlib ZebLibrary
-
+	install -c -s LastData D_BINDIR
+	
 # endif
+
 
 include:
 	install -c -m 0444 DataStore.h D_FCCINC
+	install -c -m 0444 ds_fields.h D_FCCINC
+	install -c -m 0444 DataChunk.h D_FCCINC
+
+dctest:		dctest.o $(DCOBJS)
+	$(CC) $(CFLAGS) -o dctest dctest.o $(DCOBJS) $(LIBS)
 
 dsDaemon:	$(DSOBJS) $(OBJS)
 	$(CC) $(CFLAGS) -o dsDaemon $(DSOBJS) $(OBJS) $(LIBS) $(XLIBS)
@@ -67,8 +63,6 @@ dsDaemon:	$(DSOBJS) $(OBJS)
 dsDaemon.lf:	Daemon.state
 	uic < make-lf
 
-NetXfr.lf:	NetXfr.state
-	uic < make-nx-lf
 
 dsdump:	dsdump.o $(OBJS)
 	$(CC) $(CFLAGS) -o dsdump dsdump.o $(OBJS) $(LIBS)
@@ -76,24 +70,24 @@ dsdump:	dsdump.o $(OBJS)
 dsdwidget:	dsdwidget.o $(OBJS)
 	$(CC) $(CFLAGS) -o dsdwidget dsdwidget.o $(OBJS) $(LIBS) $(XLIBS)
 
-LastData:	LastData.o $(OBJS)
-	$(CC) $(CFLAGS) -o LastData LastData.o $(OBJS) $(LIBS) $(XLIBS)
-
 prt_Notify:	prt_Notify.o d_Notify.o $(OBJS)
 	$(CC) $(CFLAGS) -o prt_Notify prt_Notify.o d_Notify.o $(OBJS) $(LIBS)
 
-add:	add.o $(OBJS)
-	$(CC) $(CFLAGS) -o add add.o $(OBJS) $(LIBS)
 
-Archiver:	Archiver.o $(OBJS)
-	$(CC) $(CFLAGS) -o Archiver Archiver.o $(OBJS) $(LIBS) $(XLIBS)
-
+# if RT_DS_TOOLS
 NetXfr:	NetXfr.o nx_BCast.o nx_PktGrabber.o nx_DirImage.o $(OBJS)
 	$(CC) $(CFLAGS) -o NetXfr NetXfr.o nx_BCast.o nx_PktGrabber.o \
 			nx_DirImage.o $(OBJS) $(LIBS) $(XLIBS);
 
-notify:	notify.o $(OBJS)
-	$(CC) $(CFLAGS) -o notify notify.o $(OBJS) $(LIBS)
+LastData:	LastData.o $(OBJS)
+	$(CC) $(CFLAGS) -o LastData LastData.o $(OBJS) $(LIBS) $(XLIBS)
+
+NetXfr.lf:	NetXfr.state
+	uic < make-nx-lf
+
+Archiver:	Archiver.o $(OBJS)
+	$(CC) $(CFLAGS) -o Archiver Archiver.o $(OBJS) $(LIBS) $(XLIBS)
+# endif
 
 dsdelete:	dsdelete.o $(OBJS)
 	$(CC) $(CFLAGS) -o dsdelete dsdelete.o $(OBJS) $(LIBS)
@@ -105,7 +99,7 @@ dsdelete:	dsdelete.o $(OBJS)
  */
 saber_lib:
 	#setopt ansi
-	#load $(CFLAGS) $(SRCS)
+	#load $(CFLAGS) $(SRCS) $(DCOBJS)
 	#load -Bstatic $(LIBS) /locallib/gcc-gnulib
 
 saber_dsd: saber_lib
@@ -114,9 +108,15 @@ saber_dsd: saber_lib
 saber_pn: saber_lib
 	#load $(CFLAGS) prt_Notify.c d_Notify.c
 
+saber_dct:
+	#setopt ansi
+	#load $(CFLAGS) dctest.c $(DCSRCS)
+	#load -Bstatic $(LIBS) /locallib/gcc-gnulib
+
+
 clean:
 	rm -f *~ dsDaemon LastData dsdump dsdwidget dsdelete NetXfr 
-	rm -f prt_Notify core notify dsDaemon.lf *.o Makefile.bak
+	rm -f prt_Notify core notify rfdump Archiver *.lf *.o Makefile.bak
 
 Makefile: mf
 
