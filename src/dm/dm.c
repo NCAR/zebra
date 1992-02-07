9f554982a120e7c6a3ce899113b14739aed728bc
Jon Corbet <corbet@eol.ucar.edu>
1992-02-07 21:02:24 +0000

    Moved a lot of config stuff out to dm_config.c.  Also fixed the obnoxious 'i core dump if you give me a nonexistent config file' bug.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@1319 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/dm/dm.c
+++ src/dm/dm.c
@@ -20,7 +20,9 @@
  * maintenance or updates for its software.
  */
 # include <stdio.h>
+# include <unistd.h>
 # include <varargs.h>
+# include <fcntl.h>
 # include <X11/Intrinsic.h>
 
 # include <ui.h>
@@ -30,7 +32,7 @@
 # include <timer.h>
 # include <config.h>
 # include <copyright.h>
-MAKE_RCSID ("$Id: dm.c,v 2.9 1991-12-27 21:01:45 corbet Exp $")
+MAKE_RCSID ("$Id: dm.c,v 2.10 1992-02-07 21:02:24 corbet Exp $")
 
 
 /*
@@ -44,6 +46,9 @@ stbl Bmaps;
 ButtonMap *Default_map;	/* The default button map	*/
 Display *Dm_Display;
 
+char ConfigDir[200];	/* Where to look for display configs */
+char ConfigPD[200];	/* Where to save plot descriptions	*/
+
 /*
  * Is sound enabled?
  */
@@ -52,7 +57,7 @@ static int SoundEnabled = FALSE;
 /*
  * How long and how often to sleep while creating windows.
  */
-static int SleepAfter = 4, SleepFor = 1;
+int SleepAfter = 4, SleepFor = 1;
 
 /*
  * Do we restart windows which die?
@@ -60,19 +65,15 @@ static int SleepAfter = 4, SleepFor = 1;
 static bool Restart = TRUE;
 
 
-# ifdef __STDC__
-	static void do_wbounds (char *, struct dm_rq_wbounds *);
-	int dm_shutdown (void);
-	static bool ResolveLinks (struct config *, struct ui_command *);
-	int SEChange (char *, int, int, int, SValue *, int, SValue *);
-	static void ForceRestart (char *);
-# else
-	static void do_wbounds ();
-	int dm_shutdown ();
-	static bool ResolveLinks ();
-	int SEChange ();
-	static void ForceRestart ();
-# endif
+/*
+ * Forward routines.
+ */
+static void do_wbounds FP ((char *, struct dm_rq_wbounds *));
+int dm_shutdown FP ((void));
+int SEChange FP ((char *, int, int, int, SValue *, int, SValue *));
+static void ForceRestart FP ((char *));
+static int dm_dispatcher FP ((int, struct ui_command *));
+static int dm_msg_handler FP ((Message *));
 
 
 
@@ -80,7 +81,7 @@ main (argc, argv)
 int argc;
 char **argv;
 {
-	int dm_dispatcher (), dm_msg_handler (), msg_incoming (), get_pd ();
+	int msg_incoming (), get_pd ();
 	int is_active (), type[4], pd_param (), pd_defined (), tw_cb ();
 	char loadfile[100];
 	Widget top;
@@ -128,6 +129,10 @@ char **argv;
 	usy_c_indirect (vtable, "restart", &Restart, SYMT_BOOL, 0);
 	usy_c_indirect (vtable, "sleepafter", &SleepAfter, SYMT_INT, 0);
 	usy_c_indirect (vtable, "sleepfor", &SleepFor, SYMT_INT, 0);
+	usy_c_indirect (vtable, "configdir", ConfigDir, SYMT_STRING, 200);
+	strcpy (ConfigDir, ".");
+	usy_c_indirect (vtable, "configpd", ConfigPD, SYMT_STRING, 200);
+	strcpy (ConfigPD, ".");
 	usy_daemon (vtable, "soundenabled", SOP_WRITE, SEChange, 0);
 	tty_watch (msg_get_fd (), (void (*)()) msg_incoming);
 # ifdef titan
@@ -144,8 +149,10 @@ char **argv;
 /*
  * If a file appears on the command line, open it.
  */
-	if (argc > 1)
-		ut_open_file (argv[1], TRUE);
+	ERRORCATCH
+		if (argc > 1)
+			ut_open_file (argv[1], TRUE);
+	ENDCATCH
 /*
  * Interpret commands.
  */
@@ -158,6 +165,7 @@ char **argv;
 
 
 
+static int
 dm_dispatcher (arg, cmds)
 int arg;
 struct ui_command *cmds;
@@ -268,6 +276,24 @@ struct ui_command *cmds;
 	   case DMC_RESTART:
 	   	ForceRestart (UPTR (cmds[1]));
 		break;
+	/*
+	 * Saving of configurations.
+	 */
+	   case DMC_CFGSAVE:
+	   	SaveConfig (cmds[1].uc_ctype == UTT_END ?
+					Cur_config : UPTR (cmds[1]));
+		break;
+	/*
+	 * New windows and configs.
+	 */
+	   case DMC_NEWCONFIG:
+	   	NewConfig (UPTR (cmds[1]), cmds[2].uc_ctype == UTT_END ?
+				"template" : UPTR (cmds[2]));
+		break;
+
+	   case DMC_NEWWINDOW:
+	   	NewWindow (cmds + 1);
+		break;
 
 	   default:
 	   	ui_error ("(BUG): Unknown keyword: %d\n", UKEY (*cmds));
@@ -278,6 +304,7 @@ struct ui_command *cmds;
 
 
 
+static int
 dm_msg_handler (msg)
 struct message *msg;
 /*
@@ -437,291 +464,34 @@ bool curonly;
 
 
 
-display (cmds)
-struct ui_command *cmds;
+struct config *
+LookupConfig (name)
+char *name;
 /*
- * Actually put up a given configuration.
+ * Try to find a configuration by this name.
  */
 {
-	struct config *cfg;
-	int type, win, disp_suspend ();
-	union usy_value v;
-	stbl new_table;
-	char cfg_sname[MAXNAME], *name = UPTR (*cmds);
-/*
- * If this config is already the current config, we do nothing.
- */
-	if (! strcmp (name, Cur_config))
-		return;
-	sprintf (cfg_sname, "cfg_%s", name);
+	int type;
+	SValue v;
+	char fname[200];
 /*
  * Look up this config.
  */
- 	if (! usy_g_symbol (Configs, name, &type, &v))
+ 	if (usy_g_symbol (Configs, name, &type, &v))
+		return ((struct config *) v.us_v_ptr);
+/*
+ * If we didn't find it, look for a file by this name in the default
+ * config directory, load it if it exists, then try again.
+ */
+	sprintf (fname, "read %s/%s", ConfigDir, name);
+	if (access (fname + 5, R_OK))
 		ui_error ("Unknown configuration: '%s'\n", name);
-	cfg = (struct config *) v.us_v_ptr;
-/*
- * If this window has linked PD's, go through and resolve them now.
- */
-	if (cfg->c_nlink > 0)
-		if (! ResolveLinks (cfg, cmds + 1))
-			return;
-/*
- * Get a new symbol table for this display config.
- */
-	new_table = usy_c_stbl (cfg_sname);
-	strcpy (Cur_config, name);
-/*
- * Go through and configure every window.
- */
-	for (win = 0; win < cfg->c_nwin; win++)
-	{
-		struct cf_window *wp = cfg->c_wins + win, *exist;
-	/*
-	 * If this is a widget window, deal with it separately.
-	 */
-	 	if (wp->cfw_flags & CF_WIDGET)
-		{
-			uw_ForceOverride (wp->cfw_name);
-			uw_SetGeometry (wp->cfw_name, wp->cfw_x, wp->cfw_y,
-				wp->cfw_dx, wp->cfw_dy);
-			uw_popup (wp->cfw_name);
-		}
-	/*
-	 * Otherwise it's a graphics window.  If it does not yet exist, we
-	 * have to create it.  The sleep is there to avoid "connection
-	 * refused" problems caused by too many processes trying to hook
-	 * into the X server at once.
-	 */
-		else if (! (exist = lookup_win (wp->cfw_name, FALSE)))
-		{
-			msg_ELog (EF_DEBUG, "Create win %s", wp->cfw_name);
-			create_win (wp);
-			if ((win % SleepAfter) == 0)
-				sleep (SleepFor);
-		}
-	/*
-	 * If it does exist, deal with the PD, and send it a new config.
-	 */
-		else
-		{
-			msg_ELog (EF_DEBUG, "Existing win %s", wp->cfw_name);
-			if (! wp->cfw_linkpar && ! wp->cfw_forcepd)
-				wp->cfw_pd = exist->cfw_pd; /* no copy! */
-			wp->cfw_win = exist->cfw_win;
-			config_win (wp);
-		}
-	/*
-	 * Add it to the new config table, and remove it from the old.
-	 */
-	 	v.us_v_ptr = (char *) wp;
-		usy_s_symbol (new_table, wp->cfw_name, SYMT_POINTER, &v);
-		usy_z_symbol (Current, wp->cfw_name);
-	}
-/*
- * If there are any entries left in the current table, they represent
- * windows which must be suspended.  Do that, then get rid of the old table.
- */
-	usy_traverse (Current, disp_suspend, 0, FALSE);
-	usy_z_stbl (Current);
-	Current = new_table;
+	ui_perform (fname);
+ 	if (usy_g_symbol (Configs, name, &type, &v))
+		return ((struct config *) v.us_v_ptr);
+	ui_error ("Unknown configuration: '%s'\n", name);
 }
 
-
-
-
-static bool
-ResolveLinks (cfg, cmds)
-struct config *cfg;
-struct ui_command *cmds;
-/*
- * Resolve all linked pd's in this config.
- */
-{
-	int i;
-	struct cf_window *win;
-/*
- * First, go through and make sure we have the right number of params.
- */
-	for (i = 0; i < cfg->c_nlink; i++)
-		if (cmds[i].uc_ctype == UTT_END)
-		{
-			msg_ELog (EF_PROBLEM,
-				"%d link parameters needed, %d given",
-				cfg->c_nlink, i - 1);
-			return (FALSE);
-		}
-	if (cmds[i].uc_ctype != UTT_END)
-		msg_ELog (EF_PROBLEM, "Too many link parameters given");
-/*
- * Go through now and resolve each one.
- */
-	for (i = 0; i < cfg->c_nwin; i++)
-	{
-		struct cf_window *linkwin = cfg->c_wins + i;
-		int link = linkwin->cfw_linkpar;
-	/*
-	 * If no link parameter in this window, no work to do.
-	 */
-	 	if (! link)
-			continue;
-	/*
-	 * Do some sanity checking.
-	 */
-	 	if (link > cfg->c_nlink)
-		{
-			msg_ELog (EF_PROBLEM, "Win %s wants too high link %d",
-				linkwin->cfw_name, link);
-			return (FALSE);
-		}
-	/*
-	 * Dig out the link window.
-	 */
-	 	if (! (win = lookup_win (UPTR (cmds[link - 1]), TRUE)))
-		{
-			msg_ELog (EF_PROBLEM, "Link to bad window '%s'",
-				UPTR (cmds[link - 1]));
-			return (FALSE);
-		}
-	/*
-	 * Link it.  Since the given win is required to be in the current
-	 * display configuration, we know that the PD has to be realized.
-	 *
-	 * 7/91 jc	Also mark the source window as needing to have
-	 *		it's PD pushed out to it next time around.
-	 */
-	 	linkwin->cfw_pd = win->cfw_pd;
-		linkwin->cfw_linksrc = win;
-		win->cfw_tmpforce = TRUE;
-	}
-	return (TRUE);
-}
-
-
-
-disp_suspend (name, type, v, junk)
-char *name;
-int type, junk;
-SValue *v;
-/*
- * Suspend this window.
- */
-{
-	struct dm_msg dmsg;
-	struct cf_window *wp = (struct cf_window *) v->us_v_ptr;
-/*
- * If it's a widget, just pop it down.
- */
-	if (wp->cfw_flags & CF_WIDGET)
-		uw_popdown (name);
-/*
- * Otherwise we have to tell it to go away.
- */
- 	else
-	{
-		dmsg.dmm_type = DM_SUSPEND;
-		msg_send (name, MT_DISPLAYMGR, FALSE, (char *) &dmsg,
-			sizeof (struct dm_msg));
-	}
-	return (TRUE);
-}
-
-
-
-
-
-create_win (win)
-struct cf_window *win;
-/*
- * Create a new window graphics proc.
- */
-{
-	union usy_value v;
-	int i;
-/*
- * Create the symbol table entry for this process.
- */
-	v.us_v_ptr = (char *) win;
-	usy_s_symbol (Windows, win->cfw_name, SYMT_POINTER, &v);
-/*
- * If this is a nongraphic window, we don't create it.  We just ping it, and
- * wait for the hello.
- */
-	win->cfw_args[0] = win->cfw_name;
-	if (win->cfw_nongraph)
-	{
-		struct dm_hello dmh;
-		dmh.dmm_type = DM_HELLO;
-		msg_send (win->cfw_name, MT_DISPLAYMGR, FALSE, &dmh,
-			sizeof (dmh));
-	}
-/*
- * Otherwise actually fire off the process.
- */
-	else if (fork () == 0)
-	{
-		/* close (0); close (1); close (2); */
-		close (msg_get_fd ());
-		execv (win->cfw_prog, win->cfw_args);
-		printf ("Unable to exec '%s'\n", win->cfw_prog);
-		perror (win->cfw_prog);
-		exit (1);
-	}
-}
-
-
-
-
-config_win (win)
-struct cf_window *win;
-/*
- * Configure this window.
- */
-{
-	struct dm_msg msg;
-	bool created = FALSE;
-/*
- * Fill in the message structure.
- */
- 	msg.dmm_type = DM_RECONFIG;
-	msg.dmm_x = win->cfw_x;
-	msg.dmm_y = win->cfw_y;
-	msg.dmm_dx = win->cfw_dx;
-	msg.dmm_dy = win->cfw_dy;
-/*
- * Ship it out.  If this is a nongraphic window, we then quit.
- */
-	msg_send (win->cfw_name, MT_DISPLAYMGR, FALSE, (char *) &msg,
-		sizeof (struct dm_msg));
-	if (win->cfw_nongraph)
-		return;
-/*
- * Dig out the plot description for this window.  Clone it so that our changes
- * do not affect the other invocations of this PD.
- */
-	if (! win->cfw_pd)
-	{
-		if (win->cfw_pd = pda_GetPD (win->cfw_desc))
-			win->cfw_pd = pd_CopyPD (win->cfw_pd);
-		else
-		{
-			msg_ELog (EF_EMERGENCY, "Window %s wants bad PD %s",
-				win->cfw_name, win->cfw_desc);
-			return;
-		}
-		created = TRUE;
-	}
-/*
- * Then ship over the PD too.
- */
-	if (win->cfw_linkpar || win->cfw_forcepd|| created ||win->cfw_tmpforce)
-		send_pd (win);
-	win->cfw_tmpforce = FALSE;
-/*
- * And the button maps.
- */
-	SendButtonMap (win);
-}
 
 
 
@@ -1480,3 +1250,4 @@ struct dm_rq_wbounds *wb;
 sendreply:
 	msg_send (from, MT_DISPLAYMGR, FALSE, &reply, sizeof (reply));
 }
+
