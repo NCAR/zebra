35cb1c78b3f6d5703f16b701998c884901cdbddd
Kris Gould <kris@eol.ucar.edu>
1992-03-04 22:13:52 +0000

    Added the InsertWidget and AnnotWidget files.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@1337 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/gp/Makefile.cpp
+++ src/gp/Makefile.cpp
@@ -1,4 +1,4 @@
-/* $Id: Makefile.cpp,v 1.10 1992-02-19 23:56:52 barrett Exp $ */
+/* $Id: Makefile.cpp,v 1.11 1992-03-04 22:13:52 kris Exp $ */
 /*		Copyright (C) 1987,88,89,90,91 by UCAR
  *	University Corporation for Atmospheric Research
  *		   All rights reserved
@@ -40,7 +40,7 @@ OBJS =  TimeSeries.o ColorTable.o EventQueue.o LLEvent.o PlotControl.o \
 	Icons.o Skewt.o RBand.o Annotate.o XSection.o LimitWidgets.o \
 	ConstAltPlot.o Utilities.o DataMenu.o malloc.o \
 	XYGraph.o PlotPrim.o LayoutControl.o AxisControl.o Label.o \
-	XYWind.o XYCommon.o XYContour.o
+	XYWind.o XYCommon.o XYContour.o InsertWidget.o AnnotWidget.o
 # else
 OBJS =  TimeSeries.o ColorTable.o EventQueue.o LLEvent.o PlotControl.o \
 	PlotExec.o UserEvent.o GraphicsW.o Contour.o FillContour.o \
@@ -50,7 +50,7 @@ OBJS =  TimeSeries.o ColorTable.o EventQueue.o LLEvent.o PlotControl.o \
 	Icons.o Skewt.o RBand.o Annotate.o XSection.o LimitWidgets.o \
 	ConstAltPlot.o Utilities.o DataMenu.o malloc.o \
 	XYGraph.o PlotPrim.o LayoutControl.o AxisControl.o \
-	XYWind.o XYCommon.o XYContour.o
+	XYWind.o XYCommon.o XYContour.o InsertWidget.o AnnotWidget.o
 # endif
 
 all:	gp
