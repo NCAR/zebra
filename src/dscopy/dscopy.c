10201f0ef7159108fd2e41d8b8f7b71eb1ec8e2f
Jon Corbet <corbet@eol.ucar.edu>
1992-06-19 16:41:19 +0000

    Working version.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@1502 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/dscopy/dscopy.c
+++ src/dscopy/dscopy.c
@@ -19,8 +19,317 @@
  * maintenance or updates for its software.
  */
 
+# include <config.h>
 # include <defs.h>
 # include <message.h>
 # include <DataStore.h>
 
-MAKE_RCSID ("$Id: dscopy.c,v 1.1 1992-06-12 16:10:07 corbet Exp $");
+MAKE_RCSID ("$Id: dscopy.c,v 1.2 1992-06-19 16:41:19 corbet Exp $");
+
+
+
+/*
+ * Keywords.
+ */
+# define KW_SOURCE	0
+# define KW_DEST	1
+# define KW_TIME	2
+# define KW_FIELDS	3
+# define KW_GO		4
+# define KW_DIE		5
+
+
+/*
+ * Copy info.
+ */
+# define MAXFLD 40
+PlatformId Source = BadPlatform, Dest = BadPlatform;
+FieldId Fids[MAXFLD];
+int NField = 0;
+ZebTime Begin = { 0, 0 }, End = { 0, 0 };
+DataClass Class;	/* Class of data we move. */
+
+/*
+ * Forwards.
+ */
+static void	Die FP ((void));
+static int	Incoming FP ((Message *));
+static int	Dispatcher FP ((int, struct ui_command *));
+static void	Go FP ((void));
+static ZebTime	*GetTimes FP ((int *));
+static void	DoFields FP ((ZebTime *));
+
+
+
+
+
+main (argc, argv)
+int argc;
+char **argv;
+{
+	char loadfile[100], pname[100];
+	SValue v;
+/*
+ * Hook into the user interface.
+ */
+	fixdir_t ("DSCOPY_LF_DIR", LIBDIR, "dscopy.lf", loadfile, ".lf");
+	if (argc > 1)
+	{
+		ui_init (loadfile, FALSE, TRUE);
+		v.us_v_ptr = argv[1];
+		usy_s_symbol (usy_g_stbl ("ui$variable_table"), "commandfile",
+				SYMT_STRING, &v);
+	}
+	else
+		ui_init (loadfile, TRUE, FALSE);
+	SetupConfigVariables ();
+/*
+ * Hook into the message system.
+ */
+	sprintf (pname, "dscopy-%d", getpid ());
+	msg_connect (Incoming, pname);
+	ds_Initialize ();
+/*
+ * Go.
+ */
+	ui_get_command ("initial", "dscopy>", Dispatcher, 0);
+	Die ();
+}
+
+
+
+
+static void
+Die ()
+/*
+ * Time to shut down.
+ */
+{
+	ui_finish ();
+	exit (0);
+}
+
+
+
+
+
+static int
+Dispatcher (junk, cmds)
+int junk;
+struct ui_command *cmds;
+/*
+ * Deal with a UI command.
+ */
+{
+	int i;
+
+	switch (UKEY (*cmds))
+	{
+	   case KW_DIE:
+	   	Die ();
+		break;
+
+	   case KW_FIELDS:
+		cmds++;
+		for (i = 0; cmds->uc_ctype != UTT_END; i++, cmds++)
+			Fids[NField++] = F_Lookup (UPTR (*cmds));
+		break;
+
+	   case KW_TIME:
+	   	TC_UIToZt (&UDATE(cmds[1]), &Begin);
+		TC_UIToZt (&UDATE(cmds[2]), &End);
+		break;
+
+	   case KW_SOURCE:
+	   	if ((Source = ds_LookupPlatform (UPTR (cmds[1]))) ==
+				BadPlatform)
+			ui_error ("Unknown Platform %s", UPTR (cmds[1]));
+		break;
+
+	   case KW_DEST:
+	   	if ((Dest = ds_LookupPlatform (UPTR (cmds[1]))) == BadPlatform)
+			ui_error ("Unknown Platform %s", UPTR (cmds[1]));
+		break;
+
+	   case KW_GO:
+	   	Go ();
+		break;
+	}
+	return (1);
+}
+
+
+
+
+
+static void
+Go ()
+/*
+ * Actually do it.
+ */
+{
+	ZebTime *obstimes;
+	int ntime;
+/*
+ * Some checking.
+ */
+	if (Source == BadPlatform)
+		ui_error ("No source platform specified");
+	if (Dest == BadPlatform)
+		ui_error ("No destination platform specified");
+	if (Begin.zt_Sec == 0)
+		ui_error ("No begin time");
+	if (End.zt_Sec == 0)
+		ui_error ("No end time");
+/*
+ * Get the times for the source platform.
+ */
+	if ((obstimes = GetTimes (&ntime)) == 0)
+	{
+		ui_error ("No data available for source platform");
+		Die ();
+	}
+/*
+ * Figure out the fields.
+ */
+	DoFields (obstimes);
+	if (NField <= 0)
+		ui_error ("No fields!");
+/*
+ * Figure out the class of data we will be moving.
+ */
+	switch (ds_PlatformDataOrg (Source))
+	{
+	   case Org1dGrid:
+	   case Org2dGrid:
+	   case Org3dGrid: 	Class = DCC_RGrid; break;
+	   case OrgIRGrid:	Class = DCC_IRGrid; break;
+	   case OrgScalar:	Class = DCC_Scalar; break;
+	   case OrgOutline:	Class = DCC_Boundary; break;
+	   case OrgCmpImage:
+	   case OrgImage:	Class = DCC_Image; break;
+	}
+/*
+ * Do it.  Times come in descending order, so we reverse them here for the
+ * hell of it.
+ */
+	for (ntime--; ntime >= 0; ntime--)
+		CopyObservation (obstimes + ntime);
+}
+
+
+
+
+
+
+static ZebTime *
+GetTimes (ntime)
+int *ntime;
+/*
+ * Make a list of observation times.
+ */
+{
+	static ZebTime Times[500];	/* XXX ! */
+	int nt, i;
+
+	nt = ds_GetObsTimes (Source, &End, Times, 500, 0);
+	for (*ntime = 0; *ntime < nt; (*ntime)++)
+		if (TC_Less (Times[*ntime], Begin))
+			break;
+	return (*ntime ? Times : 0);
+}
+
+
+
+
+
+static void
+DoFields (t)
+ZebTime *t;
+/*
+ * Figure out the fields.
+ */
+{
+/*
+ * If they gave no fields, we do them all.
+ */
+	if (NField <= 0)
+	{
+		NField = MAXFLD;
+		ds_GetFields (Source, t, &NField, Fids);
+	}
+}
+
+
+
+
+
+static int 
+Incoming (msg)
+Message *msg;
+/*
+ * Deal with an incoming message.
+ */
+{
+	struct mh_template *tmpl = (struct mh_template *) msg->m_data;
+
+	switch (msg->m_proto)
+	{
+	   case MT_MESSAGE:
+		if (tmpl->mh_type == MH_DIE)
+			Die ();
+		else
+			msg_ELog (EF_PROBLEM, "Weird MH msg %d",tmpl->mh_type);
+		break;
+
+	   default:
+	   	msg_ELog (EF_PROBLEM, "Unknown msg proto %d", msg->m_proto);
+		break;
+	}
+	return (0);
+}
+
+
+
+
+
+CopyObservation (t)
+ZebTime *t;
+/*
+ * Copy the observation containing this time.
+ */
+{
+	static ZebTime times[1000];
+	Location locs[1000];
+	int nsample, samp;
+	char *sname = ds_PlatformName (Source);
+	DataChunk *dc;
+	char atime[40];
+
+	TC_EncodeTime (t, TC_Full, atime);
+	ui_printf ("Doing obs at %s\n", atime);
+/*
+ * Get the times available in this observation.
+ */
+	nsample = ds_GetObsSamples (Source, t, times, locs, 1000);
+	/* XXX XXX XXX */
+	if (nsample <= 0)
+	{
+		nsample = 1;
+		times[0] = *t;
+	}
+	/* XXX XXX XXX */
+/*
+ * Now just do them.
+ */
+	for (samp = 0; samp < nsample; samp++)
+	{
+		dc = ds_Fetch (Source, Class, times + samp, times + samp,
+			Fids, NField, 0, 0);
+		dc->dc_Platform = Dest;
+		if (samp == 0)
+			dc_SetGlobalAttr (dc, "copied_from", sname);
+		ds_Store (dc, samp == 0, 0, 0);
+		dc_DestroyDC (dc);
+	}
+}
