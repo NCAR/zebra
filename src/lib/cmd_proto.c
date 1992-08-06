e2206a1f7d91f4ab2e2a7de772bd53b338b26c14
Jon Corbet <corbet@eol.ucar.edu>
1992-08-06 22:22:15 +0000

    Initial revision
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@1618 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/lib/cmd_proto.c
@@ -0,0 +1,66 @@
+/*
+ * Command execution protocol code.
+ */
+
+/*		Copyright (C) 1987,88,89,90,91,92 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+
+# include <defs.h>
+# include "message.h"
+
+MAKE_RCSID ("$Id: cmd_proto.c,v 1.1 1992-08-06 22:22:14 corbet Exp $")
+
+
+static int cp_RunCommand FP ((Message *));
+
+
+void
+cp_Exec (process, command)
+char *process, *command;
+/*
+ * Send the given command to this process.
+ */
+{
+	msg_send (process, MT_COMMAND, FALSE, command, strlen (command) + 1);
+}
+
+
+
+
+void
+cp_SetupCmdProto ()
+/*
+ * Set up to execute incoming commands.
+ */
+{
+	msg_AddProtoHandler (MT_COMMAND, cp_RunCommand);
+}
+
+
+
+
+static int
+cp_RunCommand (msg)
+Message *msg;
+/*
+ * Deal with a command protocol packet.
+ */
+{
+	ui_perform (msg->m_data);
+	return (0);
+}
---
+++ src/zrun/zrun.c
@@ -0,0 +1,86 @@
+/*
+ * Run a command in a zeb process.
+ */
+/*		Copyright (C) 1987,88,89,90,91,92 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+
+# include <defs.h>
+# include "message.h"
+
+MAKE_RCSID ("$Id");
+
+
+int IncMsg (), QResp ();
+
+
+main (argc, argv)
+int argc;
+char **argv;
+{
+/*
+ * Make sure they gave us the info
+ */
+	if (argc != 3)
+	{
+		printf ("Usage: %s process command\n", argv[0]);
+		exit (1);
+	}
+/*
+ * Hook in and send the command
+ */
+	msg_connect (IncMsg, "zrun");
+	cp_Exec (argv[1], argv[2]);
+}
+
+
+
+
+int
+IncMsg (msg)
+struct message *msg;
+{
+	if (msg->m_proto != MT_MESSAGE)
+		return;
+	if (msg->m_len == 0)
+		return (1);
+	printf ("%s\n", msg->m_data);
+	return (0);
+}
+
+
+
+
+
+int
+QResp (info)
+char *info;
+/*
+ * Something came back.
+ */
+{
+	if (! info)
+		exit (0);
+	printf ("%s\n", info);
+}
+
+
+
+void
+ui_perform (x)
+char *x;
+{} /* XXX XXX XXX */
