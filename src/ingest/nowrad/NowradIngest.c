0b8152118d1d7d9a0791e515f58be5400c866acc
Gary Granger <granger@ucar.edu>
1992-09-11 06:36:16 +0000

    I guess this is the DataChunk version.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@1683 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ingest/nowrad/NowradIngest.c
+++ src/ingest/nowrad/NowradIngest.c
@@ -19,8 +19,7 @@
  * maintenance or updates for its software.
  */
 
-static char *rcsid = 
-   "$Id: NowradIngest.c,v 1.1 1992-09-11 06:34:16 granger Exp $";
+static char *rcsid = "$Id";
 
 # include <copyright.h>
 # include <unistd.h>
@@ -35,20 +34,14 @@ static char *rcsid =
 
 
 /*
- * Our platform
- */
-PlatformId	Plat;
-
-/*
  * The image and the grid to which we're mapping it
  */
-unsigned char	*Image, *Grid;
-int	Nx, Ny, GridX = 600, GridY = 400;
+unsigned char	*Image;
 
 /*
  * Image unpacking info
  */
-int	Nbytes, Prefixlen, Linelen, Xres, Yres;
+int	Prefixlen, Linelen, Xres, Yres;
 
 /*
  * Image limits in line/elem and lat/lon coordinates
@@ -57,9 +50,9 @@ int	Minelem, Maxelem, Minline, Maxline;
 float	Minlon, Maxlon, Minlat, Maxlat;
 
 /*
- * Scaling for 2 byte values
+ * Origin latitude
  */
-int	Scale_2b = 80;
+float	Origin_lat;
 
 /*
  * Useful stuff
@@ -68,49 +61,18 @@ int	Scale_2b = 80;
 # define DEG_TO_RAD(x)	((x)*0.017453292)
 # define RAD_TO_DEG(x)	((x)*57.29577951)
 # define DEG_TO_KM(x)	((x)*111.3238367) /* on a great circle */
-# define BADVAL	-999.0
 
-int	Mdays[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-/*
- * The directory to ingest from
- */
-char	*DirName;
-
-/*
- * The temporary file to use if we have to uncompress a file
- */
-# define TEMPFILE	"/tmp/nowrad"
-
-/*
- * The symbol table and filename for the list of already ingested files
- */
-stbl	DoneTable;
-# define DONEFILES	"DoneFiles"
-char	Listfile[100];
+int	Mdays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 /*
  * Prototypes
  */
-# ifdef __STDC__
-	static void	swapfour (int *, int);
-	static void	ll_limits (void);
-	static unsigned char	imageval (int, int);
-	static int	MDispatcher (struct message *);
-	static void	DoDirectory (void);
-	static void	CheckForNew (void);
-	static void	MakeDoneList (void);
-	static int	DoFile (char *, int *, int *);
-# else
-	static void	swapfour ();
-	static void	ll_limits ();
-	static unsigned char	imageval ();
-	static int	MDispatcher ();
-	static void	DoDirectory ();
-	static void	CheckForNew ();
-	static void	MakeDoneList ();
-	static int	DoFile ();
-# endif
+static void	swapfour FP ((int *, int));
+static void	ll_limits FP ((void));
+static unsigned char	imageval FP ((int, int));
+static int	MDispatcher FP ((struct message *));
+static int	DoFile FP ((char *, char *));
+static void	Die FP ((void));
 
 
 
@@ -122,13 +84,11 @@ char **argv;
 /*
  * Checking
  */
-	if (argc != 2)
+	if (argc != 4)
 	{
-		printf ("Usage: %s <directory>\n", argv[0]);
+		printf ("Usage: %s <platform> <file> <origin_lat>\n", argv[0]);
 		exit (1);
 	}
-
-	DirName = argv[1];
 /*
  * Initialize UI symbol stuff and connect to the message handler
  */
@@ -138,292 +98,114 @@ char **argv;
  * DS initialization
  */
 	ds_Initialize ();
-	if ((Plat = ds_LookupPlatform ("nowrad")) == BadPlatform)
+
+	if (sscanf (argv[3], "%f", &Origin_lat) != 1)
 	{
-		msg_ELog (EF_PROBLEM, "Bad platform 'nowrad'");
-		return (FALSE);
-	}
-/*
- * Build the list of done files
- */
-# ifdef notdef
-	sprintf (Listfile, "%s/%s", DirName, DONEFILES);
-	MakeDoneList ();
-# endif
-/*
- * Set up a periodic timer, then wait for things to happen
- */
-	tl_AddRelativeEvent (CheckForNew, 0, 0, 60 * INCFRAC);
-	msg_await ();
-}
-
-
-
-
-static void
-MakeDoneList ()
-/*
- * Build the list of already done files
- */
-{
-	char	fname[100];
-	SValue	v;
-	FILE	*fp;
-/*
- * Open up the list file
- */
-	DoneTable = usy_c_stbl ("DoneTable");
-	if ((fp = fopen (Listfile, "r")) == NULL)
-		return;
-/*
- * Go through and put each filename into the symbol table
- */
-	while (fgets (fname, 100, fp))
-	{
-	/*
-	 * Remove the trailing <CR>
-	 */
-		fname[strlen (fname) - 1] = '\0';
-	/*
-	 * Add this file to the symbol table
-	 */
-		v.us_v_int = 1;
-		usy_s_symbol (DoneTable, fname, SYMT_INT, &v);
-		msg_ELog (EF_INFO, "Added '%s' to done list", fname);
-	}
-/*
- * All done
- */
-	fclose (fp);
-}
-
-
-
-
-static void
-DoDirectory ()
-/*
- * Deal with un-ingested files in the named directory
- */
-{
-	DIR	*dirp;
-	FILE	*fp;
-	int	type, date, time;
-	SValue	v;
-	char	fullname[100];
-	struct dirent	*entry;
-/*
- * Open up the directory
- */
-	if ((dirp = opendir (DirName)) == NULL)
-	{
-		msg_ELog (EF_EMERGENCY, 
-			"Unable to open nowrad directory '%s'", DirName);
-		exit (1);
-	}
-/*
- * Open the done list file
- */
-	fp = fopen (Listfile, "a");
-/*
- * Loop through the entries
- */
-	while ((entry = readdir (dirp)) != NULL)
-	{
-		char	*fname = entry->d_name;
-
-		sprintf (fullname, "%s/%s", DirName, fname);
-	/*
-	 * Don't do ., .., or DoneFiles
-	 */
-		if (! strcmp (fname, ".") || ! strcmp (fname, "..") ||
-			! strcmp (fname, DONEFILES))
-			continue;
-	/*
-	 * Bail out if this one's been done
-	 */
-		if (usy_g_symbol (DoneTable, entry->d_name, &type, &v))
-		{
-			msg_ELog (EF_DEBUG, "Already did '%s'", fullname);
-			continue;
-		}
-	/*
-	 * Ingest this file
-	 */
-		msg_ELog (EF_DEBUG, "Doing '%s'", fullname);
-		if (! DoFile (fullname, &date, &time))
-			continue;
-	/*
-	 * Add this filename to the done list symbol table and file
-	 */
-		v.us_v_int = 1;
-		usy_s_symbol (DoneTable, entry->d_name, SYMT_INT, &v);
-
-		fprintf (fp, "%s\n", entry->d_name);
-	}
-/*
- * Close the directory and the list file
- */
-	closedir (dirp);
-	fclose (fp);
-}
-
-
-
-
-static void
-CheckForNew ()
-/*
- * See if we have a new "mdradar.Z" file and deal with it 
- */
-{
-	int	date, time;
-	char	newname[100], fullname[100];
-
-	sprintf (fullname, "%s/mdradar.Z", DirName);
-	if (access (fullname, F_OK) != 0)
-	{
-		msg_ELog (EF_DEBUG, "No mdradar file this time");
-		return;
+		msg_ELog (EF_PROBLEM, "Bad origin latitude '%s'", argv[3]);
+		Die ();
 	}
 
-	if (DoFile (fullname, &date, &time))
-	{
-		sprintf (newname, "%s/%d%04d.fl.Z", DirName, date, time / 100);
-		if (rename (fullname, newname) < 0)
-			msg_ELog (EF_PROBLEM, "Error %d renaming '%s' to '%s'",
-				errno, fullname, newname);
-	}
+	DoFile (argv[1], argv[2]);
+	Die ();
 }
 
 
 
 
 static int
-DoFile (fname, rdate, rtime)
-char	*fname;
-int	*rdate, *rtime;
+DoFile (platform, fname)
+char	*platform, *fname;
 /*
- * Ingest the named NOWRAD file.  Return TRUE and the date and time of the
- * image if we're successful.
+ * Ingest the named NOWRAD file.
  */
 {
 	FILE	*infile;
-	int	*header, *nav_cod;
-	int	i, j, line, elem, status, year, month, day, stuff[128];
-	int	one = 1, two = 2, use_temp;
+	int	header[64], nav_cod[128];
+	int	i, j, nbytes, line, elem, status, year, month, day;
+	int	hour, minute, second, nx, ny;
+	int	one = 1, two = 2;
 	float	dummy, fline, felem, lat, lon, latstep, lonstep;
-	time	t;
-	char	fldname[5], *c, lasttwo[3];
+	ZebTime	t;
 	RGrid	rg;
-	DataObject	dobj;
+	DataChunk	*dc;
 	ScaleInfo	scale;
 	Location	loc;
+	PlatformId	pid;
+	FieldId		fid;
+	unsigned char	*grid;
 /*
- * Uncompress the file if the last two characters of the 
- * filename are ".Z"
+ * Platform ID
  */
-	strcpy (lasttwo, fname + strlen(fname) - 2);
-	if (strcmp (lasttwo, ".Z") == 0)
+	if ((pid = ds_LookupPlatform (platform)) == BadPlatform)
 	{
-		sprintf (stuff, "cat %s | uncompress -c > %s", fname, 
-			TEMPFILE);
-			use_temp = TRUE;
-		system (stuff);
+		msg_ELog (EF_PROBLEM, "Bad platform '%s'", platform);
+		Die ();
 	}
+
 /*
- * Open the input file
+ * Open the file
  */
-	if (use_temp)
-	{
-	/*
-	 * Open the file and unlink now, and the file will go away 
-	 * automatically when we're done
-	 */
-		infile = fopen (TEMPFILE, "r");
-		unlink (TEMPFILE);
-	}
-	else
-		infile = fopen (fname, "r");
+	infile = fopen (fname, "r");
 
 	if (infile == 0)
 	{
 		msg_ELog (EF_PROBLEM, "Error %d opening file '%s'\n", errno,
 			fname);
-		return (FALSE);
+		Die ();
 	}
 /*
  * Read the 256 byte "area directory" header and the 512 byte
  * navigation codicil and swap bytes around in each one.  
  * NOTE: We don't swap in those portions which contain text
  */
-	header = (int *) malloc (64 * sizeof (int));
 	fread ((void *) header, 4, 64, infile);
 	swapfour (header, 20);
 	swapfour (header + 32, 19);
 
-	nav_cod = (int *) malloc (128 * sizeof (int));
 	fread ((void *) nav_cod, 4, 128, infile);
 	swapfour (nav_cod + 1, 39);
 /*
- * Verify that this is a PS (polar stereographic) image
+ * Verify that this is a RECT (rectangular projection) image
  */
-	if (strncmp (nav_cod, "PS  ", 4))
+	if (strncmp (nav_cod, "RECT", 4))
 	{
 		char	imtype[4];
 
 		strncpy (imtype, nav_cod, 4);
 		imtype[4] = '\0';
 
-		msg_ELog (EF_PROBLEM, "'%s' contains a '%s' image, not PS",
+		msg_ELog (EF_PROBLEM, "'%s' contains a '%s' image, not RECT",
 			fname, imtype);
 		fclose (infile);
-		return (FALSE);
+		Die ();
 	}
-/*
- * Image size (Nx x Ny)
- */
-	Ny = header[8];
-	Nx = header[9];
 /*
  * Resolution (# of satellite units per image unit)
  */
 	Yres = header[11];
 	Xres = header[12];
 /*
- * Image size (Nx x Ny), bytes per element and prefix length
+ * Image size (nx x ny), and prefix length
  */
-	Ny = header[8];
-	Nx = header[9];
-	Nbytes = header[10];
+	ny = header[8];
+	nx = header[9];
 	Prefixlen = header[14];
 
-	Linelen = Nx * Nbytes + Prefixlen;
+	Linelen = nx + Prefixlen;
 /*
- * Field name from header word 51 (convert to lower case and remove spaces)
+ * Data size test
  */
-	strncpy (fldname, header + 51, 4);
-	fldname[4] = '\0';
-
-	for (i = 0; i < 4; i++)
+	nbytes = header[10];
+	if (nbytes != 1)
 	{
-		c = fldname + i;
-		if (*c == ' ')
-			*c = '\0';
-		else
-			*c = tolower (*c);
+		msg_ELog (EF_EMERGENCY, "Cannot handle %d byte data!");
+		exit (1);
 	}
-/*
- * KLUGE: change "wsi " to "visr" for now, since the manually digitized radar
- * data we were getting used "visr"
- */
-	if (! strcmp (fldname, "wsi "))
-		strcpy (fldname, "visr");
 /*
  * Read the image data
  */
-	Image = (unsigned char *) malloc (Linelen * Ny);
-	fread ((void *) Image, 1, Linelen * Ny, infile);
+	Image = (unsigned char *) malloc (Linelen * ny);
+	fread ((void *) Image, 1, Linelen * ny, infile);
 /*
  * We're done with the file
  */
@@ -432,10 +214,10 @@ int	*rdate, *rtime;
  * Element and line limits
  */
 	Minline = header[5];
-	Maxline = Minline + (Ny - 1) * Yres;
+	Maxline = Minline + (ny - 1) * Yres;
 
 	Minelem = header[6];
-	Maxelem = Minelem + (Nx - 1) * Xres;
+	Maxelem = Minelem + (nx - 1) * Xres;
 /*
  * Initialize the navigation stuff
  */
@@ -444,7 +226,7 @@ int	*rdate, *rtime;
 	{
 		msg_ELog (EF_PROBLEM, 
 			"Bad navigation initialization for file '%s'", fname);
-		return (FALSE);
+		Die ();
 	}
 
 	status = nvxini_ (&two, "LL  ");
@@ -452,7 +234,7 @@ int	*rdate, *rtime;
 	{
 		msg_ELog (EF_PROBLEM, 
 			"Can't use lan/lon coords for file '%s'", fname);
-		return (FALSE);
+		Die ();
 	}
 /*
  * Get the lat/lon limits
@@ -463,24 +245,24 @@ int	*rdate, *rtime;
 /*
  * Find the lat/lon steps
  */
-	latstep = (Maxlat - Minlat) / (GridY - 1);
-	lonstep = (Maxlon - Minlon) / (GridX - 1);
+	latstep = (Maxlat - Minlat) / (ny - 1);
+	lonstep = (Maxlon - Minlon) / (nx - 1);
 /*
  * Allocate the grid
  */
-	Grid = (unsigned char *) malloc (GridX * GridY * sizeof (char));
+	grid = (unsigned char *) malloc (nx * ny * sizeof (char));
 /*
  * Loop through the points in the grid
  */
-	for (j = 0; j < GridY; j++)
+	for (j = 0; j < ny; j++)
 	{
 		if (! ((j+1) % 20))
 			msg_ELog (EF_DEBUG, "%s: line %d of %d", fname,
-				j + 1, GridY);
+				j + 1, ny);
 
 		lat = Maxlat - j * latstep;
 
-		for (i = 0; i < GridX; i++)
+		for (i = 0; i < nx; i++)
 		{
 		/*
 		 * Translate lat/lon into line and element in the image
@@ -495,21 +277,43 @@ int	*rdate, *rtime;
 		 * Assign this grid point
 		 */
 			if (status == 0)
-				Grid[GridX * j + i] = imageval (line, elem);
+				grid[nx * j + i] = imageval (line, elem);
 			else
-				Grid[GridX * j + i] = 0;
+				grid[nx * j + i] = 0;
 		}
 	}
 /*
- * Static data object initialization.
+ * Get a field ID
  */
-	dobj.do_id = Plat;
-	dobj.do_org = OrgImage;
-	dobj.do_npoint = 1;
-	dobj.do_aloc = ALLOC (Location);
-	dobj.do_nfield = 1;
-	dobj.do_fields[0] = fldname;
-	dobj.do_flags = 0;
+	F_Init ();
+	fid = F_DeclareField ("intensity", "", "");
+/*
+ * Scaling
+ */
+	scale.s_Scale = 1.0;
+	scale.s_Offset = 0.0;
+/*
+ * Location
+ */
+	loc.l_lat = Minlat;
+	loc.l_lon = -Maxlon;
+	loc.l_alt = 0.000;
+/*
+ * rgrid info
+ */
+	rg.rg_Xspacing = DEG_TO_KM (lonstep) * cos (DEG_TO_RAD (Origin_lat));
+	rg.rg_Yspacing = DEG_TO_KM (latstep);
+	rg.rg_Zspacing = 0.0;
+
+	rg.rg_nX = nx;
+	rg.rg_nY = ny;
+	rg.rg_nZ = 1;
+/*
+ * Create the data chunk
+ */
+        dc = dc_CreateDC (DCC_Image);
+        dc->dc_Platform = pid;
+        dc_ImgSetup (dc, 1, &fid, &scale);
 /*
  * Date and time (We have to convert from Julian date.  I hate that.)
  */
@@ -518,49 +322,30 @@ int	*rdate, *rtime;
 		Mdays[2] = 29;	/* February has 29 days in leap years */
 
 	day = header[3] % 1000;
-	month = 1;
+	month = 0;
 	while (day > Mdays[month])
 		day -= Mdays[month++];
 
-	t.ds_yymmdd = *rdate = year * 10000 + month * 100 + day;
-	t.ds_hhmmss = *rtime = header[4];
+	hour = header[4] / 10000;
+	minute = (header[4] / 100) % 100;
+	second = header[4] % 100;
+
+	TC_ZtAssemble (&t, year, month, day, hour, minute, second, 0);
 /*
- * Fill in the rest of our data object.
+ * Shove the image into the data chunk
  */
-	dobj.do_begin = dobj.do_end = t;
-	dobj.do_times = &t;
-
-	dobj.do_badval = BADVAL;
-/*
- * laziness KLUGE: the origin latitude is fixed for now
- */
-	rg.rg_Xspacing = DEG_TO_KM (lonstep) * cos (DEG_TO_RAD (39.71));
-	rg.rg_Yspacing = DEG_TO_KM (latstep);
-	rg.rg_Zspacing = 0.0;
-
-	rg.rg_nX = GridX;
-	rg.rg_nY = GridY;
-	rg.rg_nZ = 0;
-
-	dobj.do_desc.d_img.ri_rg = &rg;
-
-	scale.s_Scale = (Nbytes == 1) ? 1.0 : (float) Scale_2b;
-	scale.s_Offset = 0.0;
-	dobj.do_desc.d_img.ri_scale = &scale;
-
-	loc.l_lat = Minlat;
-	loc.l_lon = -Maxlon;
-	loc.l_alt = 0.000;
-	dobj.do_aloc = &loc;
-
-	dobj.do_data[0] = (float *) Grid;
-	msg_ELog (EF_INFO, "Nowrad image %dx%d deg, %.1f/%.1f", GridX, GridY,
-		GridX * lonstep, GridY * latstep);
+	dc_ImgAddImage (dc, 0, fid, &loc, &rg, &t, grid, nx * ny);
+	msg_ELog (EF_INFO, "Nowrad image %dx%d, %.1fx%.1f deg.", nx, ny,
+		nx * lonstep, ny * latstep);
 /*
  * Send it
  */
-	ds_PutData (&dobj, TRUE);
-	return (TRUE);
+	ds_Store (dc, 1, NULL, 0);
+/*
+ * Free all that memory we hogged up
+ */
+	free (Image);
+	free (grid);
 }
 
 
@@ -655,39 +440,14 @@ int	line, elem;
 	/*
 	 * Find the offset into the image
 	 */
-		pos = image_y * Linelen + Prefixlen + image_x * Nbytes;
+		pos = image_y * Linelen + Prefixlen + image_x;
 	/*
-	 * Unpack the appropriate number of bytes
+	 * Return the value
 	 */
-		switch (Nbytes)
-		{
-		    case 1:
-			return (Image[pos]);
-			break;
-		    case 2:
-		/*
-		 * 2 bytes per element.
-		 */
-			val = Image[pos + 1] << 8 | Image[pos];
-			val /= Scale_2b;
-			if (val < 256)
-				return ((unsigned char)(val));
-			else
-			{
-				msg_ELog (EF_PROBLEM, 
-					"BUG: scaled value %d too big", val);
-				return (0);
-			}
-			break;
-		    default:
-			msg_ELog (EF_EMERGENCY, 
-				"Exiting.  Cannot handle %d byte values.", 
-				Nbytes);
-			exit (1);
-		}
+		return (Image[pos]);
 	}
 	else
-		return (0);
+		return ((unsigned char) 0xff);
 }
 
 
@@ -711,3 +471,16 @@ struct message *msg;
 	}
 	return (0);
 }   	
+
+
+
+
+static void
+Die ()
+/*
+ * Finish gracefully.
+ */
+{
+	ui_finish ();
+	exit (0);
+}
