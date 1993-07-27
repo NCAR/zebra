9dfcb1e26ce18e754aaad7a1c74599227b5c99c6
Chris Burghart <burghart@ucar.edu>
1993-07-27 20:15:54 +0000

    Decimated this kluge.  I chose one reasonable definition of XDR_F_INFINITY from netcdf.h rather than having all of the system-dependent garbage hanging around.  Now it should work correctly on a lot of machines and (more importantly) not fail to compile on *any* machine.  It's a kluge for a one shot dataset, so nobody outside of RDP really needs it to work anyway.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@2655 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/gp/xdr_infinity.h
+++ src/gp/xdr_infinity.h
@@ -17,58 +17,11 @@
  */
 
 /* 
- * "$Id: xdr_infinity.h,v 1.1 1993-05-26 20:11:09 corbet Exp $ 
- * XDR infinity stuff lifted from netcdf.h.
+ * "$Id: xdr_infinity.h,v 1.2 1993-07-27 20:15:54 burghart Exp $ 
+ * One somewhat reasonable definition for infinity in XDR, lifted from 
+ * NetCDF code.  This may not be right on some machines, but it's here
+ * as a kluge anyway, and it won't stop things from compiling anywhere.
  */
 
-/*
- * XDR_F_INFINITY is a float value whose EXTERNAL (xdr)
- * represention is ieee floating infinity.
- *
- * This section shows three techniques for setting these:
- *  Direct assignment (vax, cray) - works for non IEEE machines
- *		Doesn't work when IEEE machines don't allow
- *      float or double constants whose values are infinity.
- *  Use of a union (preferred portable method) - should work on
- *      any ANSI compiler with IEEE floating point representations,
- *      modulo byte order and sizeof() considerations.
- *  Use of pointer puns - may work with many older compilers
- *      which don't allow intialization of unions.
- *      Often doesn't work with compilers which have strict
- *      alignment rules.
- */ 
-
-    /* Direct assignment. All cases should be mutually exclusive */
-
-#ifdef vax
-#	define	XDR_F_INFINITY	1.70141173e+38
-#endif /* vax */
-
-#ifdef cray
-#	define	XDR_F_INFINITY	1.797693134862313000e+308
-#endif /* cray */
-
-#ifdef notdef /* you might want to try these, on an IEEE machine */
-#	define XDR_F_INFINITY	3.40282357e+38
-#endif
-
-#ifdef __STDC__
-    /* Use of a union, assumes IEEE representation and 1 byte unsigned char */
-#  ifndef    XDR_F_INFINITY
-#    define USE_F_UNION
-     union xdr_f_union {unsigned char bb[4]; float ff;} ;
-     extern union xdr_f_union xdr_f_infs ;  /* instantiated in array.c */
-#    define  XDR_F_INFINITY    (xdr_f_infs.ff)
-#  endif /* !XDR_F_INFINITY */
-
-
-#else /* __STDC__ */
-    /* Use of a pointer pun, assumes IEEE representation, 4 byte long */
-
-#  ifndef    XDR_F_INFINITY
-#    define USE_F_LONG_PUN
-     extern long xdr_f_infinity ;  /* instantiated in array.c */
-#    define XDR_F_INFINITY *((float *)&xdr_f_infinity)
-#  endif /* !XDR_F_INFINITY */
-
-#endif /* __STDC__ */
+long xdr_f_infinity = 0x7f800000;
+# define XDR_F_INFINITY (*((float *)&xdr_f_infinity))
