ff59123671f461f175d2b7fdcc3fb5fa88186d8f
Gary Granger <granger@ucar.edu>
1993-09-02 07:43:17 +0000

    extensive usage message; two new options, -z and -o, which take advantage of new API function ds_DeleteObs
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@2777 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/dsdelete/dsdelete.c
+++ src/dsdelete/dsdelete.c
@@ -18,7 +18,7 @@
  * through use or modification of this software.  UCAR does not provide 
  * maintenance or updates for its software.
  */
-static char *rcsid = "$Id: dsdelete.c,v 2.5 1993-08-05 18:14:27 corbet Exp $";
+static char *rcsid = "$Id: dsdelete.c,v 2.6 1993-09-02 07:43:17 granger Exp $";
 
 # include "defs.h"
 # include "message.h"
@@ -30,6 +30,30 @@ static char *rcsid = "$Id: dsdelete.c,v 2.5 1993-08-05 18:14:27 corbet Exp $";
 extern char *getenv FP((char *));
 #endif
 
+#define USAGE \
+"Deletes files from the named platform.  Use with caution!\n\
+All files are deleted whose times fall before a determined cutoff time.\n\
+If 'zap-time' is a number, the cutoff time is that number of seconds prior \n\
+to the current ZEB time.  Otherwise the cutoff time is the 'zap-time' \n\
+taken as an absolute time in UI format.\n\
+   -o\tdeletes the single observation which begins at the cutoff time\n\
+   -z\tdeletes the single most recent observation\n\
+Examples: \n\
+   dsdelete -z radar                  Delete the most recent radar file\n\
+   dsdelete prof915h '(3600*24)'      Delete files 24 hrs earlier and before\n\
+   dsdelete -o gms 29-feb-93,4:15:42  Delete file containing the given time\n\
+   dsdelete test 0                    Delete all files in platform 'test'\n"
+
+
+void
+usage(prog)
+char *prog;
+{
+	printf ("Usage: %s [-o] [-z] platform zap-time\n", prog);
+	printf ("%s", USAGE);
+}
+
+
 main (argc, argv)
 int argc;
 char **argv;
@@ -37,14 +61,35 @@ char **argv;
 	PlatformId plat;
 	int type;
 	SValue v;
-	ZebTime zaptime;
-	struct parse_tree *pt;
-/*
- * Check args.
- */
-	if (argc != 3)
+	char cbuf[100];
+	ZebTime zaptime, now;
+	bool obs = FALSE, last_obs = FALSE;
+ 	struct parse_tree *pt;
+	extern char *optarg;
+	extern int optind;
+	int c;
+
+	while ((c = getopt (argc, argv, "oz")) != -1)
 	{
-		printf ("Usage: %s platform zap-time\n", argv[0]);
+		switch (c)
+		{
+		   case 'o':
+			obs = TRUE;
+			break;
+		   case 'z':
+			last_obs = TRUE;
+			break;
+		   case '?':
+			usage(argv[0]);
+			exit(1);
+		}
+	}
+/*
+ * Check args.  Still need at least a platform name.
+ */
+	if (! (optind < argc))
+	{
+		usage(argv[0]);
 		exit (1);
 	}
 /*
@@ -56,35 +101,71 @@ char **argv;
 /*
  * Figure out the params, then do the dirty work.
  */
-	if ((plat = ds_LookupPlatform (argv[1])) == BadPlatform)
+	if ((plat = ds_LookupPlatform (argv[optind])) == BadPlatform)
 	{
-		printf ("Unknown platform: '%s'\n", argv[1]);
+		printf ("Unknown platform: '%s'\n", argv[optind]);
 		exit (1);
 	}
 /*
- * See which format of zap time they give.
+ * At this point, if they want the most recent observation, then we don't
+ * need a time from the command line.
  */
-	if (! (pt = ue_parse (argv[2], 0, FALSE)))
+	if (last_obs)
 	{
-		printf ("Funky date: '%s'\n", argv[2]);
-		exit (1);
+	/*
+	 * Calculate the cutoff time by inquiring about the most recent
+	 * time available.
+	 */
+		tl_Time (&now);
+		if (! ds_DataTimes (plat, &now, 1, DsBefore, &zaptime))
+		{
+			printf ("No observations available to delete.\n");
+			exit (1);
+		}
 	}
-	ue_eval (pt, &v, &type);
-	if (type == SYMT_INT)
-	{
-		tl_Time (&zaptime);
-		zaptime.zt_Sec -= v.us_v_int;
-	}
-	else if (type == SYMT_DATE)
-		TC_UIToZt (&v.us_v_date, &zaptime);
 	else
 	{
-		printf ("Bad date: %s\n", argv[2]);
-		exit (1);
+		/*
+		 * See which format of zap time they give.
+		 */
+		++optind;
+		if (! (optind < argc))
+		{
+			printf ("Time argument missing.\n");
+			usage(argv[0]);
+			exit (1);
+		}
+		if (! (pt = ue_parse (argv[optind], 0, FALSE)))
+		{
+			printf ("Funky date: '%s'\n", argv[optind]);
+			exit (1);
+		}
+		ue_eval (pt, &v, &type);
+		if (type == SYMT_INT)
+		{
+			tl_Time (&zaptime);
+			zaptime.zt_Sec -= v.us_v_int;
+		}
+		else if (type == SYMT_DATE)
+		{
+			TC_UIToZt (&v.us_v_date, &zaptime);
+		}
+		else
+		{
+			printf ("Bad date: %s\n", argv[optind]);
+			exit (1);
+		}
 	}
-/*
- * Now do it.
- */
-	ds_DeleteData (plat, &zaptime);
+	/*
+	 * Now do it.
+	 */
+	strcpy (cbuf, "Deleting %s ");
+	TC_EncodeTime (&zaptime, TC_Full, cbuf+strlen(cbuf));
+	printf (cbuf, (obs || last_obs) ? "at" : "before");
+	printf ("\n");
+	if (obs || last_obs)
+		ds_DeleteObs (plat, &zaptime);
+	else
+		ds_DeleteData (plat, &zaptime);
 	exit (0);
 }
