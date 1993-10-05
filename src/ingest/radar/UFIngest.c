d6e3c4289695976cc1d0c3061dd11bc21d50ec3b
Chris Burghart <burghart@ucar.edu>
1993-10-05 16:15:07 +0000

    New universal format ingest program!
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@2861 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/ingest/radar/UFIngest.c
@@ -0,0 +1,612 @@
+/*
+ * Rasterize UF radar data and ship it to the data store.
+ */
+
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+
+
+# include <copyright.h>
+# include <errno.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+
+# include <config.h>
+# include <defs.h>
+# include <message.h>
+# include <DataStore.h>
+# include <ImageXfr.h>
+# include "HouseKeeping.h"
+# include "radar_ingest.h"
+# include "display.h"
+ 
+
+/*
+ * Define globals here. !!! check for compatibilty	
+ */
+int	XRes = 800, YRes = 800;
+int	XRadar = 400, YRadar = 400;
+float	AzFill = 0.6;
+float	PixScale = 5.0;		/* Pixels per kilometer		*/
+float	RadarLat = 0, RadarLon = 0;
+int	MinSweep = 25;
+int	GMTOffset = 0;
+int	NFrames = 2;		/* How many frames		*/
+int	Niceness = 0;
+int	NBeam = 0;
+bool	Project = TRUE;
+
+/*
+ * We should be able to trust sweep and volume flags in UF data
+ */
+bool	TrustSweep = TRUE;
+bool	TrustVol = TRUE;
+
+/*
+ * No thresholding by default
+ */
+bool	DoThresholding = FALSE;
+int	ThrFldOffset;
+unsigned char	ThrCounts = 0;
+
+/*
+ * Image transfer stuff
+ */
+struct _ix_desc *ShmDesc = 0;
+int	ImageSet = -1;
+
+/*
+ * Name of the platform we're ingesting
+ */
+# define PF_LEN 80
+char	PlatformName[PF_LEN];
+
+/*
+ * Who consumes our data.
+ */
+char	Consumer[200];
+char	*CArgs[20];
+int	NCArg = 0;
+bool	CSet = FALSE;
+int	CPid = 0;		/* Its process ID	*/
+
+/*
+ * Field info.
+ */
+# define MFIELD 5
+RDest		Rd[MFIELD];
+ScaleInfo	Scale[MFIELD];
+int		NField = 0, FIndex[MFIELD];
+char		*Fields[MFIELD];
+static unsigned char	*Image[MFIELD];
+
+/*
+ * Prototypes
+ */
+static int 	Dispatcher FP ((int, struct ui_command *));
+static void	Go FP ((void));
+static void	SetupIndirect FP ((void));
+static void	ClearFrames FP ((void));
+static void	Source FP ((struct ui_command *));
+static void	NewField FP ((struct ui_command *));
+static void	ThreshParams FP ((struct ui_command *));
+static void	SetConsumer FP ((struct ui_command *));
+static void	InvokeConsumer FP ((void));
+static int	MHandler FP ((Message *));
+static void	CheckMessages FP ((void));
+int		die FP ((void));
+
+
+
+
+main (argc, argv)
+int argc;
+char **argv;
+{
+	SValue v;
+	char loadfile[200];
+	char	ourname[20];
+/*
+ * Initialize.
+ */
+	sprintf (ourname, "UFIngest_%x", getpid ());
+	msg_connect (MHandler, ourname);
+	msg_DeathHandler (die);
+	fixdir ("RI_LOAD_FILE", LIBDIR, "UFIngest.lf", loadfile);
+	if (argc > 1)
+	{
+		ui_init (loadfile, FALSE, TRUE);
+		v.us_v_ptr = argv[1];
+		usy_s_symbol (usy_g_stbl ("ui$variable_table"), "commandfile",
+				SYMT_STRING, &v);
+	}
+	else
+		ui_init (loadfile, TRUE, FALSE);
+
+	ui_setup ("UFIngest", &argc, argv, 0);
+	SetupIndirect ();
+	ds_Initialize ();
+/*
+ * Time to go in to UI mode.
+ */
+	ui_get_command ("initial", "UFIngest>", Dispatcher, 0);
+	die ();
+}
+
+
+
+
+
+static void
+SetupIndirect ()
+/*
+ * Create all of the indirect variables which are used to control things.
+ */
+{
+	stbl vtable = usy_g_stbl ("ui$variable_table");
+
+	usy_c_indirect (vtable, "x_resolution", &XRes, SYMT_INT, 0);
+	usy_c_indirect (vtable, "y_resolution", &YRes, SYMT_INT, 0);
+	usy_c_indirect (vtable, "x_radar", &XRadar, SYMT_INT, 0);
+	usy_c_indirect (vtable, "y_radar", &YRadar, SYMT_INT, 0);
+	usy_c_indirect (vtable, "radar_lat", &RadarLat, SYMT_FLOAT, 0);
+	usy_c_indirect (vtable, "radar_lon", &RadarLon, SYMT_FLOAT, 0);
+	usy_c_indirect (vtable, "azimuth_fill", &AzFill, SYMT_FLOAT, 0);
+	usy_c_indirect (vtable, "pixels_per_km", &PixScale, SYMT_FLOAT, 0);
+	usy_c_indirect (vtable, "minimum_sweep", &MinSweep, SYMT_INT, 0);
+	usy_c_indirect (vtable, "gmt_offset", &GMTOffset, SYMT_INT, 0);
+	usy_c_indirect (vtable, "platform", PlatformName, SYMT_STRING, PF_LEN);
+	usy_c_indirect (vtable, "nframes", &NFrames, SYMT_INT, 0);
+	usy_c_indirect (vtable, "niceness", &Niceness, SYMT_INT, 0);
+	usy_c_indirect (vtable, "project", &Project, SYMT_BOOL, 0);
+	usy_c_indirect (vtable, "threshold", &DoThresholding, SYMT_BOOL, 0);
+}
+
+
+
+static int
+Dispatcher (junk, cmds)
+int junk;
+struct ui_command *cmds;
+/*
+ * The command dispatcher.
+ */
+{
+	switch (UKEY (*cmds))
+	{
+	/*
+	 * Time to actually do things.
+	 */
+	   case RIC_GO:
+	   	Go ();
+		break;
+	/*
+	 * Input definition.
+	 */
+	   case RIC_SOURCE:
+	   	Source (cmds + 1);
+		break;
+	/*
+	 * Fields.
+	 */
+	   case RIC_FIELD:
+	   	NewField (cmds + 1);
+		break;
+	/*
+	 * Thresholding.
+	 */
+	   case RIC_THRESHOLD:
+	   	ThreshParams (cmds + 1);
+		break;
+	/*
+	 * Consumption, conspicuous or otherwise.
+	 */
+	   case RIC_CONSUMER:
+	   	SetConsumer (cmds + 1);
+		break;
+	/*
+	 * Time to complain.
+	 */
+	   default:
+	   	msg_ELog (EF_PROBLEM, "Unknown kw %d", UKEY (*cmds));
+		break;
+	}
+	return (TRUE);
+}
+
+
+
+
+
+static void
+Source (cmds)
+struct ui_command *cmds;
+/*
+ * Here is a definition of our input source.
+ */
+{
+	if (UKEY (*cmds) == RIC_FILE)
+	{
+		FileInput (UPTR (cmds[1]));
+		msg_ELog (EF_INFO, "Ingesting file '%s'", UPTR (cmds[1]));
+	}
+	else
+		perror("FileInput");
+}
+
+
+
+
+static void
+NewField (cmds)
+struct ui_command *cmds;
+/*
+ * Add another field to the list.
+ */
+{
+	int	index = cmds[1].uc_v.us_v_int;
+	float	min = UFLOAT (cmds[2]), max = UFLOAT (cmds[3]);
+/*
+ * Make sure we don't have too many.
+ */
+	if (NField >= MFIELD)
+	{
+		msg_ELog (EF_PROBLEM, "Too many fields; dropping %s",
+			  UPTR (cmds[0]));
+		return;
+	}
+/*
+ * Now remember the stuff.
+ */
+	Fields[NField] = usy_string (UPTR (cmds[0]));
+	FIndex[NField] = index;
+	Rd[NField].rd_foffset = NField;
+/*
+ * Take the min and max limits for this field and turn them into 
+ * scaling factors such that val = (count * scale) + offset
+ */
+	Scale[NField].s_Scale = (max - min) / 255.0;
+	Scale[NField].s_Offset = min;
+	NField++;
+}
+
+
+
+
+die ()
+/*
+ * Finish gracefully.
+ */
+{
+	ui_finish ();
+	if (ShmDesc)
+	{
+	/*
+	 * Shut down our consumer (if any)
+	 */
+		if (CPid)
+		{
+			int	status = 0;
+
+			msg_send (IX_GetConsumer (ShmDesc), MT_FINISH,
+				  FALSE, &status, sizeof (status));
+		}
+	/*
+	 * Then detach from the image transfer shared memory
+	 */
+		IX_Detach (ShmDesc);
+	}
+
+	msg_ELog (EF_INFO, "Exiting.");
+	
+	exit (0);
+}
+
+
+static void
+Go ()
+/*
+ * Start really rasterizing.
+ */
+{
+	Beam	beam, UFGetBeam ();
+	int	i, nbeam = 0;
+	Housekeeping	*hk;
+/*
+ * Set up our beam input source.
+ */
+	signal (SIGINT, die);
+	SetupInput ();
+/*
+ * Definition checking.
+ */
+	if (NField <= 0)
+	{
+		msg_ELog (EF_EMERGENCY, "No fields given");
+		die ();
+	}
+/*
+ * Set up our shared memory segment.
+ */
+	if (! (ShmDesc = IX_Create (0x910425, XRes, YRes, NField, NFrames,
+				    Fields)))
+	{
+		msg_ELog (EF_EMERGENCY, "No shm segment");
+		die ();
+	}
+	IX_LockMemory (ShmDesc);
+	IX_Initialize (ShmDesc, 0xff);
+/*
+ * Invoke the consumer to pull stuff out of that segment.
+ * Pause for a bit until the consumer attaches to our image segment.
+ */
+	InvokeConsumer ();
+	if (CPid)
+	{
+		char	*consumer = IX_GetConsumer (ShmDesc);
+		
+		while (consumer[0] == '\0')
+		{
+			msg_ELog (EF_DEBUG, "Waiting for consumer attach...");
+			sleep (1);
+		}
+	}
+	
+
+/*
+ * If they have asked for a priority change, try to do it.
+ */
+	if (Niceness)
+		setpriority (PRIO_PROCESS, 0, Niceness);
+/*
+ * Origin setting.
+ */
+	cvt_Origin (RadarLat, RadarLon);
+/*
+ * Now plow through the beams.
+ */
+	while ((beam = UFGetBeam (FIndex, Scale, NField)) != NULL)
+	{
+	/*
+	 * Rasterize it.
+	 */
+		Rasterize (beam, Rd, NField, FALSE);
+		if ((++nbeam % 100) == 0)
+			CheckMessages ();
+	}
+}
+
+
+
+
+void
+OutputSweep (bt, alt, newvol, left, right, up, down, mode)
+UItime *bt;
+float alt;
+int newvol, left, right, up, down, mode;
+/*
+ * Put this sweep out.
+ */
+{
+	long systime;
+	int i;
+	RGrid rg;
+	Location loc;
+	char attr[100];
+	static bool	firstsweep = TRUE;
+/*
+ * Set newvol to true if this is the first sweep, regardless of what
+ * our caller says.
+ */
+	if (firstsweep)
+	{
+		newvol = TRUE;
+		firstsweep = FALSE;
+	}
+/*
+ * Radars tend to record in local time; make the move over to GMT now.
+ */
+	systime = TC_FccToSys (bt) + GMTOffset*3600;
+	TC_SysToFcc (systime, bt);
+/*
+ * Assemble our data object, so that we can send this stuff out to the
+ * data store.
+ */
+	rg.rg_Xspacing = rg.rg_Yspacing = 1.0 / PixScale;
+	rg.rg_nX = XRes;
+	rg.rg_nY = YRes;
+	rg.rg_nZ = rg.rg_Zspacing = 0;
+
+	cvt_ToLatLon (-XRadar / PixScale, -YRadar / PixScale, &loc.l_lat,
+			&loc.l_lon);
+	loc.l_alt = alt;
+/*
+ * Figure out attributes.
+ */
+	strcpy (attr, newvol ? "newfile," : "");
+	strcat (attr, (mode == SM_PPI) ? "radar,ppi" : "radar,sur");
+/*
+ * Ship it out.
+ */
+	msg_ELog (EF_DEBUG, "Sending image...");
+	IX_SendFrame (ShmDesc, ImageSet, bt, &rg, &loc, Scale, left, up,
+			right, down, attr);
+	ImageSet = -1;
+}
+
+
+
+int
+BeginSweep ()
+/*
+ * Get set to do a new sweep.
+ */
+{
+	int i;
+	char s[50];
+/*
+ * If we already have a sweep going, we just clear the frames and start over.
+ */
+	if (ImageSet >= 0)
+	{
+		ClearImages ();
+		return (TRUE);
+	}
+/*
+ * Otherwise we get a new set.
+ */
+	while ((ImageSet = IX_GetWriteFrame (ShmDesc, (char **) Image, 
+					     FALSE)) < 0)
+	{
+		msg_ELog (EF_DEBUG, "Waiting for image frame...");
+		sleep (1);
+	}
+
+	for (i = 0; i < NField; i++)
+		Rd[i].rd_image = Image[i];
+
+	/* ClearImages (); */
+	return (TRUE);
+}
+
+
+
+
+ClearImages ()
+{
+	int i;
+
+	for (i = 0; i < NField; i++)
+		memset (Image[i], 0xff, XRes*YRes); 
+}
+
+
+
+
+static void
+ThreshParams (cmds)
+struct ui_command *cmds;
+/*
+ * Turn on thresholding.
+ */
+{
+	ThrFldOffset = cmds[0].uc_v.us_v_int;
+	ThrCounts = (unsigned char) cmds[1].uc_v.us_v_int;
+	DoThresholding = TRUE;
+}
+
+
+
+
+static void
+SetConsumer (cmds)
+struct ui_command *cmds;
+/*
+ * Set our consumer.
+ */
+{
+	int arg;
+
+	CSet = TRUE;
+/*
+ * The first arg is always the program itself.
+ */
+	strcpy (Consumer, UPTR (*cmds));
+	CArgs[0] = Consumer;
+/*
+ * Go through and do the rest of the args.
+ */
+	for (NCArg = 1; cmds[NCArg].uc_ctype != UTT_END; NCArg++)
+		CArgs[NCArg] = usy_string (UPTR (cmds[NCArg]));
+	CArgs[NCArg] = 0;
+}
+
+
+
+
+static void
+InvokeConsumer ()
+/*
+ * Execute the consumer process.
+ */
+{
+/*
+ * If there is no consumer, let's hope they plan to fire one off themselves.
+ */
+	if (! CSet)
+	{
+		ui_warning ("No consumer process given");
+		return;
+	}
+/*
+ * Create the new process, then try to exec the consumer program in the
+ * child.
+ */
+	if ((CPid = vfork ()) == 0)
+	{
+		execvp (Consumer, CArgs);
+		/* What??? We're still here? */
+		msg_ELog (EF_EMERGENCY, "Exec of consumer %s failed err %d",
+				Consumer, errno);
+		_exit (1);
+	}
+}
+
+
+
+
+static void
+CheckMessages ()
+/*
+ * Drain our incoming message queue.
+ */
+{
+	int fd = msg_get_fd ();
+	fd_set fds;
+	static struct timeval tv = { 0, 0 };
+/*
+ * While something is pending on the message socket, dispatch it.
+ */
+	for (;;)
+	{
+		FD_ZERO (&fds);
+		FD_SET (fd, &fds);
+		if (select (fd + 1, &fds, 0, 0, &tv) <= 0)
+			return;
+		msg_incoming (fd);
+	}
+}
+
+
+
+
+static int
+MHandler (msg)
+Message *msg;
+/*
+ * Deal with messages.
+ */
+{
+	struct mh_template *tmpl = (struct mh_template *) msg->m_data;
+
+	if (msg->m_proto == MT_MESSAGE && tmpl->mh_type == MH_DIE)
+		die ();
+	msg_ELog (EF_PROBLEM, "Unknown msg proto %d", msg->m_proto);
+}
---
+++ src/ingest/radar/UFInput.c
@@ -0,0 +1,298 @@
+/*
+ * UF input module
+ */
+
+/*		Copyright (C) 1993 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+
+# include <math.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <defs.h>
+# include <message.h>
+# include <DataStore.h>
+# include "HouseKeeping.h"
+# include "radar_ingest.h"
+
+
+int	Nbeam = 0;
+
+int	Fd;
+char	*InSource;
+
+/*
+ * The beam structure we pass back.
+ */
+Beamst		Bst;
+Housekeeping	Hk;
+char		*Databuf;
+
+# define BUFLEN	16000
+
+
+void 
+FileInput (char *file)
+/*
+ * Make a note of file input. Where does this come in???
+ */
+{
+	InSource = (char *) malloc (strlen (file) + 1);
+	strcpy (InSource, file);
+}
+
+
+
+
+void
+SetupInput ()
+/*
+ * Set up the input source. Get things ready to read beam by beam.
+ */
+{
+    if ((Fd = open (InSource, O_RDONLY)) < 0)
+    {
+		msg_ELog (EF_EMERGENCY, "Error %d opening '%s'", errno, 
+			  InSource);
+		die ();
+    }
+}
+
+
+
+
+Beam 
+UFGetBeam (f_request, scale, n_request)
+int	*f_request, n_request;
+ScaleInfo	*scale;
+/*
+ * Read the next UF beam and return it in an RP7-style beam structure.
+ * Return NULL when EOF is reached.
+ *
+ * Entry:
+ *	f_request:	List of offsets to desired fields.
+ *	scale:		List of scale factors to be applied to the 
+ *			requested fields.
+ *	n_request:	Number of fields requested
+ * Exit:
+ *	The next UF beam is converted into an RP7-style beam structure 
+ *	containing only the requested fields.  The data are returned non-
+ *	interleaved.
+ */
+{
+	int	numfields = 0, i, j;
+	float	londeg, latdeg;
+	int	status, rec_len, rec_len2;
+	short	raw_rec[BUFLEN];
+	short	*fldhdr[MAXFIELD], *flddata[MAXFIELD];
+	short	*fhdr, *dp, *datahdr, uf_badval;
+	int	numgates;
+
+	Nbeam++;
+/*
+ * Leading record length
+ */
+	if ((status = read (Fd, &rec_len, 4)) != 4)
+	{
+	/*
+	 * Return NULL if we're at EOF
+	 */
+		if (status == 0)
+			return (NULL);
+	/*
+	 * Otherwise complain
+	 */
+		msg_ELog (EF_EMERGENCY, "rec_len read error %s", InSource);
+		die ();
+	}
+
+	if (rec_len > sizeof (raw_rec))
+	{
+		msg_ELog (EF_EMERGENCY, "Not enough space for %d byte record",
+			  rec_len);
+		die ();
+	}
+/*
+ * Log message once in a while
+ */
+	if (! (Nbeam % 500))
+		msg_ELog (EF_DEBUG, "%d beams read", Nbeam);
+/*
+ * The record itself
+ */
+	if (read (Fd, raw_rec, rec_len) != rec_len)
+	{
+		msg_ELog (EF_EMERGENCY, 
+			  "Error %d reading %d bytes (%dth record)", errno,
+			  rec_len, Nbeam);
+		die ();
+	}
+/*
+ * Trailing record length
+ */
+	if (read (Fd, &rec_len2, 4) != 4)
+   	{
+		msg_ELog (EF_EMERGENCY, "trailing rec_len read error %s", 
+			  InSource);
+		die ();
+	}
+
+	if (rec_len != rec_len2)
+   	{
+		msg_ELog (EF_EMERGENCY, 
+			  "rec_len mismatch: %d vs. %d (at %dth record)",
+			  rec_len, rec_len2, Nbeam);
+		die ();
+	}
+/*
+ * Get pointers to the various headers
+ */
+	datahdr = raw_rec + raw_rec[4] - 1;
+	numfields = datahdr[0];
+
+	for (i = 0; i < numfields; i++)
+		fldhdr[i] = raw_rec + datahdr[4 + 2 * i] - 1;
+/*
+ * Make sure the request list is reasonable
+ */
+	for (i = 0; i < n_request; i++)
+	{
+		if (f_request[i] >= numfields)
+		{
+			msg_ELog (EF_EMERGENCY, 
+				  "%dth field requested, only %d present",
+				  f_request[i] + 1, numfields);
+			die ();
+		}
+	}
+/*
+ * Associate our housekeeping structure with the beam structure
+ */
+	Bst.b_hk = &Hk;
+/*
+ * Stuff things into housekeeping
+ */
+	Hk.log_rec_num = raw_rec[5];
+	Hk.rec_type = 0;		/* record type 0 = data */
+	Hk.parm_per_gate = n_request;
+
+	Hk.year = raw_rec[25];		/* last two digits */
+	Hk.month = raw_rec[26];
+	Hk.day = raw_rec[27];
+	Hk.hour = raw_rec[28];
+	Hk.minute = raw_rec[29];
+	Hk.second = raw_rec[30];
+
+	Hk.azimuth = ((float) raw_rec[32] / 64.0) * DEG_TO_BIN;
+	Hk.elevation = ((float) raw_rec[33] / 64.0) * DEG_TO_BIN;
+	Hk.fixed = ((float) raw_rec[35] / 64.0) * DEG_TO_BIN;
+
+	Hk.sweep_index = raw_rec[9];
+	Hk.vol_count = raw_rec[6];
+
+	latdeg = (raw_rec[18] + raw_rec[19] / 60.0 + 
+		  (raw_rec[20] / 64.0) / 3600.0);
+	londeg = (raw_rec[21] + raw_rec[22] / 60.0 + 
+		  (raw_rec[23] / 64.0) / 3600.0);
+	Hk.latitude = latdeg * LAT_CF;
+	Hk.longitude = londeg * LON_CF;
+	Hk.altitude = raw_rec[24];
+/*
+ * Scan type (we only really care about PPI and RHI)
+ */
+	switch (raw_rec[34])
+	{
+	    case 1:
+		Hk.scan_mode = SM_PPI;
+		break;
+	    case 3:
+		Hk.scan_mode = SM_RHI;
+		break;
+	    default:
+		Hk.scan_mode = SM_CAL; /* others don't matter */
+	}
+/*
+ * Gate spacing, number of gates, and range to first gate
+ * (We use info from the first field here and assume they're all the same)
+ */
+	fhdr = fldhdr[0];
+	
+	Hk.gate_spacing = fhdr[4];		/* gate spacing (m) */
+	Hk.rhozero1 = fhdr[2];       		/* range to leading edge of */
+	Hk.rhozero2 = fhdr[3] - fhdr[4] / 2;	/* 1st gate = rhozero1 +    */
+						/* rhozero2/1000 (in km)    */
+	numgates = Hk.gates_per_beam = fhdr[5];
+/*
+ * Allocate data space
+ */
+	if (Databuf)
+		Databuf = (char *) realloc (Databuf, numfields * numgates);
+	else
+		Databuf = (char *) malloc (numfields * numgates);
+/*
+ * Chunk descriptor: first gate, number of gates, and pointer to the data.
+ */
+	Bst.b_npart = 1;
+
+	Bst.b_gdesc[0].gd_first = 1;
+	Bst.b_gdesc[0].gd_ngate = numgates;
+	Bst.b_gdesc[0].gd_data = Databuf;
+/*
+ * Do the per-field stuff
+ */
+	uf_badval = datahdr[44];
+
+	for (i = 0; i < n_request; i++)
+	{
+		float	ufscale, val;
+		int	count;
+		
+		fhdr = fldhdr[f_request[i]];
+		dp = raw_rec + fhdr[0] - 1;	/* pointer to data */
+	/*
+	 * Move the signed two byte data over to our unsigned one byte array
+	 * by converting to engineering units and then applying the user's
+	 * scale and bias.
+	 */
+		ufscale = fhdr[1];
+
+		for (j = 0; j < numgates; j++)
+		{
+			if (dp[j] != uf_badval)
+			{
+				val = dp[j] / ufscale;
+				count = nint ((val - scale[i].s_Offset) /
+					      scale[i].s_Scale);
+			}
+			else
+				count = 0xff;
+		/*
+		 * Make sure the resulting count fits into an unsigned char,
+		 * otherwise use 0xff
+		 */
+			Databuf[j + i*numgates] = (count >= 0 && count < 256) ?
+					(unsigned char) count : 0xff;
+		}
+	/*
+	 * Stash the scale and bias.
+	 */
+		Hk.parm_info[i].pi_scale = 100 * scale[i].s_Scale;
+		Hk.parm_info[i].pi_bias = 100 * scale[i].s_Offset;
+	}
+
+	return (&Bst);
+}
