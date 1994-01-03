e6b50d473427475d202e81e0e35d5c3d96653a2e
Gary Granger <granger@ucar.edu>
1994-01-03 07:21:11 +0000

    add lots of options and flexibility using new platform search functions
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@3151 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/dsdump/dsdump.c
+++ src/dsdump/dsdump.c
@@ -19,19 +19,49 @@
  * maintenance or updates for its software.
  */
 
+# include <string.h>
 # include "defs.h"
 # include "message.h"
 # include <copyright.h>
 # include "DataStore.h"
-/* # include "dsPrivate.h" */
-/* # include "dslib.h" */
 
-MAKE_RCSID ("$Id: dsdump.c,v 3.8 1993-07-01 20:13:08 granger Exp $")
+MAKE_RCSID ("$Id: dsdump.c,v 3.9 1994-01-03 07:21:11 granger Exp $")
+
+
+/*
+ * Local prototypes
+ */
+static void DumpSubplatforms FP((PlatformId pid, PlatformInfo *pi));
+static void DumpPlatform FP((PlatformId pid, PlatformInfo *pi, 
+			     bool names, bool files));
+static void PrintInfo FP((int index, DataFileInfo *dfi));
 
 
 msg_handler ()
 { }
 
+
+static void
+usage (prog)
+char *prog;
+{
+	printf("Usage: %s -h\n", prog);
+	printf("       %s [options] [-e name] [regexp ...]\n", prog);
+	printf("Lists all platforms if no regular expressions are given.\n");
+	printf("\t-h\tPrint this usage information\n");
+	printf("\t-a\tAlphabetize list for each regular expression\n");
+	printf("\t-e\tMatch the following name exactly\n");
+	printf("\t-s\tInclude subplatforms in the list\n");
+	printf("\t-c\tShow subplatforms (children) for each platform\n");
+	printf("\t-x\tExclude data files from listing\n");
+	printf("\t-n\tList platform names only\n");
+	printf("Examples:\n");
+	printf("\tAlphabetized list of all platforms: %s -a\n", prog);
+	printf("\tList 'ship' platforms, including subplatforms for each:\n");
+	printf("\t%s -c '.*ship.*'\n", prog);
+	printf("\tList radars, and the platform exactly named 'base':\n");
+	printf("\t%s radars -e base\n", prog);
+}
 
 
 
@@ -39,140 +69,235 @@ main (argc, argv)
 int argc;
 char **argv;
 {
-	int i, pid, np, nplat;
-	static char *field = "velocity";
-	ZebTime begin, end, ts[5];
-	UItime kludge;
-	Location locs[5];
-	char atime[40];
+	int i, nplat, opt;
 	PlatformInfo pi;
+	PlatformId *platforms;
+	PlatformId pid;
+	char *pattern;
+	bool sort, subs, tier, exact, first, files, names;
+	char name[20];
+	int matches;
+/*
+ * First check for the help option
+ */
+	if ((argc > 1) && (!strcmp(argv[1], "-h")))
+	{
+		usage (argv[0]);
+		exit (0);
+	}
 
-	msg_connect (msg_handler, "DSDump");
+	sprintf (name, "DSDump-%d", getpid());
+	msg_connect (msg_handler, name);
 	usy_init ();
 	if (! ds_Initialize ())
+	{
+		printf("%s: could not connect to DataStore daemon\n",argv[0]);
 		exit (1);
+	}
 /*
  * How many platforms?
  */
 	nplat = ds_GetNPlat ();
-	printf ("We have %d platforms.\n", nplat);
-	for (i = 0; i < nplat; i++)
-	{
-		ds_GetPlatInfo (i, &pi);
-		if (argc < 2 || ! strcmp (argv[1], pi.pl_Name))
+	printf ("%d platforms, total.\n", nplat);
+	matches = 0;
+	platforms = NULL;
+/*
+ * Default options: don't sort and don't include subplatforms
+ */
+	sort = FALSE;
+	subs = FALSE;
+	tier = FALSE;
+	exact = FALSE;
+	files = TRUE;
+	names = FALSE;
+/*
+ * Traverse the options, turning on options as encountered
+ */
+	opt = 1;
+	first = FALSE;	/* true once we try at least one pattern */
+	do {
+		if ((opt < argc) && (argv[opt][0] == '-'))
 		{
-			ds_LockPlatform (i);
-			dump_platform (i, &pi);
-			ds_UnlockPlatform (i);
+			if (exact)
+			{
+				printf ("%s: -e needs platform name\n",
+					argv[0]);
+				exit (2);
+			}
+			switch (argv[opt][1])
+			{
+			   case 's':
+				subs = TRUE;
+				break;
+			   case 'a':
+				sort = TRUE;
+				break;
+			   case 'c':
+				tier = TRUE;
+				break;
+			   case 'e':
+				exact = TRUE;
+				break;
+			   case 'x':
+				files = FALSE;
+				break;
+			   case 'n':
+				names = TRUE;
+				break;
+			   default:
+				printf ("%s: illegal option '%s'\n",
+					argv[0], argv[opt]);
+				usage (argv[0]);
+				exit (1);
+				break;
+			}
 		}
+		else if (exact)	/* match this name exactly */
+		{
+			pid = ds_LookupPlatform (argv[opt]);
+			if (pid == BadPlatform)
+				printf ("%s: bad platform\n", argv[opt]);
+			else
+			{
+				ds_GetPlatInfo (pid, &pi);
+				DumpPlatform (pid, &pi, names, files);
+				if (tier)
+					DumpSubplatforms (pid, &pi);
+				++matches;
+			}
+			exact = FALSE;
+			first = TRUE;
+		}
+		else
+		{
+			pattern = (opt < argc) ? (argv[opt]) : (NULL);
+			platforms = ds_GatherPlatforms (pattern, &nplat, 
+							sort, subs);
+			matches += nplat;
+			for (i = 0; i < nplat; i++)
+			{
+				ds_GetPlatInfo (platforms[i], &pi);
+				DumpPlatform (platforms[i], &pi, names, files);
+				if (tier)
+					DumpSubplatforms (platforms[i], &pi);
+			}
+			if (pattern && (nplat == 0))
+				printf ("No matches for '%s'\n", pattern);
+			if (platforms)
+				free (platforms);
+			first = TRUE;
+		}
+		++opt;
 	}
-	exit (0);
-
-
-
-
-# ifdef notdef
+	while ((opt < argc) || (! first));
 /*
- * Try a data get.
+ * Done.
  */
-	if ((pid = ds_LookupPlatform ("cp4")) == BadPlatform)
-	{
-		ui_printf ("Lookup failure on 'cp4\n");
-		exit (1);
-	}
-	ui_printf ("\ncp4 PID is %d\n", pid);
-/*
- * Get some sample info.
- */
-	kludge.ds_yymmdd = 910802;
-	kludge.ds_hhmmss = 220100;
-	TC_UIToZt (&kludge, &begin);
-	np = ds_GetObsSamples (pid, &begin, ts, locs, 5);
-	ui_printf ("ds_GetObsSamples: Got %d samples\n", np);
-	for (i = 0; i < np; i++)
-	{
-		TC_EncodeTime (ts + i, TC_Full, atime);
-		ui_printf ("\t%d: %s, alt %.2f\n", i, atime, locs[i].l_alt);
-	}
-/*
- * Obs times.
- */
-	np = ds_GetObsTimes (pid, &begin, ts, 5, 0);
-	ui_printf ("ds_GetObsTimes (no attr): Got %d times\n", np);
-	for (i = 0; i < np; i++)
-	{
-		TC_EncodeTime (ts + i, TC_Full, atime);
-		ui_printf ("\t%d: %s\n", i, atime);
-	}
-	np = ds_GetObsTimes (pid, &begin, ts, 5, "sur");
-	ui_printf ("ds_GetObsTimes (attr sur): Got %d times\n", np);
-	for (i = 0; i < np; i++)
-	{
-		TC_EncodeTime (ts + i, TC_Full, atime);
-		ui_printf ("\t%d: %s\n", i, atime);
-	}
-	exit (0);
-# endif
+	printf ((matches == 1) ? "\n1 match found.\n" :	((matches) ?
+		 "\n%d matches found.\n" : "\nNo matches found.\n"), matches);
+	exit(0);
 }
 
 
 
-# ifdef notdef
-static struct fname
-{
-	int	flag;
-	char	*name;
-} Flags[] =
-{
-	{	DPF_MOBILE,	"mobile"	},
-	{	DPF_COMPOSITE,	"composite"	},
-	{	DPF_DISCRETE,	"discrete"	},
-	{	DPF_REGULAR,	"regular"	},
-	{	DPF_SUBPLATFORM, "subplatform"	},
-	{	DPF_REMOTE,	"remote-dir"	},
-};
-
-# define NFLAG (sizeof (Flags)/sizeof (struct fname))
-
-# endif
-
-
-dump_platform (pid, pi)
+static void
+DumpPlatform (pid, pi, names, files)
 PlatformId pid;
 PlatformInfo *pi;
+bool names;		/* list names only when true */
+bool files;		/* list files if true */
 {
 	int i, index;
 	DataSrcInfo dsi;
 	DataFileInfo dfi;
-/*
- * Don't dump subplats.
- */
-	if (pi->pl_SubPlatform)
-		return;
+	char *name;
 
-	ui_printf ("\nPlatform %s, %d data sources", pi->pl_Name,
-		pi->pl_NDataSrc);
+	ds_LockPlatform (pid);
+/*
+ * Add a newline only when not listing only the names, and when listing files
+ */
+	if (!names && files)
+		ui_printf ("\n");
+	if (name = strchr(pi->pl_Name, '/'))
+		++name;
+	else
+		name = pi->pl_Name;
+	ui_printf ("Platform %s, %d data sources", name, pi->pl_NDataSrc);
 	if (pi->pl_Mobile)
 		ui_printf (" (MOBILE)");
 	ui_printf ("\n");
-/*
- * Now we need to dump out each source.
- */
-	for (i = 0; i < pi->pl_NDataSrc; i++)
+
+	if (!names)
 	{
-		ds_GetDataSource (pid, i, &dsi);
-		ui_printf (" Data source '%s', in %s, type %d\n", 
-			dsi.dsrc_Name, dsi.dsrc_Where, dsi.dsrc_Type);
-		for (index = dsi.dsrc_FFile; index > 0; index = dfi.dfi_Next)
+	/*
+	 * Now dump out each source.
+	 */
+		for (i = 0; i < pi->pl_NDataSrc; i++)
 		{
-			ds_GetFileInfo (index, &dfi);
-			PrintInfo (index, &dfi);
+			ds_GetDataSource (pid, i, &dsi);
+			ui_printf (" Data source '%s', in %s, type %d\n", 
+				   dsi.dsrc_Name, dsi.dsrc_Where, 
+				   dsi.dsrc_Type);
+			if (! files)
+				continue;
+			for (index = dsi.dsrc_FFile; index > 0; 
+			     index = dfi.dfi_Next)
+			{
+				ds_GetFileInfo (index, &dfi);
+				PrintInfo (index, &dfi);
+			}
 		}
 	}
+	ds_UnlockPlatform (pid);
 }
 
 
 
+static void
+DumpSubplatforms (pid, pi)
+PlatformId pid;
+PlatformInfo *pi;
+{
+	PlatformId *subplats;
+	PlatformInfo spi;
+	char buf[256];
+	int buflen;
+	int n, i;
+/*
+ * Request a list of subplatforms from the daemon
+ */
+	subplats = ds_LookupSubplatforms (pid, &n);
+	if (!subplats)
+		return;
+	ui_printf (" Subplatforms:\n");
+	buf[0] = '\0';
+	buflen = 0;
+	for (i = 0; i < n; ++i)
+	{
+		char *name;
+
+		ds_GetPlatInfo (subplats[i], &spi);
+		if (name = strchr(spi.pl_Name, '/'))
+			++name;
+		else
+			name = spi.pl_Name;
+		if (buflen && (buflen + strlen(name) + 3 >= 78))
+		{
+			ui_printf (" %s\n", buf);
+			buf[0] = '\0';
+			buflen = 0;
+		}
+		sprintf (buf+buflen, " '%s'", name);
+		buflen += strlen(name) + 3;
+	}
+	if (buflen)
+		ui_printf (" %s\n", buf);
+	free (subplats);
+}
+
+
+
+static void
 PrintInfo (index, dfi)
 int index;
 DataFileInfo *dfi;
@@ -195,38 +320,3 @@ DataFileInfo *dfi;
 		index, dfi->dfi_Name, abegin, aend, dfi->dfi_NSample);
 }		
 
-
-
-
-# ifdef notdef
-
-dumpchain (which, start, dlen)
-char *which;
-int start, dlen;
-/*
- * Dump out a datafile chain.
- */
-{
-	DataFile *dp;
-	char abegin[40], aend[20];
-
-	while (start)
-	{
-		dp = DFTable + start;
-	/*
-	 * Pull out the date information and encode it.
-	 */
-		TC_EncodeTime (&dp->df_begin, TC_Full, abegin);
-		TC_EncodeTime (&dp->df_end, TC_TimeOnly, aend);
-	/*
-	 * Do the print.
-	 */
-		ui_printf ("  %s%c %2d/%d-%d %s %s > %s [%hu]\n", which,
-			(dp->df_flags & DFF_Archived) ? 'A' : 'N',
-			start, dp->df_use, dp->df_rev, dp->df_name,
-			abegin, aend, dp->df_nsample);
-		start = dp->df_FLink;
-	}
-}
-
-# endif
