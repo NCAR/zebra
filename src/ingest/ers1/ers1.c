a8790096724fe1388794d21bc4ad961646b03d3b
Rebecca Sobol <sobol@eol.ucar.edu>
1994-07-13 17:56:57 +0000

    Two changes. 1. Instead of setting the time based on the first record in the file, the time is set using the first record that contains points in the area of interest. 2. Interpolation is done over several swaths rather than one swath.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@3871 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ingest/ers1/ers1.c
+++ src/ingest/ers1/ers1.c
@@ -12,7 +12,7 @@
 # include <message.h>
 # include <DataStore.h>
 
-MAKE_RCSID ("$Id: ers1.c,v 1.2 1994-06-22 19:35:15 corbet Exp $");
+MAKE_RCSID ("$Id: ers1.c,v 1.3 1994-07-13 17:56:57 sobol Exp $");
 # define DEG_TO_RAD(deg) ((deg)*0.017453292) /* Deg to Rad conv. */
 
 /*
@@ -85,8 +85,10 @@ const float BADVAL = -9999.9;
 /*
  * Forwards.
  */
-int GetRevEntry FP ((FILE *, ZebTime *, int *, int *));
-void GrabData FP ((PlatformId, int, ZebTime *, int, int));
+int GetRevEntry FP ((FILE *, ZebTime *, ZebTime *, int *, int *));
+void GrabData FP ((PlatformId, int, ZebTime *, ZebTime *, int, int, int *,
+ int *, int *));
+void StoreData FP ((PlatformId, ZebTime *, int *, int *));
 
 int Handler () {}
 
@@ -139,8 +141,8 @@ char **argv;
 {
 	PlatformId pid;
 	FILE *revfile;
-	int datafile, beginr, endr;
-	ZebTime zt;
+	int datafile, beginr, endr, npt, nbad, tnpt;
+	ZebTime zt, tmp_time;
 	float xsc, ysc;
 /*
  * Checking.
@@ -185,13 +187,33 @@ char **argv;
 /*
  * Blast.
  */
-	while (GetRevEntry (revfile, &zt, &beginr, &endr))
+	npt = 0;
+	nbad = 0;
+	while (GetRevEntry (revfile, &zt, &tmp_time, &beginr, &endr))
 	{
 		char at[60];
 		TC_EncodeTime (&zt, TC_Full, at);
 		printf ("%s %d -> %d\n", at, beginr, endr);
-		GrabData (pid, datafile, &zt, beginr, endr);
+		tnpt = 0;
+		GrabData (pid, datafile, &zt, &tmp_time, beginr, endr,
+		 &npt, &nbad, &tnpt);
+		if ((tnpt < MIN_POINTS) && (npt >= MIN_POINTS))
+		{
+			StoreData (pid, &zt, &npt, &nbad);
+			npt = 0;
+			nbad = 0;
+		}
 	}
+/*
+ * If we didn't get enough points just blow it off.
+ */
+        if (npt < MIN_POINTS)
+	{
+                printf ("-> %d points insufficient, blowing off this one\n",
+				npt);
+		exit (0);
+        }
+	StoreData (pid, &zt, &npt, &nbad);
         exit (0);
 }
 
@@ -201,9 +223,9 @@ char **argv;
 
 
 int
-GetRevEntry (fp, zt, begin, end)
+GetRevEntry (fp, zt, tmp_time, begin, end)
 FILE *fp;
-ZebTime *zt;
+ZebTime *zt, *tmp_time;
 int *begin, *end;
 /*
  * Extract needed info from the "revcat" file.
@@ -211,6 +233,7 @@ int *begin, *end;
 {
 	char revline[120];
 	SValue v;
+	int year;
 /*
  * Read the next line from the file.
  */
@@ -224,6 +247,16 @@ int *begin, *end;
 	revline[22] = '\0';
 	uit_parse_date (revline + 4, &v, TRUE);
 	TC_UIToZt (&v.us_v_date, zt);
+/*
+ * ris - 6/25/94 - what we really need is the time associated with the first 
+ *  good data point which we get from the data file.  This is documented as 
+ *  # of seconds since the first day of the year, but this was not correct
+ *  when dealing with 1993 files.  Instead we will use # of seconds since
+ *  the beginning of 1992.  So for now we will get the number of seconds
+ *  from 1/1/70 to 1/1/92 so that we can add to it later.
+ */
+	TC_ZtAssemble (tmp_time, 92, 1, 1, 0, 0, 0, 0);
+
 	*begin = atoi (revline + 80);
 	*end = atoi (revline + 87);
 }
@@ -233,19 +266,19 @@ int *begin, *end;
 
 
 void
-GrabData (pid, fd, zt, begin, end)
+GrabData (pid, fd, zt, tmp_time, begin, end, npt, nbad, tnpt)
 PlatformId pid;
 int fd, begin, end;
-ZebTime *zt;
+ZebTime *zt, *tmp_time;
+int *npt, *nbad, *tnpt;
 /*
  * Pull in a sweep from the file.
  */
 {
-	int pt, recno, npt = 0, gnx = GRID_NX, gny = GRID_NY, nbad = 0;
+	int pt, recno, time;
 	ERSRecord rec;
-        DataChunk *dc;
-        FieldId fids[2];
-	float speed, dir;
+	float speed, dir, lat, lon;
+	char at[60];
 /*
  * Move to the first record.
  */
@@ -269,8 +302,8 @@ ZebTime *zt;
                  * Convert values and see that they are in our area
                  * of interest.
                  */
-                        float lat = swap2(rec.er_lat[pt])/100.0;
-                        float lon = swap2(rec.er_lon[pt])/100.0 + 180.0;
+                        lat = swap2(rec.er_lat[pt])/100.0;
+                        lon = swap2(rec.er_lon[pt])/100.0 + 180.0;
                         if (lon > 180.0)
                                 lon -= 360.0;
                         if (lat < LAT_MIN || lat > LAT_MAX ||
@@ -278,31 +311,59 @@ ZebTime *zt;
                                 continue;
                 /*
                  * OK we got one.  Fix up the coords for bints.
+		 *  If this is the first one set up the correct zebtime.
                  */
+			if (*npt == 0)
+			{
+				time = swap4(rec.er_time); 
+				zt->zt_Sec = tmp_time->zt_Sec + time;
+				TC_EncodeTime (zt, TC_Full, at);
+				printf ("Time set to %s \n", at);
+			}
+
                         speed = swap2(rec.er_speed[pt])/100.0;
                         dir = swap2(rec.er_dir[pt])/100.0 + 180.0;
-			U[npt] = -speed * sin (DEG_TO_RAD (dir));
-			V[npt] = -speed * cos (DEG_TO_RAD (dir));
-                        Xsta[npt] = (lon - LON_MIN)/GRID_XS;
-                        Ysta[npt] = (lat - LAT_MIN)/GRID_YS;
 		/*
 		 * They have a FUNKY way of representing bad data.
 		 */
 			if (speed == 51 || dir == 510)
-				nbad++;
+			{
+				(*nbad)++;
+			}
 			else
-				npt++;
+			{
+				U[*npt] = -speed * sin (DEG_TO_RAD (dir));
+				V[*npt] = -speed * cos (DEG_TO_RAD (dir));
+                	        Xsta[*npt] = (lon - LON_MIN)/GRID_XS;
+                        	Ysta[*npt] = (lat - LAT_MIN)/GRID_YS;
+				(*npt)++;
+				(*tnpt)++;
+				if (*npt == MAXPT)
+				{
+					StoreData (pid, zt, npt, nbad);
+					*npt = 0;
+					*tnpt = 0;
+					*nbad = 0;
+				}
+			}
                 }
         }
+        return;
+}
+
+
+void
+StoreData (pid, zt, npt, nbad)
+PlatformId pid;
+ZebTime *zt;
+int *npt, *nbad;
 /*
- * If we didn't get enough points just blow it off.
+ * Store the data gathered in this sweep.
  */
-        if (npt < MIN_POINTS)
-	{
-                printf ("-> %d points insufficient, blowing off this one\n",
-				npt);
-                return;
-        }
+{
+        DataChunk *dc;
+        FieldId fids[2];
+	int gnx = GRID_NX, gny = GRID_NY;
 /*
  * Fix up a data chunk.
  */
@@ -315,16 +376,16 @@ ZebTime *zt;
 /*
  * OK we have to interpolate this thing.
  */
-	printf ("-> %d points ", npt);
-	if (nbad > 0)
-		printf ("(+ %d bad) ", nbad);
+	printf ("-> %d points ", *npt);
+	if (*nbad > 0)
+		printf ("(+ %d bad) ", *nbad);
 	printf ("in range. Be patient...");
         fflush (stdout);
-        bints_ (Grid, &gnx, &gny, Xsta, Ysta, U, Dz, Dzr, &npt,
+        bints_ (Grid, &gnx, &gny, Xsta, Ysta, U, Dz, Dzr, npt,
                         &B_IP, &B_R, &B_RMX, &B_NQD, &B_NFILT, &BADVAL);
         dc_RGAddGrid (dc, 0, fids[0], &Origin, &Rg, zt, Grid, 0);
         printf ("u..."); fflush (stdout);
-        bints_ (Grid, &gnx, &gny, Xsta, Ysta, V, Dz, Dzr, &npt,
+        bints_ (Grid, &gnx, &gny, Xsta, Ysta, V, Dz, Dzr, npt,
                         &B_IP, &B_R, &B_RMX, &B_NQD, &B_NFILT, &BADVAL);
         dc_RGAddGrid (dc, 0, fids[1], &Origin, &Rg, zt, Grid, 0);
         printf ("v.\n"); fflush (stdout);
