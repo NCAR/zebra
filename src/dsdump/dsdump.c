b665e9ede40261c27ea56b324245f1430a6d131c
Gary Granger <granger@ucar.edu>
1994-08-31 18:22:53 +0000

    add the -z and -p options for limiting dumps to recent periods of time
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@3895 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/dsdump/dsdump.c
+++ src/dsdump/dsdump.c
@@ -19,21 +19,22 @@
  * maintenance or updates for its software.
  */
 
+# include <stdio.h>
 # include <string.h>
 # include "defs.h"
 # include "message.h"
 # include <copyright.h>
 # include "DataStore.h"
 
-MAKE_RCSID ("$Id: dsdump.c,v 3.10 1994-01-03 10:22:31 granger Exp $")
+MAKE_RCSID ("$Id: dsdump.c,v 3.11 1994-08-31 18:22:53 granger Exp $")
 
 
 /*
  * Local prototypes
  */
 static void DumpSubplatforms FP((PlatformId pid, PlatformInfo *pi));
-static void DumpPlatform FP((PlatformId pid, PlatformInfo *pi, 
-			     bool names, bool files));
+static void DumpPlatform FP((PlatformId pid, PlatformInfo *pi, ZebTime *since,
+			     bool names, bool files, bool obs));
 static void PrintInfo FP((int index, DataFileInfo *dfi));
 
 
@@ -55,12 +56,62 @@ char *prog;
 	printf("\t-c\tShow subplatforms (children) for each platform\n");
 	printf("\t-x\tExclude data files from listing\n");
 	printf("\t-n\tList platform names only\n");
+	printf("\t-z\tList only the most recent observation\n");
+	printf("\t-p '<number> [days|minutes|hours]'\n");
+	printf("\t\tList data within a certain period of the current time,\n");
+	printf("\t\twhere units can be abbreviated and defaults to days.\n");
 	printf("Examples:\n");
 	printf("\tAlphabetized list of all platforms: %s -a\n", prog);
 	printf("\tList 'ship' platforms, including subplatforms for each:\n");
 	printf("\t%s -c '.*ship.*'\n", prog);
 	printf("\tList radars, and the platform exactly named 'base':\n");
 	printf("\t%s radars -e base\n", prog);
+	printf("\tShow the last two days worth of data for all platforms:\n");
+	printf("\t%s -p '2 days'\n", prog);
+}
+
+
+long
+GetPeriod (arg)
+char *arg;
+/*
+ * Parse a period string and return the number of seconds it represents.
+ */
+{
+	long result = 0;
+	int offset;
+	char *units;
+	float num;
+
+	if (arg == NULL)
+	{
+		fprintf (stderr, "missing period string\n");
+		exit (9);
+	}
+	if (sscanf (arg, "%f %n", &num, &offset) != 1)
+	{
+		fprintf (stderr, "illegal period string: '%s'\n", arg);
+		exit (3);
+	}
+	units = arg + offset;
+	if (!units[0] || !strncmp("days", units, strlen(units)))
+	{
+		result = num * 24 * 60 * 60;
+	}
+	else if (!strncmp("hours", units, strlen(units)))
+	{
+		result = num * 60 * 60;
+	}
+	else if (!strncmp("minutes", units, strlen(units)))
+	{
+		result = num * 60;
+	}
+	else
+	{
+		fprintf (stderr, "illegal period units: '%s'\n", units);
+		exit (4);
+	}
+	return (result);
 }
 
 
@@ -74,9 +125,12 @@ char **argv;
 	PlatformId *platforms;
 	PlatformId pid;
 	char *pattern;
-	bool sort, subs, tier, exact, first, files, names;
+	bool sort, subs, tier, exact, first, files, names, obs;
 	char name[20];
 	int matches;
+	long period = 0;	/* Length of time to show data for */
+	ZebTime now;
+	ZebTime since;		/* Time to dump since */
 /*
  * First check for the help option
  */
@@ -94,6 +148,10 @@ char **argv;
 		printf("%s: could not connect to DataStore daemon\n",argv[0]);
 		exit (1);
 	}
+	tl_Time (&now);
+	now.zt_MicroSec = 0;
+	since.zt_Sec = 0;	/* default: show all files */
+	since.zt_MicroSec = 0;
 /*
  * How many platforms?
  */
@@ -110,6 +168,7 @@ char **argv;
 	exact = FALSE;
 	files = TRUE;
 	names = FALSE;
+	obs = FALSE;		/* only show most recent file */
 /*
  * Traverse the options, turning on options as encountered
  */
@@ -144,6 +203,14 @@ char **argv;
 			   case 'n':
 				names = TRUE;
 				break;
+			   case 'z':
+				obs = TRUE;
+				break;
+			   case 'p':
+				period = GetPeriod (argv[++opt]);
+				since = now;
+				since.zt_Sec -= period;
+				break;
 			   default:
 				printf ("%s: illegal option '%s'\n",
 					argv[0], argv[opt]);
@@ -160,7 +227,8 @@ char **argv;
 			else
 			{
 				ds_GetPlatInfo (pid, &pi);
-				DumpPlatform (pid, &pi, names, files);
+				DumpPlatform (pid, &pi, &since, names, files,
+					      obs);
 				if (tier)
 					DumpSubplatforms (pid, &pi);
 				++matches;
@@ -177,7 +245,8 @@ char **argv;
 			for (i = 0; i < nplat; i++)
 			{
 				ds_GetPlatInfo (platforms[i], &pi);
-				DumpPlatform (platforms[i], &pi, names, files);
+				DumpPlatform (platforms[i], &pi, &since,
+					      names, files, obs);
 				if (tier)
 					DumpSubplatforms (platforms[i], &pi);
 			}
@@ -201,11 +270,13 @@ char **argv;
 
 
 static void
-DumpPlatform (pid, pi, names, files)
+DumpPlatform (pid, pi, since, names, files, obs)
 PlatformId pid;
 PlatformInfo *pi;
+ZebTime *since;		/* Time since which to dump files */
 bool names;		/* list names only when true */
 bool files;		/* list files if true */
+bool obs;		/* list only most recent file */
 {
 	int i, index;
 	DataSrcInfo dsi;
@@ -231,7 +302,8 @@ bool files;		/* list files if true */
 	if (!names)
 	{
 	/*
-	 * Now dump out each source.
+	 * Now dump out each source, quitting at the first file outside
+	 * of our period, unless period == 0.
 	 */
 		for (i = 0; i < pi->pl_NDataSrc; i++)
 		{
@@ -245,7 +317,11 @@ bool files;		/* list files if true */
 			     index = dfi.dfi_Next)
 			{
 				ds_GetFileInfo (index, &dfi);
+				if (TC_Less(dfi.dfi_End, *since))
+					break;
 				PrintInfo (index, &dfi);
+				if (obs)
+					break;
 			}
 		}
 	}
