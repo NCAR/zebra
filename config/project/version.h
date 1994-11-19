57e70e619f80f36bc16741850a7cc5c08f847180
Gary Granger <granger@ucar.edu>
1994-11-19 05:58:55 +0000

    change the version from 4.0-SFCD (merged from the branch) to pre-4.1
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@4156 3f9b6c58-9310-455a-aec6-8b30269ab333

--- config/project/version.h
+++ config/project/version.h
@@ -132,14 +132,14 @@ static const char rcs_author[] = id ;
  */
 
 #ifdef notdef
-RCSID("$Id: version.h,v 1.3 1994-11-19 00:26:39 burghart Exp $")
+RCSID("$Id: version.h,v 1.4 1994-11-19 05:58:55 granger Exp $")
 #endif
 
 #if !defined(lint) && !defined(LINT) && !defined(SABER)
 
 #if __STDC__
 static const char V_sccsid[4] = { '@', '(', '#', ')' };
-static const char V_rcs_id[] = "@(#)$Id: version.h,v 1.3 1994-11-19 00:26:39 burghart Exp $";
+static const char V_rcs_id[] = "@(#)$Id: version.h,v 1.4 1994-11-19 05:58:55 granger Exp $";
 static const char V_compileid[] = 
 	"@(#)" "$Included: " __FILE__ " on " __DATE__ " at " __TIME__ " $";
 #endif /* __STDC__ */
@@ -151,7 +151,7 @@ static const char V_compileid[] =
  * well as updating the ChangeLog and this file.
  */
 static char zeb_version_id1[] = 
-"@(#)$ZebVersion: 4.0-SFCD $";
+"@(#)$ZebVersion: pre-4.1 $";
 static char zeb_version_id2[] = 
 "@(#)$ZebVersion: Research Data Program, NCAR $";
 static char zeb_version_id3[] = 
