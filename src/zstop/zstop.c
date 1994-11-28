43c7e6236d4ecb2204580a97d7019f720fb853ba
Jon Corbet <corbet@eol.ucar.edu>
1994-11-28 23:18:33 +0000

    zebstop is now zstop.
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@4187 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ src/zstop/zstop.c
@@ -0,0 +1,64 @@
+/*
+ * Message server zapper.
+ */
+static char *rcsid = "$Id: zstop.c,v 1.1 1994-11-28 23:18:33 corbet Exp $";
+
+/*		Copyright (C) 1987,88,89,90,91 by UCAR
+ *	University Corporation for Atmospheric Research
+ *		   All rights reserved
+ *
+ * No part of this work covered by the copyrights herein may be reproduced
+ * or used in any form or by any means -- graphic, electronic, or mechanical,
+ * including photocopying, recording, taping, or information storage and
+ * retrieval systems -- without permission of the copyright owner.
+ * 
+ * This software and any accompanying written materials are provided "as is"
+ * without warranty of any kind.  UCAR expressly disclaims all warranties of
+ * any kind, either express or implied, including but not limited to the
+ * implied warranties of merchantibility and fitness for a particular purpose.
+ * UCAR does not indemnify any infringement of copyright, patent, or trademark
+ * through use or modification of this software.  UCAR does not provide 
+ * maintenance or updates for its software.
+ */
+# include <defs.h>
+# include <message.h>
+# include <copyright.h>
+
+int
+main (argc, argv)
+int argc;
+char **argv;
+{
+	int handler ();
+	struct mh_template tm;
+
+	if (! msg_connect (handler, "Grim reaper"))
+		exit (1);
+	tm.mh_type = MH_DIE;
+	msg_send (MSG_MGR_NAME, MT_MESSAGE, 0, &tm, sizeof (tm));
+	/* 
+	 * Verify receipt of the death notice instead of rudely exiting
+	 * right away and causing write errors in the handler
+	 */
+	while (msg_poll(5) != MSG_TIMEOUT)
+	{
+		/* wait for a shutdown message or a timeout */
+	}
+	printf ("%s: shutdown message never received.\n", argv[0]);
+	exit (1);
+}
+
+
+int
+handler (Message *msg)
+{
+	struct mh_template *tm = (struct mh_template *) msg->m_data;
+
+	if (tm->mh_type == MH_SHUTDOWN)
+	{
+		printf ("zebstop: message manager shutting down\n");
+		exit (0);
+	}
+	else
+		return (0);
+}
