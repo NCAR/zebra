a9df68cbbd8286f35366bca784c2a69cca5bd778
Gary Granger <granger@ucar.edu>
1995-04-15 00:18:29 +0000

    add proto for bm_BuildBitmaps and external ref for MenuIcon
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@4435 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/lib/bitmaps.h
+++ src/lib/bitmaps.h
@@ -1,6 +1,6 @@
 /*
  * bitmaps
- * $Id: bitmaps.h,v 1.2 1993-11-30 02:12:31 granger Exp $
+ * $Id: bitmaps.h,v 1.3 1995-04-15 00:18:29 granger Exp $
  */
 /*		Copyright (C) 1987,88,89,90,91 by UCAR
  *	University Corporation for Atmospheric Research
@@ -23,6 +23,8 @@
 #ifndef __zeb_bitmaps_h_
 #define __zeb_bitmaps_h_
 
-extern Pixmap	RightArrow, LeftArrow, ASAPButton;
+extern Pixmap	RightArrow, LeftArrow, ASAPButton, MenuIcon;
+
+void bm_BuildBitmaps FP ((Widget parent));
 
 #endif /* !__zeb_bitmaps_h_ */
