1bf8a779d96f8b7ae170515b2a848fc824548cfd
Gary Granger <granger@ucar.edu>
1995-04-15 00:25:19 +0000

    reference the copyright string variable to avoid unused warnings
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@4437 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/lib/copyright.h
+++ src/lib/copyright.h
@@ -1,7 +1,10 @@
 #ifndef __zeb_copyright_h_
 #define __zeb_copyright_h_
 
-static char *Copyright =
+static char *Copyright = (
+#if __STDC__
+0, Copyright,
+#endif
 "		Copyright (C) 1987,88,89,90,91 by UCAR\n\
 	University Corporation for Atmospheric Research\n\
 		   All rights reserved\n\
@@ -17,6 +20,6 @@ any kind, either express or implied, including but not limited to the\n\
 implied warranties of merchantibility and fitness for a particular purpose.\n\
 UCAR does not indemnify any infringement of copyright, patent, or trademark\n\
 through use or modification of this software.  UCAR does not provide \n\
-maintenance or updates for its software.\n";
+maintenance or updates for its software.\n");
 
 #endif /* !__zeb_copyright_h_ */
