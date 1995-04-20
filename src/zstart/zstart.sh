0cfba2cd488e20c39a6bf689b780495e6a226ce9
Gary Granger <granger@ucar.edu>
1995-04-20 08:10:21 +0000

    run with -f; source a .zebra file in user's home directory; source proj_env file in project directory before defaulting the path variables for all of the Zebra programs; options for starting only a datastore or only a display manager; option to exec a shell with the correct environment settings and path; use mstatus -u to tell who is already running a zebra session; see source for other info
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@4531 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/zstart/zstart.sh
+++ src/zstart/zstart.sh
@@ -1,12 +1,23 @@
-#!/bin/csh
+#!/bin/csh -f
 #
 # This is an attempt at a generalized zeb startup script.
 #
-# $Id: zstart.sh,v 1.4 1995-02-03 18:09:52 burghart Exp $
+# $Id: zstart.sh,v 1.5 1995-04-20 08:10:21 granger Exp $
 #
 # Here we do basic location of directories, set environment variables,
 # and try to hand things off to a project-specific startup file.
 #
+
+#
+# If this user has a zebra resource file, source it immediately.  That
+# way they can override settings for themselves, such as ZEB_TOPDIR,
+# ZEB_PROJDIR, and ZEB_CONFIGS.
+#
+	if ($?HOME) then
+		if (-f $HOME/.zebra) then
+			source "$HOME/.zebra"
+		endif
+	endif
 
 #
 # The base directory must be right.  We need to either fix things up to
@@ -31,19 +42,12 @@
 	endif
 
 #
-# Make pointers to all of our executables so that somebody can
-# override them if desired.
-#
-	if (! $?ZEB_MESSAGE) setenv ZEB_MESSAGE $ZEB_TOPDIR/bin/message
-	if (! $?ZEB_EVENTLOGGER) setenv ZEB_EVENTLOGGER \
-					 $ZEB_TOPDIR/bin/EventLogger
-	if (! $?ZEB_TIMER) setenv ZEB_TIMER $ZEB_TOPDIR/bin/timer
-	if (! $?ZEB_DSDAEMON) setenv ZEB_DSDAEMON $ZEB_TOPDIR/bin/dsDaemon
-	if (! $?ZEB_DM) setenv ZEB_DM $ZEB_TOPDIR/bin/dm
-#
 # Look at args
 #
 	unset projdir
+	set dmonly=0
+	set dsonly=0
+	set execshell=0
 	while ($#argv)
 		switch ($argv[1])
 		    case -preserve:
@@ -52,6 +56,15 @@
 		    case -data*:
 			setenv DATA_DIR $argv[2]
 			shift
+			breaksw
+		    case -dm:
+			set dmonly=1
+			breaksw
+		    case -ds:
+			set dsonly=1
+			breaksw
+		    case -shell:
+			set execshell=1;
 			breaksw
 		    default:
 		    	set projdir=$argv[1]
@@ -62,22 +75,22 @@
 #
 # Figure out where the project directory is.
 #
-	if ( ! $#argv && ! $?projdir ) then
-		if ( -f proj_startup ) then
+	if ( ! $?projdir ) then
+		if ( -f proj_startup || -f proj_env ) then
 			set projdir=`pwd`
+		else if ( $?ZEB_PROJDIR ) then
+			set projdir="$ZEB_PROJDIR"
 		else
 			echo -n "Please enter the project directory name: "
 			set projdir=$<
 		endif
-#	else
-#		set projdir=$argv[1]
 	endif
 
 again:
 #
 # See if we can find it.
 #
-	if (-d $projdir ) then
+	if (-d "$projdir" ) then
 		; # nothing
 	else if ( -d $ZEB_TOPDIR/$projdir ) then
 		set projdir=$ZEB_TOPDIR/$projdir
@@ -92,18 +105,43 @@ again:
 	echo 'Project dir is' $projdir
 	setenv ZEB_PROJECT $projdir:t
 #
+# Give the project the chance to augment some settings, supply defaults, 
+# or replace some settings.  If it does not set a data directory, zstart
+# will prompt the user for one.
+#
+	cd $projdir
+	if (-f proj_env) source proj_env
+
+#
+# Make pointers to all of our executables so that somebody can
+# override them (such as in proj_env or .zebra) if desired.
+#
+	if (! $?ZEB_MESSAGE) setenv ZEB_MESSAGE $ZEB_TOPDIR/bin/message
+	if (! $?ZEB_EVENTLOGGER) setenv ZEB_EVENTLOGGER \
+					 $ZEB_TOPDIR/bin/EventLogger
+	if (! $?ZEB_TIMER) setenv ZEB_TIMER $ZEB_TOPDIR/bin/timer
+	if (! $?ZEB_DSDAEMON) setenv ZEB_DSDAEMON $ZEB_TOPDIR/bin/dsDaemon
+	if (! $?ZEB_DM) setenv ZEB_DM $ZEB_TOPDIR/bin/dm
+
+# 
+# Jump to the shell exec if nothing else explicitly requested
+#
+	if (! $dmonly && ! $dsonly && $execshell) goto start_shell
+
+#
 # Data directory.
 #
 ddir_again:
-	if ( ! $?DATA_DIR ) then
+	if ( ! $dmonly && ! $?DATA_DIR && ! $?DS_DAEMON_HOST ) then
 		echo -n "Where is your data directory? "
 		set ddir=$<
-		if ( ! -d $ddir ) then
+		if ( ! -d "$ddir" ) then
 			echo Directory $ddir does not exist!
 			goto ddir_again
 		endif
 		setenv DATA_DIR $ddir
 	endif
+
 #
 # Set the color map to something that will hopefully make outlines
 # show up.  Test its exit status to see if we could connect to a display.
@@ -120,18 +158,26 @@ ddir_again:
 		echo "will be used.  Then run 'xhost $HOST' on the server."
 		exit 1
 	endif		
+
+#
+# If the user told us only to do the dm startup, so be it
+#
+	if ($dmonly) goto start_dm
+
 #
 # Do everything possible to insure that we start clean.  That means either
 # stopping a running Zeb or setting ZEB_SOCKET to a unique name. 'mstatus'
-# exits with 0 status when message is running, 1 otherwise.
+# exits with 0 status when message is running, 1 otherwise.  Use the -u
+# option to get the name of the user.
 #
-	mstatus >& /dev/null
+	set someone=`mstatus -u`
 	if ($status == 0) then
-		echo "Someone is already running Zeb.  Enter"
+		echo "User $someone is already running Zeb.  Enter"
 restart_prompt:
 		echo "  1) to stop the current Zeb session and start over, or"
-		echo "  2) to run an additional, separate session of Zeb."
-		echo -n "Please enter 1, 2, or [Qq]uit: "
+		echo "  2) to run an additional, separate session of Zeb, or"
+		echo "  3) to start a display manager for this session."
+		echo -n "Please enter 1, 2, 3, or [Qq]uit: "
 		set ans=$<
 		if ( "$ans" =~ [Qq]* ) then
 		  exit 1
@@ -147,6 +193,9 @@ restart_prompt:
 		  echo "This Zeb session will use the socket $ZEB_SOCKET"
 		  echo "Enter 'setenv ZEB_SOCKET $ZEB_SOCKET' at the C-shell"
 		  echo "prompt to run Zeb programs from that shell."
+		else if ("$ans" == "3") then
+		  echo "Starting a display manager."
+		  goto start_dm
 		else
 		  goto restart_prompt
 		endif
@@ -164,11 +213,12 @@ restart_prompt:
 	endif
 #
 # Start core processes.  Message is started in foreground now; we'll wait
-# for it to background itself.
+# for it to background itself.  Use eval, though, in case someone wants to
+# redefine it (such as to a debugger or testcenter) and use an explicit '&'.
 #
 	echo 'Starting core zeb processes: '
 	echo '	message daemon'
-	$ZEB_MESSAGE
+	eval $ZEB_MESSAGE
 
 	echo -n '	event logger '
 	$ZEB_EVENTLOGGER &
@@ -177,23 +227,40 @@ restart_prompt:
 	echo -n '	timer '
 	$ZEB_TIMER &
 #
-# Now we need to run the per-project startup file.
+# Now we need to run the per-project startup file.  Things are running so
+# this file can actually start processes and zebra clients (but none that
+# need the datastore daemon, unless DS_DAEMON_HOST is set).
 #
-	cd $projdir
 	if ( -f proj_startup ) source proj_startup
 #
 # Fire off the data store daemon and the display manager.  Daemon only if
-# there is not a pointer to a remote host, however
+# there is not a pointer to a remote host, however.
 #
 	if ($?DS_DAEMON_HOST) then
 		echo '	(DS Daemon running on' $DS_DAEMON_HOST ')'
 	else
 		$ZEB_DSDAEMON ds.config &
 	endif
+#
+# If they only wanted a baseline datastore running, we quit here
+#
+	if ( $dsonly && $execshell ) goto start_shell
+	if ( $dsonly ) exit 0
 	sleep 5
+
+start_dm:
 	if ( ! $?DEFAULT_CONFIG ) setenv DEFAULT_CONFIG empty
-	$ZEB_DM dm.config
+	if ( ! $?ZEB_DM_CONFIG ) setenv ZEB_DM_CONFIG dm.config
+	eval $ZEB_DM $ZEB_DM_CONFIG
 #
 # Maybe we shut down.
 #
 	if ( ! $?PRESERVE_ZEB ) zstop
+	if ( ! $execshell ) exit 0
+#
+# Fall through on -shell option
+#
+start_shell:
+	if ( ! $?ZEB_SHELL ) setenv ZEB_SHELL 'csh -f'
+	set path=(. $path)
+	exec $ZEB_SHELL
