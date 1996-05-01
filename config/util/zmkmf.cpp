55b0bae0f3299548d5f25fe8093f8df5e509c6b4
Gary Granger <granger@ucar.edu>
1996-05-01 20:30:21 +0000

    changes to allow compiling against binary distributions: config/cf installs config files into CONFIGDIR: lib/config/cf; ConfigDir defined in Project.tmpl; config/project installs Imakefile include files into PROJCONFIGDIR: LIBDIR/config/project, ProjConfigDir defined in Project.tmpl; Project.tmpl tests for UseInstalledImake when defining Imake command and variables, so that it uses library config files instead of CONFIGSRC; zmkmf compiled with location of bindir (for imake) and correct include spec for library config dirs, so it now works without any arguments; uic compiled with UIDIR=libdir and installs ui_commands into libdir/ui; ui compile rules set ui$defs_file and ui$rdss_root according to UIDEFSFILE and UIRDSSROOT make variables; Project.tmpl sets UIDEFSFILE and UIRDSSROOT according to value of UseInstalledZebra; install public header files with InstallMultipleFiles rule; top include directory changed to $(TOP)/share to allow header files to be installed into $(TOP)/include, as with bin and lib; use SpecialObjectRule to limit command-line defines to the files which need them
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@5400 3f9b6c58-9310-455a-aec6-8b30269ab333

--- config/util/zmkmf.cpp
+++ config/util/zmkmf.cpp
@@ -3,15 +3,17 @@ XCOMM!/bin/sh
 XCOMM
 XCOMM generate a Makefile from an Imakefile from inside or outside the sources
 XCOMM 
-XCOMM $Id: zmkmf.cpp,v 1.4 1995-05-25 00:22:27 granger Exp $
+XCOMM $Id: zmkmf.cpp,v 1.5 1996-05-01 20:29:38 granger Exp $
 
 usage=\
-"usage:  $0 [-a] top_of_zeb_source current_directory [imake_opt ...]
+"usage:  $0 [-a] [top_of_zebra_source [current_directory]]
    Directory paths can be relative or absolute.  Relative paths are 
-   recommended if inside the Zeb distribution tree, absolute paths
-   should be used if outside the Zeb distribution tree.
+   recommended if inside the Zebra distribution tree, absolute paths
+   should be used if outside the Zebra distribution tree.
  Ex: zmkmf ./../.. ./ingest/ingestor"
 
+configdirspec=CONFIGDIRSPEC
+imakebin=BINDIR
 topdir=
 curdir=.
 do_all=
@@ -23,14 +25,12 @@ case "$1" in
     ;;
 esac
 
-if (test $# -ge 2) then
-    topdir=$1
-    curdir=$2
-    shift 2
-else
-    echo "$usage" 1>&2
-    exit 1
-fi
+case $# in 
+    0) ;;
+    1) topdir=$1 ;;
+    2) topdir=$1  curdir=$2 ;;
+    *) echo "$usage" 1>&2; exit 1 ;;
+esac
 
 case "$topdir" in
     -*) echo "$usage" 1>&2; exit 1 ;;
@@ -41,12 +41,18 @@ if [ -f Makefile ]; then
     mv Makefile Makefile.bak
 fi
 
-args="-I$topdir/config/project -I$topdir/config/cf -DTOPDIR=$topdir -DCURDIR=$curdir $*"
+if [ "$topdir" = "" ]; then
+    imake=$imakebin/imake
+    args="-DUseInstalledImake "$configdirspec
+else
+    imake=$topdir/config/imake/imake
+    args="-I$topdir/config/project -I$topdir/config/cf -DTOPDIR=$topdir -DCURDIR=$curdir $*"
+fi
 
-echo $topdir/config/imake/imake $args
+echo $imake $args
 case "$do_all" in
 yes)
-    $topdir/config/imake/imake $args && 
+    $imake $args && 
     echo "make Makefiles" &&
     make Makefiles &&
     echo "make includes" &&
@@ -55,6 +61,6 @@ yes)
     make depend
     ;;
 *)
-    $topdir/config/imake/imake $args
+    $imake $args
     ;;
 esac
