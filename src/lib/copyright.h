a77f5d1b88af733cd0592d8ca37e1cc1d8c96f0e
Gary Granger <granger@ucar.edu>
1996-09-05 16:37:09 +0000

    add Z_copyright inline static function to "use" Copyright variable and give applications a way to print the copyright text
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@5491 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/lib/copyright.h
+++ src/lib/copyright.h
@@ -3,11 +3,8 @@
 
 #if !defined(SABER) && !defined(lint) && !defined(LINT)
 
-static char *Copyright = (
-#if __GNUC__
-0, Copyright,
-#endif
-"		Copyright (C) 1987,88,89,90,91 by UCAR\n\
+static char *_Copyright = 
+"		Copyright (C) 1987-1996 by UCAR\n\
 	University Corporation for Atmospheric Research\n\
 		   All rights reserved\n\
 \n\
@@ -22,7 +19,20 @@ any kind, either express or implied, including but not limited to the\n\
 implied warranties of merchantibility and fitness for a particular purpose.\n\
 UCAR does not indemnify any infringement of copyright, patent, or trademark\n\
 through use or modification of this software.  UCAR does not provide \n\
-maintenance or updates for its software.\n");
+maintenance or updates for its software.\n";
+
+#if __GNUC__
+static inline const char *Z_copyright()
+{
+	return (_Copyright);
+}
+#else
+static char *Z_copyright()
+{
+	return (_Copyright);
+}
+#endif
+
 
 #endif /* !lint */
 
