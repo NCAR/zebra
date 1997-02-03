1f176a65f7980137ff0f4601c021ec17e9086a8f
Jon Corbet <corbet@eol.ucar.edu>
1997-02-03 17:21:38 +0000

    The first of the FASTEX changes coming home to roost.  The slow tape ingestor has been pretty seriously thrashed up.  It now reads a "fields" file which describes the contents of the slow tape, since NOAA likes to change that.  Derivations now look in the right place for there source fields, whereever that may be.  PT is also derived now.  Samples are batched instead of being ds_Store'd individually, which helps in ingesting data in a non-geological time frame.
    "Fastex.fields" is an example field file.
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@5773 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/ingest/st_ingest/st_ingest.c
+++ src/ingest/st_ingest/st_ingest.c
@@ -20,6 +20,8 @@
  */
 # include <stdio.h>
 # include <math.h>
+# define __EXTENSIONS__		/* Solaris string.h wants this for strtok_r */
+# include <string.h>
 # include <defs.h>
 # include <message.h>
 # include <DataStore.h>
@@ -28,20 +30,80 @@
 
 # define BFACT		10
 # define BSIZE		3440
-# define MAXFLD		100
-# define N_RAW		40	/* Number of raw fields on the tape */
-# define F_OFFSET	14	/* Offset to the first field we want */
+/* # define N_RAW		40	/* Number of raw fields on the tape */
+/* # define F_OFFSET	14	/* Offset to the first field we want */
 
-static int	Nfld;
+/*
+ * Field information.
+ */
+# define MAXP3FIELD 100
+static int	Nfld, Nrec, DoRec[MAXP3FIELD], NRead;
+static FieldId  Fids[MAXP3FIELD], RecFids[MAXP3FIELD];
+
+/*
+ * Here is a list of the fields we care about, for one reason or another,
+ * and the offset into the array of indirect field pointers.
+ */
+enum FieldIndex
+{
+	FI_Year = 0, FI_Month = 1, FI_Day = 2, FI_Hour = 3, FI_Minute = 4,
+	FI_Second = 5, FI_SelINE = 6, FI_INE1Lat = 7, FI_INE1Lon = 8,
+	FI_INE2Lat = 9, FI_INE2Lon = 10, FI_GPSLon = 11, FI_GPSLat = 12,
+	FI_RAlt1 = 13, FI_RAlt2 = 14, FI_GPSAlt = 15, FI_Wspd = 16,
+	FI_Wdir = 17, FI_UWind = 18, FI_VWind = 19, FI_ThetaE = 20,
+	FI_ThetaEE = 21, FI_Pres = 22, FI_Temp = 23, FI_DP = 24,
+	FI_LWater = 25, FI_PT = 26
+};
+# define N_INDEX 27	/* Keep this current! */
+
+struct FMap
+{
+	char *fm_Name;
+	enum FieldIndex fm_Index;
+} FieldMap[] = {
+	{ "Iyr",	FI_Year },
+	{ "Imo",	FI_Month },
+	{ "Idy", 	FI_Day },
+	{ "Ihr",	FI_Hour },
+	{ "Imn",	FI_Minute },
+	{ "Isc",	FI_Second },
+	{ "Nav",	FI_SelINE },
+	{ "fla1",	FI_INE1Lat },
+	{ "flo1",	FI_INE1Lon },
+	{ "fla2",	FI_INE2Lat },
+	{ "flo2",	FI_INE2Lon },
+	{ "fla4",	FI_GPSLat },
+	{ "flo4",	FI_GPSLon },
+	{ "ra1",	FI_RAlt1 },
+	{ "ra2",	FI_RAlt2 },
+ 	{ "ra3",	FI_GPSAlt },
+	{ "wspd",	FI_Wspd },
+	{ "wdir",	FI_Wdir },
+	{ "pres",	FI_Pres },
+	{ "tdry",	FI_Temp },
+	{ "dp",		FI_DP },
+	{ "lw",		FI_LWater },
+/*
+ * Derived fields here
+ */
+	{ "u_wind",	FI_UWind },
+	{ "v_wind",	FI_VWind },
+	{ "theta_e",	FI_ThetaE },
+	{ "theta_ee",	FI_ThetaEE },
+	{ "pt",		FI_PT },
+};
+
+static FieldId FOffsets[N_INDEX];
 
 
 /*
  * Routines.
  */
 static int	rawget FP ((FILE *, float *));
-static void	getfids FP ((FieldId *));
+static void	getfids FP ((char *));
 static void	do_derivation FP ((float *));
 int		Die FP ((void));
+static void	SaveOffset FP ((char *, int));
 
 /*
  * Globals.
@@ -51,21 +113,20 @@ main (argc, argv)
 int	argc;
 char	**argv;
 {
-	int	i;
+	int	i, rec, nsamp, first = 1;
 	int	year, month, day, hour, minute, second;
 	DataChunk	*dc;
 	Location	origin;
 	ZebTime		zt;
 	FILE		*fptr;
-	FieldId		fids[MAXFLD];
-	float		data[MAXFLD];
+	float		data[MAXP3FIELD];
 	PlatformId	pid;
 /*
  * Check arguments.
  */
-	if (argc < 3)
+	if (argc != 4)
 	{
-		printf ("Usage: st_ingest file platform");
+		printf ("Usage: st_ingest file platform field-file\n");
 		exit (1);
 	}
 /*
@@ -91,20 +152,18 @@ char	**argv;
 		exit (1);
 	}	
 /*
- * Set up fields.
+ * Set up fields and create an initial data chunk.
  */
-	getfids (fids);
+	getfids (argv[3]);
+	dc = dc_CreateDC (DCC_Scalar);
+	dc->dc_Platform = pid;
+	dc_SetScalarFields (dc, Nrec, RecFids);
+	nsamp = 0;
 /*
  * Plow through the data file.
  */
 	while (data != NULL)
 	{
-	/*
-	 * Create the data chunk.
-	 */
-		dc = dc_CreateDC (DCC_Scalar);
-		dc->dc_Platform = pid;
-		dc_SetScalarFields (dc, Nfld, fids);
 	/*
 	 * Get the raw data.
 	 */
@@ -120,53 +179,72 @@ char	**argv;
 	/*
 	 * Extract a time.
 	 */
-		year = (int) data[3];
-		month = (int) data[1];
-		day = (int) data[2];
-		hour = (int) data[4];
-		minute = (int) data[5];
-		second = (int) data[6];
+		year = (int) data[FOffsets[FI_Year]];
+		month = (int) data[FOffsets[FI_Month]];
+		day = (int) data[FOffsets[FI_Day]];
+		hour = (int) data[FOffsets[FI_Hour]];
+		minute = (int) data[FOffsets[FI_Minute]];
+		second = (int) data[FOffsets[FI_Second]];
 		TC_ZtAssemble (&zt, year, month, day, hour, minute,
 			second, 0); 
 	/*
 	 * Extract a location.
 	 */
-		if (data[7] == 1)
+		switch ((int) data[FOffsets[FI_SelINE]])
 		{
-			origin.l_lat = data[8];
-			origin.l_lon = data[9];
-			origin.l_alt = data[23];
-		}
-		else if (data[7] == 2)
-		{
-			origin.l_lat = data[10];
-			origin.l_lon = data[11];
-			origin.l_alt = data[23];
-		}
-		else if (data[7] == 4)
-		{
-			origin.l_lat = data[12];
-			origin.l_lon = data[13];
-			origin.l_alt = data[23];
-		}
-		else
-		{
-		    msg_ELog (EF_PROBLEM, "Unexpected nav value %f", data[7]);
-		    continue;
+		    case 1:
+			origin.l_lat = data[FOffsets[FI_INE1Lat]];
+			origin.l_lon = data[FOffsets[FI_INE1Lon]];
+			origin.l_alt = data[FOffsets[FI_RAlt1]];
+			break;
+
+		    case 2:
+			origin.l_lat = data[FOffsets[FI_INE2Lat]];
+			origin.l_lon = data[FOffsets[FI_INE2Lon]];
+			origin.l_alt = data[FOffsets[FI_RAlt2]];
+			break;
+
+		    case 4:
+			origin.l_lat = data[FOffsets[FI_GPSLat]];
+			origin.l_lon = data[FOffsets[FI_GPSLon]];
+			origin.l_alt = data[FOffsets[FI_GPSAlt]];
+			break;
+
+		    default:
+			msg_ELog (EF_PROBLEM, "Unexpected nav value %f",
+				    data[FOffsets[FI_SelINE]]);
+			continue;
 		}
 	/*
  	 * Do the rest of the fields.
 	 */
+		rec = 0;
 		for (i = 0; i < Nfld; i++)
-			dc_AddScalar (dc, &zt, 0, fids[i], 
-				data + F_OFFSET + i);  
-		dc_SetLoc (dc, 0, &origin);
+			if (DoRec[i])
+				dc_AddScalar (dc, &zt, nsamp, RecFids[rec++],
+						data + i);  
+		dc_SetLoc (dc, nsamp, &origin);
+		nsamp++;
 	/*
-	 * Store the data. 
+	 * If we have a good amount of data, store it and start over.
 	 */
-		ds_Store (dc, FALSE, NULL, 0);
-		dc_DestroyDC (dc);
+		if (nsamp >= 500)
+		{
+			ds_Store (dc, first, NULL, 0);
+			first = 0;
+			dc_DestroyDC (dc);
+			dc = dc_CreateDC (DCC_Scalar);
+			dc->dc_Platform = pid;
+			dc_SetScalarFields (dc, Nrec, RecFids);
+			nsamp = 0;
+		}
 	}
+/*
+ * Store any remaining data.
+ */
+	if (nsamp > 0)
+		ds_Store (dc, first, NULL, 0);
+	exit (0);
 }
 
 
@@ -182,12 +260,15 @@ Die ()
 
 
 static void
-getfids (fids)
-FieldId	*fids;
+getfids (file)
+char *file;
 /*
  * Get id's for Zeb fields.
  */
 {
+	FILE *fp;
+	char line[120];
+# ifdef GRUNGY_OLD_STUFF
 /*
  * Fields from the tape
  */
@@ -218,14 +299,105 @@ FieldId	*fids;
 	fids[24] = F_Lookup ("lw");
 	fids[25] = F_Lookup ("tas");
 	Nfld = 26;
+# endif
+/*
+ * Open the field description file.
+ */
+	if ((fp = fopen (file, "r")) == NULL)
+	{
+		perror (file);
+		exit (1);
+
+	}
+/*
+ * Now go through and read it.
+ */
+	Nfld = 0;
+	while (fgets (line, 120, fp))
+	{
+		char *cp, *units, *desc, *record;
+	/*
+	 * Ignore comments and blank lines.
+	 */
+		line[strlen (line) - 1] = '\0';		/* zorch newline */
+		if (cp = strchr (line, '#'))
+			*cp = '\0';
+		if (strlen (line) == 0)
+			continue;
+	/*
+	 * OK, split the line at the first blank, and see if we're
+	 * recording it.
+	 */
+		desc = line;
+		(void) strtok_r (line, " \t", &desc);
+		units = strtok_r (NULL, " \t", &desc);
+		record = strtok_r (NULL, " \t", &desc);
+		DoRec[Nfld] = (*record != 'n' && *record != 'N');
+	/*
+	 * If it's to be recorded, and declare the field.
+	 * Also stash aside the fid if it's one we need.
+	 */
+		if (DoRec[Nfld])
+		{
+			Fids[Nfld] = F_DeclareField (line, desc, units);
+			RecFids[Nrec++] = Fids[Nfld];
+		}
+		SaveOffset (line, Nfld);
+	/*
+	 * Are we recording this one?
+	 */
+		Nfld++;
+	}
+	NRead = Nfld;
 /*
  * Fields we derive
  */
-	fids[Nfld++] = F_Lookup ("u_wind");
-	fids[Nfld++] = F_Lookup ("v_wind");
-	fids[Nfld++] = F_Lookup ("theta_e");
-	fids[Nfld++] = F_Lookup ("theta_ee");	/* Kerry Emanuel's theta_e */
+	SaveOffset ("u_wind", Nfld);
+	DoRec[Nfld] = TRUE;
+	RecFids[Nrec++] = Fids[Nfld++] = F_DeclareField ("u_wind",
+			"U wind component", "m/s");
+	SaveOffset ("v_wind", Nfld);
+	DoRec[Nfld] = TRUE;
+	RecFids[Nrec++] = Fids[Nfld++] = F_DeclareField ("v_wind",
+			"V wind component", "m/s");
+	SaveOffset ("theta_e", Nfld);
+	DoRec[Nfld] = TRUE;
+	RecFids[Nrec++] = Fids[Nfld++] = F_DeclareField ("theta_e",
+			"Equivalent Potential Temperature", "K");
+	SaveOffset ("theta_ee", Nfld);
+	DoRec[Nfld] = TRUE;
+	RecFids[Nrec++] = Fids[Nfld++] = F_DeclareField ("theta_ee",
+			"Equivalent Potential Temperature (Emanuel)", "K");
+	SaveOffset ("pt", Nfld);
+	DoRec[Nfld] = TRUE;
+	RecFids[Nrec++] = Fids[Nfld++] = F_DeclareField ("pt",
+			"Potential Temperature", "K");
+	printf ("%d fields, read %d record %d\n", Nfld, NRead, Nrec);
 }
+
+
+
+
+static void
+SaveOffset (fname, offset)
+char *fname;
+int offset;
+/*
+ * See if we need to stash aside a copy of this FID.
+ */
+{
+	int i;
+
+	for (i = 0; i < N_INDEX; i++)
+		if (! strcmp (fname, FieldMap[i].fm_Name))
+		{
+			FOffsets[FieldMap[i].fm_Index] = offset;
+			return;
+		}
+}
+
+
+
 
 
 
@@ -258,8 +430,8 @@ float	*data;
 /* 
  * Get the variables.
  */
-	for (i = 0; i < N_RAW; i++)
-		if (fscanf (fptr, "%g", data + i) < 1)
+	for (i = 0; i < NRead; i++)
+		if (fscanf (fptr, "%f", data + i) < 1)
 		{
 			msg_ELog (EF_INFO, "End of data. Ingested %d records",
 				  count);
@@ -281,28 +453,31 @@ float	*data;
  */
 {
 	float	wspd, wdir, t, t_k, p, dp, l;
-	int	dndx = N_RAW;
 	extern float	bolton(), raf_thetae();
 /*
  * u wind and v wind
  */
-	wspd = data[31];
-	wdir = data[32];
+	wspd = data[FOffsets[FI_Wspd]];
+	wdir = data[FOffsets[FI_Wdir]];
 
-	data[dndx++] = wspd * cos (DEG_TO_RAD (270 - wdir));
-	data[dndx++] = wspd * sin (DEG_TO_RAD (270 - wdir));
+	data[FOffsets[FI_UWind]] = wspd * cos (DEG_TO_RAD (270 - wdir));
+	data[FOffsets[FI_VWind]] = wspd * sin (DEG_TO_RAD (270 - wdir));
 /*
  * equivalent potential temperature (standard calculation)
  */
-	p = data[26];
-	t = data[27];
-	dp = data[28];
+	p = data[FOffsets[FI_Pres]];
+	t = data[FOffsets[FI_Temp]];
+	dp = data[FOffsets[FI_DP]];
 
-	data[dndx++] = bolton (p, t, dp);
+	data[FOffsets[FI_ThetaE]] = bolton (p, t, dp);
 /*
  * Kerry Emanuel's equivalent potential temperature (includes liquid water 
  * component)
  */
-	l = data[38];	/* liquid water content	*/
-	data[dndx++] = raf_thetae (p, t, dp, l);
+	l = data[FOffsets[FI_LWater]];	/* liquid water content	*/
+	data[FOffsets[FI_ThetaEE]] = raf_thetae (p, t, dp, l);
+/*
+ * Potential temperature (per formula from Jorgensen, FASTEX, 1/97)
+ */
+	data[FOffsets[FI_PT]] = (t + 273.16)*pow (1000.0/p, 0.286714);
 }
