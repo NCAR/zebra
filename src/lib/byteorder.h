e9b243e57215537141426da3159fdaf877700a34
Chris Burghart <burghart@ucar.edu>
1997-02-15 02:38:44 +0000

    Added swap2() function
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@5818 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/lib/byteorder.h
+++ src/lib/byteorder.h
@@ -1,4 +1,4 @@
-/* $Id: byteorder.h,v 2.2 1996-01-12 01:09:57 granger Exp $ */
+/* $Id: byteorder.h,v 2.3 1997-02-15 02:38:44 burghart Exp $ */
 /*
  * Byte swapping, where called for.
  */
@@ -24,6 +24,21 @@ void *stuff;
 	t = cp[0]; cp[0] = cp[3]; cp[3] = t;
 	t = cp[1]; cp[1] = cp[2]; cp[2] = t;
 }
+
+static
+#  ifdef __GNUC__
+inline
+#  endif
+void swap2 (stuff)
+void *stuff;
+/*
+ * 2-byte swap.
+ */
+{
+	char *cp = (char *) stuff, t;
+
+	t = cp[0]; cp[0] = cp[1]; cp[1] = t;
+}
 # ifndef LITTLE_ENDIAN
 # define LITTLE_ENDIAN
 # endif
@@ -31,6 +46,7 @@ void *stuff;
 # else /* linux */
 
 # define swap4(stuff) (stuff)
+# define swap2(stuff) (stuff)
 # ifndef BIG_ENDIAN
 # define BIG_ENDIAN
 # endif
