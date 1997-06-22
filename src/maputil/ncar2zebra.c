03b87a11dd228bbbe9cab4077fbcd53568147133
Gary Granger <granger@ucar.edu>
1997-06-22 22:11:07 +0000

    include private copy of fixdir_t scarfed from rdss utils so that it does not need to link with rdss libraries
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@6072 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/maputil/ncar2zebra.c
+++ src/maputil/ncar2zebra.c
@@ -27,6 +27,58 @@ FILE	*MapFile;
 
 
 
+/*
+ * Scarfed from RDSS util fixdir.c to avoid needing the rdss util library.
+ */
+static void
+fixdir_t (env, def, file, dest, type)
+char *env, *def, *file, *dest, *type;
+/*
+ * Translate an environment variable, and qualify the
+ * given FILE by the result.  If there is no translation for
+ * ENV, the DEF name, if non-null, will be used.
+ * The result is put into DEST.
+ * Also, if a file lacks a type string, add it.
+ */
+{
+	char *trans, *getenv (), *strchr (), *strrchr (), *slash;
+/*
+ * First of all, look at the file name.  If it starts with a slash,
+ * we simply take it as it is.
+ */
+	if (file[0] == '/')
+		strcpy (dest, file);
+/*
+ * If the environment variable translates, use it.
+ */
+ 	else if (trans = getenv (env))
+	{
+		strcpy (dest, trans);
+		strcat (dest, "/");
+		strcat (dest, file);
+	}
+/*
+ * Failing that, copy the def if it exists, then put in the file.
+ */
+ 	else if (def)
+	{
+		strcpy (dest, def);
+		strcat (dest, "/");
+		strcat (dest, file);
+	}
+	else
+		strcpy (dest, file);
+/*
+ * Look for an extension, in a rather simple sort of way.
+ */
+ 	if ((slash = strrchr (dest, '/')) == 0)
+		slash = dest;
+	if (! strchr (slash, '.'))
+		strcat (dest, type);
+}
+
+
+
 main ()
 {
 	FILE	*projmap;
