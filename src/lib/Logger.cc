d20863dcf01038b8a076b020558b71846e4c738d
Gary Granger <granger@ucar.edu>
1998-08-27 22:35:27 +0000

    attempts to make logging more flexible and more convenient: a proxy log class registers a logging unit with the global registry and retrieves an application-configurable prototype by matched to the unit name
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@6565 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/lib/Logger.cc
+++ src/lib/Logger.cc
@@ -2,6 +2,8 @@
  * Logger implementation.
  */
 
+#include <algorithm>
+#include <vector>
 
 #include "Logger.hh"
 
@@ -15,9 +17,148 @@ const int Logger::DEVELOP = 0x20;
 const int Logger::ALL = 0x3f;
 
 
-Logger * Logger::prototype = 0;
+class MatchContext
+{
+public:
+	MatchContext (const string &s) : target(s) {}
+
+	inline bool operator() (const Logger *log) const
+	{
+		return (log->Context()).find (target) != string::npos;
+	}
+
+	const string &target;
+};
 
 
+/*
+ * A singleton class which keeps track of log units and their
+ * mask and prototype configurations.
+ */
+class LogRegistry
+{
+
+public:
+
+	LogRegistry() : fallback(0) { }
+
+	void Add (Logger *log)
+	{
+		known.push_back (log);
+	}
+
+	void Delete (Logger *log)
+	{
+		vector<Logger *>::iterator it;
+		it = find (known.begin(), known.end(), log);
+		if (it != known.end())
+			known.erase (it);
+	}
+	
+	/*
+	 * Look up this name in our configuration and return the
+	 * prototype it should use, possibly null.
+	 */
+	const Logger *Lookup (const string &s)
+	{
+		vector<Logger *>::reverse_iterator it;
+		it = find_if (protos.rbegin(), protos.rend(), MatchContext(s));
+		if (it != protos.rend())
+			return *it;
+		return fallback;
+	}
+
+	void AddProto (const Logger &proto, const string &unit)
+	{
+		protos.push_back (proto.Clone (unit));
+	}
+
+	void DefaultProto (const Logger &proto)
+	{
+		if (fallback)
+			delete fallback;
+		fallback = proto.Clone();
+	}
+
+	~LogRegistry ()
+	{
+		// Need to delete all of our prototypes.
+		vector<Logger *>::iterator it;
+		for (it = protos.begin(); it != protos.end(); ++it)
+		{
+			delete *it;
+		}
+	}
+
+private:
+
+	// Array of known logger units
+	vector<Logger *> known;
+
+	// Array of prototype patterns
+	vector<Logger *> protos;
+
+	// The default prototype
+	Logger *fallback;
+};
+
+
+
+LogRegistry *Logger::registry = 0;
+
+
+
+/*
+ * These methods allow a unit's log proxy (Sender) to register itself 
+ * and receive the configured logger.
+ */
+Logger *Logger::Register (Logger *log)
+{
+	if (! registry)
+		registry = new LogRegistry;
+	registry->Add (log);
+	const Logger *proto = registry->Lookup (log->Context());
+	if (proto)
+		return proto->Clone (log->Context());
+	return 0;
+}
+
+
+
+void Logger::Forget (Logger *log)
+{
+	if (registry)
+		registry->Delete (log);
+}
+
+
+
+/*
+ * These are the application entry points for configuring the 
+ * logging prototypes to be used for named units.
+ */
+void Logger::Prototype (const Logger &proto, const string &unit)
+{
+	if (! registry)
+		registry = new LogRegistry;
+	registry->AddProto (proto, unit);
+}
+
+
+/*
+ * Specify the default prototype.
+ */
+void Logger::Prototype (const Logger &proto)
+{
+	if (! registry)
+		registry = new LogRegistry;
+	registry->DefaultProto (proto);
+}
+
+
+
+
+#if 0
 /*
  * Return a clone of the logger prototype for the given name.
  * The default prototype is the null logger so that applications
@@ -42,4 +183,5 @@ Logger::SetPrototype (const Logger & proto)
 	}
 	prototype = proto.Clone (proto.name);
 }
+#endif
 
--- src/lib/Logger.hh
+++ src/lib/Logger.hh
@@ -1,5 +1,5 @@
 /*
- * $Id: Logger.hh,v 1.6 1998-05-15 21:47:25 granger Exp $
+ * $Id: Logger.hh,v 1.7 1998-08-27 22:35:27 granger Exp $
  *
  * Class for reporting error and log messages, which can be conveniently
  * subclassed to log messages through different facilities.  A subclass
@@ -16,7 +16,7 @@
 #ifndef _Logger_hh_
 #define _Logger_hh_
 
-#include <stdlib.h>		// For malloc() and free()
+//#include <stdlib.h>		// For malloc() and free()
 #include <iostream.h>
 #include <strstream.h>
 #include <string>		// for strerror()
@@ -26,11 +26,27 @@
 
 /* NOTES
 
+   Add a Log class with methods for reporting errno and a message and which
+   includes an error stream (even an in-memory string) and flush method
+   perhaps.  Subclasses can override as needed, such as redirecting error
+   messages to the EventLogger.
+
+   For logging: when a library layer or module selects a log object by
+   name, use that name as a lookup in a class map to qualify the log mask,
+   so the application can set the mask for modules by name when the module
+   asks for its log object.  The application could also set the class of
+   loggers to be used, such as log to file, stdout, EventLogger, composite,
+   whatever, or disable logging altogether by always returning the
+   NullLogger.
+
    Add methods for registering entrance and exit from routines or
    components, possibly taking advantage of automatic variables so that
    we're not messed up by exceptions.  Perhaps a method also for just
    announcing "i was reached" or "i am done."
    
+   EnterBlock eb(log, "routine name", level = DEBUG);
+   Checkpoint cp(log, "computations finished", level = DEBUG);
+
    Allow selection of messages to be logged with a predicate function which
    matches the message string, level, or "stack trace".  After compiling
    the stack, message, and level of the log event, pass to a single method
@@ -38,15 +54,99 @@
 
    Allow a global "default" logger?  Create a logger hierarchy so that log
    messages to one logger can be passed back up the tree, possibly to
-   loggers with different behaviors or selection criteria.
+   loggers with different behaviors or selection criteria.  This would be a
+   "composite" logger...
 
    Provide:
 
    log << ... << Logger::debug;
    log << ... << Logger::info;
 
+   I like the idea of a circular buffer in Nana.  We should be able to
+   implement such a class as a subclass of Logger.  How to keep logging
+   possible in library modules like BlockFile without using lots of
+   overhead when not needed?  I'd be ok with allowing the virtual call to a
+   no-op logger, but why not avoid the Printf and Format constructors when
+   the message will not even be used?  We could surround log calls with a
+   macro which can be nullified for special builds, and otherwise tests
+   whether the logger is null or no-op before evaluating the message text.
+
+	LOG(log,Info(Printf(...))); becomes
+
+	if (log) log->Info(Printf(...));
+
+   Also add options for prepending timestamps to the log messages.
+
+   It would be nice just to add a call to log a message:
+
+	Log::Info(msg);
+
+   but if we want to identify the context of the message, we need our own
+   logger reference or another parameter.
+ 
+   Add a simple object to a class or static module which gets intialized
+   and destroyed automatically:
+
+	Logger log("BlockFile");
+
+   The constructor registers the context name, and log calls always use
+   that logger instance.  It becomes a proxy for another logger, and/or it
+   gets its mask/behavior/prototype from the logger configuration.  So
+   Logger is an implementation which actually logs messages, and Sender is
+   a subclass which uses a proxy to send messages to the actual logger.  By
+   default a Sender does nothing.
+
+   To shortcut Printf and Format construction, perhaps try
+   
+	log.Debug (log && Printf());
+   
+   where an inline boolean operator for log returns false if nothing will
+   be logged.  Or log() && log.Debug (Printf())...
+
+   Might be nice to pass the contexts up the call hierarchy as part of a
+   list, rather than a string, so that the final logger can decide how to
+   convert the list of contexts to a message or display string.
+   
+   Use Push() and Pop() methods to add to the context list.  Make the list
+   a stack of context strings.
+
+   Just pass an automatic object reference which gets cloned:
+
+   Logger::Prototype (StreamLogger(cerr), "unit X");
+
+   The prototype could have a flag vector for specific events, such as
+   construction/destruction, which will be logged when the Sender or Logger
+   object is created or destroyed, corresponding to the object which
+   contains it.
+
+   Move implementation into source file, especially the methods which
+   cannot be inlined, to reduce dependency on implementation in header.
+
+   Provide a macro to include logging code within which can be defined to
+   empty to disable all logging code generation.  Or perhaps inline
+   functions versus macros which can be used like:
+
+   Debug(log,"...")
+
+   which is either empty or becomes log.Debug("...").
+
+   EnterBlock(...) becomes EnterBlock eb(...)
+
+   Might be nice for a class to be able to subclass a Sender, so that
+   initialization and destruction could be taken care of automatically,
+   except that wouldn't work because the Sender still needs to be
+   initialized explicitly to set the class or package name for logging.
+
+   The default Logger should use a circular queue of messages so that
+   applications can always dump the most recent library messages in case of
+   an error or outstanding condition, or even on a request from the user.
+   Sort of a glorified strerror().
+
    */
    
+
+class LogRegistry;
+
 
 class Logger
 {
@@ -62,9 +162,24 @@ public:
 	static const int DEVELOP;
 	static const int ALL;
 
-	static Logger *For (const char *name = 0);
-	static void SetPrototype (const Logger & proto);
+	/*
+	 * To configure the logging of a particular unit, assign
+	 * a prototype to a string which will match that unit's 
+	 * name.  More recent prototype's take precedence.
+	 */
+	static void Prototype (const Logger &, const string &unit);
 
+	/*
+	 * To set the default prototype for all units, omit the unit.
+	 */
+	static void Prototype (const Logger &);
+
+protected:
+
+	static Logger *Register (Logger *log);
+	static void Forget (Logger *log);
+
+public:
 	/* ---------------- Instance methods ---------------- */
 
 	virtual const char *levelName (int level) const
@@ -84,20 +199,19 @@ public:
 		return ("None");
 	}
 
-	// Log a message at the given level.
-	// This is the method to override in subclasses.
 
-	virtual void Log (int levels, const char *s)
-	{
-		if (name)
-			cerr << name << ": ";
-		cerr << levelName(levels) << ": " << s << endl;
-	}
+	/* ----------------
+	 * Log a message at the given level.
+	 * This is the method to override in subclasses.
+	 * ----------------
+	 */
+	virtual bool Log (int levels, const string &s) = 0;
+
 
 #define LOG_METHOD(name,level) \
-	virtual void name (const char *s) \
+	virtual bool name (const string &s) \
 	{ \
-		Log (level, s); \
+		return Log (level, s); \
 	}
 
 	// Log messages at PROBLEM/ERROR level
@@ -117,7 +231,7 @@ public:
 	LOG_METHOD(Develop,DEVELOP);
 
 	// Log system error messages
-	virtual void System (const char *s)
+	virtual bool System (const string &s)
 	{
 		ostrstream buf;
 		buf << "'" << strerror(errno) << "' (" << errno << "): ";
@@ -125,89 +239,235 @@ public:
 		Problem (buf.str());
 	}
 
-	Logger (const char *_name = 0) : name (0)
+	Logger (const string &_context = "")
 	{
-		Name (_name);
+		Declare (_context);
 	}
 
-		Logger (const Logger &log) : name (0)
+	Logger (const Logger &log)
 	{
-		Name (log.name);
+		Declare (log.context);
 	}
 		
 	Logger& operator= (const Logger &log)
 	{
-		Name (log.name);
+		Declare (log.context);
 	}
 
 	/*
 	 * Prototype pattern: Subclasses override this to return a logger
 	 * of their particular class.
 	 */
-	virtual Logger *Clone (const char *_name = 0) const
+	virtual Logger *Clone (const string &_context) const = 0;
+
+	Logger *Clone () const
 	{
-		return new Logger (_name);
+		return Clone (Context());
 	}
 
-	virtual ~Logger ()
+	virtual ~Logger () { }
+
+	// Return our unit context.
+	const string &Context () const { return context; }
+
+	// Set the current context string.
+	virtual void Declare (const string &_context)
 	{
-		if (name)
-			free (name);
+		context = _context;
 	}
 
-	// Not actually meant for public access, but needed for the template
-	// operator<<
-	ostrstream out;
+	// Extend the current context.
+	virtual void Extend (const string &_context)
+	{
+		context += "(" + _context + ")";
+	}
 
 protected:
-	char *name;
 
-	void Name (const char *_name)
-	{
-		if (this->name)
-		{
-			free (this->name);
-			this->name = 0;
-		}
-		if (_name)
-		{
-			this->name = (char *) malloc (strlen(_name) + 1);
-			strcpy (this->name, _name);
-		}
-	}
+	string context;
 
 private:
 
-	static Logger *prototype = 0;
+	static LogRegistry *registry;
 };
-
-
-/*
- * To make Logger objects behave like output streams.  Use the
- * log manipulators to send the message at a particular level.
- */
-template <class T>
-Logger &operator<< (Logger &log, T &t)
-{
-	return log;
-}
 
 
 
 class NullLogger : public Logger
 {
 public:
-	NullLogger (const char *name = 0) : Logger (name)
+	NullLogger (const string &name = "") : Logger (name)
 	{ }
 
-	virtual void Log (int /*levels*/, const char *)
+	virtual bool Log (int /*levels*/, const string &/*msg*/)
 	{ }
 
-	virtual Logger *Clone (const char *name) const
+	virtual Logger *Clone (const string &name) const
 	{
 		return new NullLogger (name);
 	}
 };
+
+
+
+class StreamLogger : public Logger
+{
+public:
+
+	StreamLogger (ostream &_out, const string &name = "") :
+		Logger (name), out(_out)
+	{ }
+
+	StreamLogger (const string &name = "") :
+		Logger (name), out(cerr)
+	{ }
+
+	virtual bool Log (int levels, const string &s)
+	{
+		out << context << ": ";
+		out << levelName(levels) << ": " << s << endl;
+		return true;
+	}
+
+	virtual Logger *Clone (const string &_context) const
+	{
+		return new StreamLogger (out, _context);
+	}
+
+protected:
+
+	ostream &out;
+};
+   
+
+
+/*
+ * Inherit the Logger interface and implement a proxy mechanism.  The
+ * constructor registers this object with the Logger class under its given
+ * name and receives in reply its logger prototype, to which log messages
+ * will actually be sent.  The destructor de-registers this object with the
+ * class.  The Sender class includes a Change callback method which the
+ * class can use to change the object's prototype.
+ */
+class Sender : public Logger
+{
+public:
+
+	Sender (const string &name = "") : Logger (name), log(0)
+	{
+		log = Logger::Register (this);
+	}
+
+	Sender (const Sender &send) : Logger (*this), log(0)
+	{
+		Assign (send);
+	}
+
+	Sender &operator= (const Sender &send)
+	{
+		log = 0;
+		Assign (send);
+	}
+
+	/*
+	 * Derive a sender from an existing one and add a sub-context.
+	 */
+	Sender (const Sender &send, const string &sub)
+	{
+		Assign (send);
+		Extend (sub);
+	}
+
+	virtual bool Log (int levels, const string &msg)
+	{
+		if (log)
+			return log->Log (levels, msg);
+		return false;
+	}
+
+	virtual Logger *Clone (const string &name) const
+	{
+		Sender *send = new Sender (*this);
+		send->Declare (name);
+		return send;
+	}
+
+	virtual void Declare (const string &_context)
+	{
+		Logger::Declare (_context);
+		if (log) log->Declare (_context);
+	}
+
+	virtual void Extend (const string &_context)
+	{
+		Logger::Extend (_context);
+		if (log) log->Extend (_context);
+	}
+
+	virtual ~Sender ()
+	{
+		Logger::Forget (this);
+		if (log)
+			delete log;
+	}
+
+	/* ---------------- Sender methods ---------------- */
+
+	void Change (const Logger &prototype)
+	{
+		if (log)
+			delete log;
+		log = prototype.Clone (context);
+	}
+
+	operator bool() { return (log != 0); }
+
+protected:
+
+	Assign (const Sender &send)
+	{
+		Logger::operator= (send);
+		if (log)
+			delete log;
+		if ((log = send.log))
+			log = log->Clone();
+	}
+
+	Logger *log;
+
+};
+
+
+
+/*
+ * Define some helper classes which rely on automatic construction
+ * and destruction to trigger their log messages.
+ */
+class EnterBlock 
+{
+public:
+
+	EnterBlock (Logger &_log, const string &_msg, 
+		    int _level = Logger::DEBUG) : 
+		log(&_log), msg(_msg), level(_level)
+	{
+		log->Log (level, "Enter: " + msg);
+	}
+
+	~EnterBlock ()
+	{
+		log->Log (level, "Leave: " + msg);
+	}
+
+protected:
+
+	Logger *log;
+	string msg;
+	int level;
+};
+
+
+
 
 
 
