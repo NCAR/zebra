7f62d84bb323a407833a0a71cea47da0436ac3a5
Gary Granger <granger@ucar.edu>
2001-01-06 00:01:39 +0000

    rework the ZTime class to remove some ambiguity caused by cast operators and scalar constructors; change comparison operators to expect ZTimes so that ZebraTimes will be constructed and normalized as ZTimes before comparison; add math operators between ZTimes, using implicit scalar double construction to implement mixed-mode math; add a test program for ztime
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@7259 3f9b6c58-9310-455a-aec6-8b30269ab333

--- src/lib/ZTime.h
+++ src/lib/ZTime.h
@@ -1,6 +1,6 @@
-// Some -*- C++ -*- goodies for dealing with ZebraTimes.
+// -*- mode: c++; c-basic-offset: 8 -*- goodies for dealing with ZebraTimes.
 //
-// $Id: ZTime.h,v 2.4 2000-08-30 23:02:31 granger Exp $
+// $Id: ZTime.h,v 2.5 2001-01-06 00:01:39 granger Exp $
 //
 /*		Copyright (C) 1987,88,89,90,91,92 by UCAR
  *	University Corporation for Atmospheric Research
@@ -25,10 +25,33 @@
 
 #include "defs.h"
 
-/*
- * Subclass the ZebraTime C struct and call it ZTime!
- * Then we can add some C++ methods to it.
- */
+//
+// Subclass the ZebraTime C struct and call it ZTime!
+// Then we can add some C++ methods to it.
+//
+// Here is the basic idea for using this class as it is currently designed.
+// There are no longer any cast operators since these can cause confusion.
+// For example, assigning a double to a ZTime would silently first truncate
+// the double to a long and lose the microseconds.  Defining both a double
+// and long cast operator would cause ambiguities.  Instead, math operators
+// are only provided between ZTimes, and constructors are provided for
+// converting doubles, longs, and ZebraTimes into ZTimes.  For construction
+// a scalar (long or double) is interpreted as seconds since the epoch.
+// For math, a scalar can be just an interval in seconds.  This
+// construction and possibly many copies will happen implicitly when
+// writing mixed-mode expressions with ZTimes, such as zt + 60.  The
+// comparison operators also work with ZTime, which allows comparisons to
+// scalars by way of the same implicit constructions.  This may cause some
+// unintended overhead when comparing ZebraTimes. However, this also means
+// that ZebraTimes will be normalized before being compared (in the
+// implicit constructor).  Without the normalization, it would be possible
+// for two non-normalized but otherwise equal ZebraTimes to compare
+// incorrectly.  The ZTime class tries to make sure that it is always
+// normalized.  
+//
+// This class should be the same size as ZebraTime structs (see
+// ztime_test), so arrays of ZTimes can be passed as arrays of ZebraTimes.
+// This means this class should never be virtual in any way.
 
 struct ZTime : public ZebraTime
 {
@@ -53,22 +76,14 @@ struct ZTime : public ZebraTime
 
 	inline long toSystem () const
 	{
-		return (TC_ZtToSys (this));
+		return zt_Sec;
 	}
 
-	inline operator long () const
+	inline ZTime &setSystem (const long sys)
 	{
-		return (toSystem());
-	}
-	
-	inline void setSystem (const long sys)
-	{
-		TC_SysToZt (sys, this);
-	}
-
-	inline void operator = (const long sys)
-	{
-		setSystem (sys);
+		zt_Sec = sys;
+		zt_MicroSec = 0;
+		return *this;
 	}
 
 	inline void encode (char *dest, TimePrintFormat format = TC_Full) const
@@ -150,30 +165,29 @@ struct ZTime : public ZebraTime
 	{
 		TC_ZtAssemble (this, year, month, day, hour, 
 			       minute, second, microsec);
+		normalize();
 	}
 
-	inline ZTime operator+= (int seconds)
+	// Return a double scalar in seconds since the epoch.
+	//
+	double toDouble () const
 	{
-		zt_Sec += seconds;
+		return (zt_Sec + ((double)zt_MicroSec/1.0e6));
+	}
+
+	inline ZTime &operator+= (const ZTime &rhs)
+	{
+		zt_Sec += rhs.zt_Sec;
+		zt_MicroSec += rhs.zt_MicroSec;
+		normalize();
 		return *this;
 	}
 
-	inline ZTime operator+ (int seconds)
+	inline ZTime &operator-= (const ZTime &rhs)
 	{
-		return ZTime(zt_Sec + seconds, zt_MicroSec);
-	}
-
-	inline ZTime operator+ (const ZebraTime &add)
-	{
-		unsigned long msecs = zt_MicroSec + add.zt_MicroSec;
-		return ZTime(zt_Sec + add.zt_Sec + (msecs / 1000000),
-			     msecs % 1000000);
-	}
-
-	inline ZTime &operator= (const ZebraTime &src)
-	{
-		zt_Sec = src.zt_Sec;
-		zt_MicroSec = src.zt_MicroSec;
+		zt_Sec -= rhs.zt_Sec;
+		zt_MicroSec -= rhs.zt_MicroSec;
+		normalize();
 		return *this;
 	}
 
@@ -184,40 +198,96 @@ struct ZTime : public ZebraTime
 		return *this;
 	}
 
+	inline ZTime &operator= (const ZebraTime &src)
+	{
+		zt_Sec = src.zt_Sec;
+		zt_MicroSec = src.zt_MicroSec;
+		normalize();
+		return *this;
+	}
+
+	void normalize()
+	{
+		if (zt_MicroSec < 0) 
+		{
+			zt_Sec -= 1 + (-zt_MicroSec / 1000000);
+			zt_MicroSec = 1000000 - (-zt_MicroSec % 1000000);
+		}
+		else
+		{
+			zt_Sec += zt_MicroSec / 1000000;
+			zt_MicroSec = zt_MicroSec % 1000000;
+		}
+	}
+
+	// ================
 	// Constructors
+	//
+	// Warning: Because we inherit from a C structre, ZebraTime,
+	// it does not have a constructor which initializes its members,
+	// so these constructors have to make sure they do that.
 
-	ZTime ()
+	inline ZTime ()
 	{
-		zt_Sec = 0;
-		zt_MicroSec = 0;
+		setSystem(0);
 	}
 
-	ZTime (const ZebraTime &zt)
+	inline ZTime (const ZebraTime &zt)
 	{
-		zt_Sec = zt.zt_Sec;
-		zt_MicroSec = zt.zt_MicroSec;
+		this->operator= (zt);
 	}
 
-	ZTime (const long sys)
+	inline ZTime (const ZTime &zt)
 	{
-		TC_SysToZt (sys, this);
+		this->operator= (zt);
 	}
 
-private:
-	ZTime (long sec, long micro)
+	// Construct a ZTime from [fractional] seconds since the epoch.
+	//
+	ZTime (const double seconds)
+	{
+		int sign = (seconds < 0 ? -1 : 1 );
+		double asecs = (seconds < 0 ? -seconds : seconds );
+		long lsecs = (long)(asecs);
+		zt_Sec = sign*lsecs;
+		zt_MicroSec = sign*((long)((asecs - lsecs)*1e6));
+	}
+
+	// Integer initialize requires both fields to disambiguate it
+	// from the double constructor.
+	//
+	inline ZTime (long sec, long micro)
 	{
 		zt_Sec = sec;
 		zt_MicroSec = micro;
+		normalize();
 	}
+
 };
+
+
+inline ZTime operator+ (const ZTime &lhs, const ZTime &rhs)
+{
+	return ZTime(lhs) += rhs;
+}
+
+
+inline ZTime operator- (const ZTime &lhs, const ZTime &rhs)
+{
+	return ZTime(lhs) -= rhs;
+}
 
 
 class ostream;
 ostream & operator<< (ostream &out, const ZebraTime &t);
 
+// ================================================================
+// NOTE: The comparison operators assume the zebra time is normalized.
+// If not, the comparison can be incorrect.
+// ================================================================
 
 inline
-bool operator< (const ZebraTime &t1, const ZebraTime &t2)
+bool operator< (const ZTime &t1, const ZTime &t2)
 {
 	return (t1.zt_Sec < t2.zt_Sec ||
 		(t1.zt_Sec == t2.zt_Sec && t1.zt_MicroSec < t2.zt_MicroSec));
@@ -225,7 +295,7 @@ bool operator< (const ZebraTime &t1, const ZebraTime &t2)
 
 
 inline
-bool operator<= (const ZebraTime &t1, const ZebraTime &t2)
+bool operator<= (const ZTime &t1, const ZTime &t2)
 {
 	return (t1.zt_Sec < t2.zt_Sec ||
 		(t1.zt_Sec == t2.zt_Sec && t1.zt_MicroSec <= t2.zt_MicroSec));
@@ -233,7 +303,7 @@ bool operator<= (const ZebraTime &t1, const ZebraTime &t2)
 
 
 inline
-bool operator> (const ZebraTime &t1, const ZebraTime &t2)
+bool operator> (const ZTime &t1, const ZTime &t2)
 {
 	return (t1.zt_Sec > t2.zt_Sec ||
 		(t1.zt_Sec == t2.zt_Sec && t1.zt_MicroSec > t2.zt_MicroSec));
@@ -242,7 +312,7 @@ bool operator> (const ZebraTime &t1, const ZebraTime &t2)
 
 
 inline
-bool operator>= (const ZebraTime &t1, const ZebraTime &t2)
+bool operator>= (const ZTime &t1, const ZTime &t2)
 {
 	return (t1.zt_Sec > t2.zt_Sec ||
 		(t1.zt_Sec == t2.zt_Sec && t1.zt_MicroSec >= t2.zt_MicroSec));
@@ -251,11 +321,16 @@ bool operator>= (const ZebraTime &t1, const ZebraTime &t2)
 
 
 inline
-bool operator== (const ZebraTime &t1, const ZebraTime &t2)
+bool operator== (const ZTime &t1, const ZTime &t2)
 {
 	return ((t1.zt_Sec == t2.zt_Sec) && 
 		(t1.zt_MicroSec == t2.zt_MicroSec));
 }
 
+inline
+bool operator!= (const ZTime &t1, const ZTime &t2)
+{
+	return (! (t1 == t2));
+}
 
 #endif /* ndef _ZTime_h_ */
---
+++ src/lib/ztime_test.cc
@@ -0,0 +1,106 @@
+
+#include <time.h>
+#include <math.h>
+
+#include <string>
+
+#include "message.h"
+#define inline
+#include "ZTime.h"
+#undef inline
+
+int check (const char *test, bool cond, int line = 0)
+{
+    if (! cond)
+    {
+	msg_ELog (EF_PROBLEM, "test failed (line %d): %s", line, test);
+	return 1;
+    }
+    return 0;
+}
+
+
+#define TEST(expr) errors += check (#expr, (expr), __LINE__)
+
+int 
+main (int, char *argv[])
+{
+    int errors = 0;
+
+    msg_connect (0, argv[0]);
+
+    ZTime zt;
+
+    // Test default construction with 0 constructors
+    TEST(zt == 0);
+    TEST(zt == ZTime(0,0));
+
+    long sys = time(0);
+
+    // Compare long constructor with double constructor.
+    TEST(ZTime(sys,0) == ZTime(sys));
+
+    // Assignment from long
+    zt = sys;
+    TEST((zt = sys) == ZTime(sys));
+
+
+    // Assembly, then individual fields.
+    zt.assemble (2001, 8, 10, 9, 11, 12, 1234);
+    TEST(zt.year() == 2001);
+    TEST(zt.month() == 8);
+    TEST(zt.day() == 10);
+    TEST(zt.hour() == 9);
+    TEST(zt.minute() == 11);
+    TEST(zt.second() == 12);
+
+    TEST(zt == zt);
+    ZTime a;
+    a.assemble (2001, 8, 10, 9, 11, 12, 0);
+    TEST(a < zt);
+    TEST(a <= zt);
+    TEST(a != zt);
+    TEST(zt > a);
+    TEST(zt >= a);
+    TEST(zt <= zt);
+
+    // Convert to double, re-construct, and re-compare
+    double seconds = zt.toDouble();
+    ZTime zt2(0);
+    zt2 = seconds;
+    TEST(fabs(zt2.toDouble() - seconds) < 0.00001);
+    
+    // And their encodings should be equal also.
+    string one(zt.ascTime());
+    string two(zt2.ascTime());
+    string cond = one + " == " + two;
+    errors += check (cond.c_str(), (one == two), __LINE__);
+
+    // Test normalization.  These should all be equal.
+    ZTime norm (seconds+2, 500000);
+    ZTime t1 = ZTime((long)seconds, 2500000);
+    TEST(t1 == norm);
+    ZTime t2 = ZTime(seconds+3, -500000);
+    TEST(t2 == norm);
+
+    // Try our hand at decoding.
+    zt.decode ("04-Jan-2001,15:46:13.25");
+    // This also tests the y2k check.
+    zt2.assemble (1, 1, 4, 15, 46, 13, 250000);
+    TEST (zt == zt2);
+
+    ZTime zt3 = zt2 + 0.25;
+    ZTime zt4 = 0.25 + zt2;
+    ZTime qtrsec = 0.25;
+    TEST (zt3 == zt4);
+    zt3 = qtrsec + zt2;
+    TEST (zt3 == zt4);
+    zt.assemble (1, 1, 4, 15, 46, 13, 2000000);
+    zt2 += 2;
+    zt2 -= 0.25;
+    TEST (zt == zt2);
+
+    TEST (sizeof(ZebraTime) == sizeof(ZTime));
+
+    return errors;
+}
