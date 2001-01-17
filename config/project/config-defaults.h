da0c2b7c3fbec69621bc94e9df73dff37f825d71
Gary Granger <granger@ucar.edu>
2001-01-17 23:21:18 +0000

    commence a serious trimming of configuration: all the defaults which should hardly ever be touched are now in config-defaults.h; all the ingest config, including which modules to build and the FORTRAN (yay) configs are in config-ingest.h (datautil has been moved to the ingest tree and hence is enabled there also); termcap and math libraries removed from config.h and given suitable defaults in Project.tmpl; BuildRDSS now always defaults to YES, and all mention of it and the various RDSS permutations due to it have been moved into Project.tmpl; now that directories need not be present to build, Distribute symbols are defunct and have been removed; all mention of XHelp removed from config.h; all the plot type settings moved to config-defaults and default to YES, except for polar plots which default to YES only if sweepfiles enabled
    git-svn-id: http://svn.eol.ucar.edu/svn/rdss/trunk/zebra/source@7285 3f9b6c58-9310-455a-aec6-8b30269ab333

---
+++ config/project/config-defaults.h
@@ -0,0 +1,365 @@
+/* ================================================================
+ * If you want to change the setting of anything in this file, override it
+ * in config.h.  This file is not meant to be changed.  If a definition 
+ * in config.h causes a conflict with a definition in this file, then 
+ * surround the definition in this file with ifndef.
+ *
+ * This file is ONLY meant to be include by config-defaults.h, and hence
+ * is not surrounded by its own ifndefs.
+ * ================================================================ */
+
+
+/*========================================================================
+ * Distribution Options
+ *------------------------------------------------------------------------
+ * This section of definitions is meant for those who are creating a
+ * customized Zebra distribution.  If you are only installing Zebra at your
+ * local site, then you are finished configuring Zebra.  Save this file and
+ * continue with the installation instructions.  Otherwise, read on.
+ *
+ * Zebra's Imakefiles contain several built-in features meant for creating
+ * and customizing Zebra distribution trees.  In particular, here are some
+ * important targets to be familiar with:
+ *
+ * distfiles::		Echoes to the terminal the path names of all files
+ *			which are meant to be distributed in a directory
+ *			and all of the subdirectories.  By setting the
+ *			DISTCURDIR variable on the command line, all of the
+ *			file names printed will be prefixed with the value
+ *			of the variable.  See the tarfile: target in the
+ *			top-level Imakefile for an example of how this can
+ *			be used.
+ *
+ * distmakefiles::	Makes Makefiles for ALL of the distribution 
+ *			directories, not just those which have been "turned
+ *			on" to be built.  This is necessary before performing
+ *			other dist targets in order to create the Makefile
+ *			which the dist targets will operate on.
+ *
+ * distclean::		Cleans in ALL directories in the distribution.  This
+ *			is useful when programs have been compiled
+ *			explicitly from within a directory which has been
+ *			disabled in the regular distribution tree.  From the
+ *			top-level, this target also removes the ./share
+ *			directory of header file links created by make depend.
+ *
+ * It used to be that several symbols existed for defining parts of the tree
+ * which should not be distributed.  This is no longer necessary as the build
+ * configuration just ignores any directories which are not present.
+ * Therefore, if you want to create a distribution which does not contain
+ * the ingest directory, Optimizer, data utilities, map utilities, or xhelp, 
+ * just remove those directories from your tree.  Those directories and their
+ * files will not be included in the distfiles output, and no builds will
+ * recurse into those directories.
+ *
+ * Questions about any of this can go to granger@ncar.ucar.edu
+ * Also see the chapter on Imake in the Zebra Developer's Guide.
+ */
+
+# ifdef MAKING_MAKEFILE
+/*
+ * If you're using Mosaic exclusively and do not want to build or install
+ * the xhelp utility, define BuildXHelp to NO.  It defaults to NO if
+ * UseMosaic is YES and BuildXHelp is not defined.  If you want the default
+ * to be Mosaic, but you still want to compile xhelp capability, define
+ * BuildXHelp to YES and UseMosaic to YES.  If BuildXHelp is NO, then zebra
+ * will always try to use Mosaic no matter what (since xhelp won't be there).
+ *
+ * If you have Mosaic and will not be using older project configurations
+ * such as the StormFEST CD's (which have project-specific info under xhelp),
+ * then you may as well define this to NO.
+ */
+# define BuildXHelp NO
+
+#endif /* MAKING_MAKEFILE */
+
+#ifndef MOSAIC_COMMAND
+# define MOSAIC_COMMAND "netscape"
+#endif
+
+/*========================================================================
+ * Selecting Graphics Process Plot Types
+ *------------------------------------------------------------------------
+ * The following definitions allow you to configure in or out various
+ * pieces of the Zebra graphics process.  There are very few cases in which
+ * it is desirable to change any of the following. (HENCE the reason they
+ * were moved here to simplify config.h.)
+ *------------------------------------------------------------------------*/
+/*
+ * --- The various plot types ---
+ */
+# define C_PT_CAP	YES		/* Constant altitude plots	*/
+# define C_PT_SKEWT	YES		/* Skew T plots			*/
+# define C_PT_XSECT	YES		/* Cross section plots		*/
+# define C_PT_TSERIES	YES		/* Time series plots		*/
+# define C_PT_XYGRAPH	YES		/* XY-Graph plots		*/
+# define C_PT_XYWIND	YES		/* XY-Graph wind plots		*/
+# define C_PT_HISTOGRAM	YES		/* Histograms			*/
+# define C_PT_THETAPLOT	YES		/* Theta plots			*/
+
+/*
+ * --- CAP subplots ---
+ *
+ * Define these YES if you want them.  If C_PT_CAP above is not YES, then
+ * ALL of these definitions will be ignored and none of these plots will
+ * be built.  Again, it is rare that anyone would want or need to define
+ * any of these to NO.
+ *
+ * The one possible exception is POLAR, which enables the "polar"
+ * representation and contours of polar data.  This is probably not useful
+ * to the bulk of people out there.  Polar plots currently only work from
+ * sweepfile data, so you will need to have enabled sweepfile access as
+ * well.
+ */
+# if C_PT_CAP
+#	define C_CAP_OVERLAY	YES	/* Overlays			*/
+#	define C_CAP_VECTOR	YES	/* Vector plots			*/
+# 	define C_CAP_LIGHTNING	YES	/* Lightning location		*/
+#	define C_CAP_RASTER	YES	/* Raster plots			*/
+#	define C_CAP_TRACKS	YES	/* Track plots (e.g. aircraft)	*/
+#  if HasSweepFiles
+# 	define C_CAP_POLAR	YES	/* Plots of polar data		*/
+#  else
+# 	define C_CAP_POLAR	NO	/* Plots of polar data		*/
+#  endif
+# endif
+/*------------------------------------------------------------------------*/
+
+/*========================================================================
+ * Customization Options
+ *------------------------------------------------------------------------
+ * Several parameter defaults used throughout the Zebra distribution can be
+ * modified by setting them in config.h rather than changing them in all of the
+ * include files.  The intention is that certain projects can keep their
+ * settings in this file and turn them on by defining a single symbol, like
+ * ARM_PROJECT for the ARM project.  All of the symbols defined here have
+ * the prefix 'CFG_'.  If a symbol is not defined, then the include files
+ * use their own defaults.
+ */
+
+/* #define ARM_PROJECT		*/
+/* #define NEXUS_PROJECT	*/
+
+/*------------------------------------------------------------------------
+ * //////////////////// Settings for the ARM project /////////////////////
+ *------------------------------------------------------------------------*/
+#ifdef ARM_PROJECT
+
+/* 
+ * The default altitude units for datachunks and for netCDF files whose alt
+ * field does not have a units attribute (or has a blank one).  If the
+ * altitude units of a datachunk are not explicitly overridden, this will
+ * be the units which are stored in the "units" attribute of the "alt"
+ * field in netCDF files.
+ */
+#define CFG_ALTITUDE_UNITS	AU_mMSL
+
+#define DistributeIngest 	NO
+#define DistributeOptimizer 	NO
+
+#endif /* ARM_PROJECT */
+/*------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------
+ * //////////////////////// Settings for NEXUS ///////////////////////////
+ *------------------------------------------------------------------------*/
+#ifdef NEXUS_PROJECT
+
+#define DistributeIngest 	NO
+#define DistributeOptimizer 	NO
+#define DistributeDataUtil 	NO
+#define DistributeRealtimeDataStoreTools YES
+
+#endif /* NEXUS_PROJECT */
+/*------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------*/
+/* ------------------ Defaults for compile-time constants ----------------*/
+/*------------------------------------------------------------------------*/
+/* The definitions below will not usually need to be changed, but they are
+ * provided here so that they can be conveniently found and changed in
+ * single file.  DO NOT EDIT THESE UNLESS YOU KNOW WHAT YOU ARE DOING!  If
+ * any of these definitions are unreasonable or downright incorrect, you
+ * may not be able to compile or run Zebra.
+ **************************************************************************/
+
+#define CFG_FULL_YEARS	/* Use four-digit years in netcdf file names */
+/* #define CFG_WARN_FIELD_NAMES *//* Warn about CDL-illegal field names */
+
+/*
+ * If defined, the 'alt' field will not be given a 'units' attribute in
+ * netCDF files.
+ */
+/* #define CFG_NC_NO_ALT_UNITS */
+
+/*
+ * Various string length limits
+ */
+#ifndef CFG_PLATNAME_LEN
+#define CFG_PLATNAME_LEN 80	/* Len of platform names		     */
+#endif
+#ifndef CFG_DIMNAME_LEN
+#define CFG_DIMNAME_LEN	32	/* Longest allowable dimension name	     */
+#endif
+#ifndef CFG_PDPARAM_LEN
+#define CFG_PDPARAM_LEN	40	/* Length of parameter names	*/
+#endif
+#ifndef CFG_ASCTIME_LEN
+#define CFG_ASCTIME_LEN	40	/* Length needed for encoded times*/
+#endif
+#ifndef CFG_PLATCLASS_LEN
+#define CFG_PLATCLASS_LEN 80	/* Len of platform class names		     */
+#endif
+#ifndef CFG_FILENAME_LEN
+#define CFG_FILENAME_LEN 32	/* Len of general file names (not full paths)*/
+#endif
+#ifndef CFG_DATAFILE_LEN
+#define CFG_DATAFILE_LEN 50	/* Len of data file names (not full paths)   */
+#endif
+#ifndef CFG_FILEPATH_LEN
+#define CFG_FILEPATH_LEN 256	/* Len of full path filenames		     */
+#endif
+#ifndef CFG_PDNAME_LEN
+#define CFG_PDNAME_LEN	40	/* Len of name of a plot description table   */
+#endif
+#ifndef CFG_MSGNAME_LEN
+#define CFG_MSGNAME_LEN	64	/* Max length of a message client name	     */
+#endif
+#ifndef CFG_MSGEVENT_LEN
+#define CFG_MSGEVENT_LEN 200	/* Length of extended logger event messages  */
+#endif
+#ifndef CFG_FIELD_NAME_LEN
+#define CFG_FIELD_NAME_LEN 40	/* Length of field names		     */
+#endif
+#ifndef CFG_FIELD_LONG_LEN
+#define CFG_FIELD_LONG_LEN 256	/* Length of field description (long name)   */
+#endif
+#ifndef CFG_FIELD_UNITS_LEN
+#define CFG_FIELD_UNITS_LEN 40	/* Length of field units string		     */
+#endif
+#ifndef CFG_SEARCHPATH_LEN
+#define CFG_SEARCHPATH_LEN 512	/* Length of exec search paths and such	     */
+#endif
+
+/*
+ * Maximum number of fields (field IDs) in the fields table
+ */
+#ifndef CFG_FIELD_MAX_ID
+#define CFG_FIELD_MAX_ID	512
+#endif
+
+/*
+ * Maximum number of platforms (including subplatforms)
+ */
+#ifndef CFG_MAX_PLATFORMS
+#define CFG_MAX_PLATFORMS 2048
+#endif
+
+/*
+ * DataChunk symbols
+ */
+#ifndef CFG_DC_MAXFIELD
+#define CFG_DC_MAXFIELD	255	/* Maximum allowable fields in one datachunk */
+#endif
+#ifndef CFG_DC_MF_HASH_SIZE
+#define CFG_DC_MF_HASH_SIZE 256	/* Least power of 2 > (not =) CFG_DC_MAXFIELD*/
+#endif
+#ifndef CFG_DC_MAXDIMN
+#define CFG_DC_MAXDIMN	30	/* Max number of dimensions for NSpace chunk,*/
+                                /* should be less than netCDF limit	     */
+#endif
+#ifndef CFG_DC_DEFAULT_BADVAL
+#define CFG_DC_DEFAULT_BADVAL -99999.	/* Default floating poing bad value  */
+		/* WARNING: Make sure this value contains a decimal point!   */
+#endif
+
+/*
+ * Plot desctiption limits
+ */
+#ifndef CFG_PD_MAXCOMP
+#define CFG_PD_MAXCOMP	50	/* Maximum # components expected in a PD     */
+#endif
+#ifndef CFG_PD_RAWTEMP
+#define CFG_PD_RAWTEMP	40960	/* Raw temp buffer space for raw PD's	     */
+#endif
+
+/*
+ * Message manager limits
+ */
+#ifndef CFG_MSG_MAXBCAST
+#define CFG_MSG_MAXBCAST 1500	   /* Maximum length of a broadcast message  */
+#endif
+#ifndef CFG_MSG_DEFAULT_PORT
+#define CFG_MSG_DEFAULT_PORT 1500  /* Default tcp port			     */
+#endif
+#ifndef CFG_MSG_SOCKET_NAME
+#define CFG_MSG_SOCKET_NAME "/tmp/fcc.socket"
+                         /* UNIX domain socket name in file system namespace */
+#endif
+
+/*
+ * Display manager limits
+ */
+#ifndef CFG_DM_CODELEN
+#define CFG_DM_CODELEN	20	/* Length of a DM event code	*/
+#endif
+#ifndef CFG_DM_MAXADATA
+#define CFG_DM_MAXADATA 128	/* Length of DM action data	*/
+#endif
+
+/*
+ * Graphics process limits
+ */
+#ifndef CFG_GP_MAX_CONTOURS
+#define CFG_GP_MAX_CONTOURS 50	/* Contour levels allowed without aborting */
+#endif
+
+/*
+ * Other parameters
+ */
+#ifndef CFG_ALTITUDE_UNITS
+#define CFG_ALTITUDE_UNITS 	AU_kmMSL /* Default units for "alt" field */
+#endif
+/*------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------*/
+/*
+ * These are always NO -- do not change them!
+ */
+# if !C_PT_CAP
+#	define C_CAP_OVERLAY	NO	/* Overlays			*/
+#	define C_CAP_VECTOR	NO	/* Vector plots			*/
+# 	define C_CAP_LIGHTNING	NO	/* Lightning location		*/
+#	define C_CAP_RASTER	NO	/* Raster plots			*/
+#	define C_CAP_TRACKS	NO	/* Track plots			*/
+# 	define C_CAP_POLAR	NO	/* Plots of polar data		*/
+# endif
+
+/*
+ * Some parameters for dealing with buffer lengths.
+ */
+# define MAX_PLAT_LEN	160	/* Maximum platform len -- remember lists */
+# define MAX_PARAM_LEN	40	/* Length of parameter names		*/
+# define TIME_LEN	40	/* Length needed for encoded times	*/
+
+/* --- Some obsolete configuration symbols put here to be forgotten --- */
+/* --- and to simplify the installation process                     --- */
+
+/*
+ * The PD monitor utility is an emacs program which provides interactive
+ * run-time editing, checking, and monitoring of graphics processes' plot
+ * descriptions.  If you have emacs (not version 19), and wish to compile
+ * the emacs lisp code for the PD monitor, define HaveEmacs to YES and make
+ * sure EmacsPath points to the emacs program.  The default is NO.  Even if
+ * the elisp code is not compiled and installed, the elisp source files
+ * will still be installed, so these can be used as is or byte-compiled
+ * later.  If you have Emacs 19, the elisp code cannot be batch compiled,
+ * so set HaveEmacs to NO. The Emacs 19 lisp file will still be installed.
+ */
+# define HaveEmacs NO
+# define EmacsPath emacs
+
---
+++ config/project/config-ingest.h
@@ -0,0 +1,156 @@
+
+XCOMM ================================================================
+XCOMM $Id: config-ingest.h,v 1.1 2001-01-17 23:21:17 granger Exp $
+XCOMM Definitions from config-ingest.h.
+XCOMM ================================================================
+
+/*
+ * This file configures settings specific to the ingest and data
+ * utility source tree, such as which ingest modules to build,
+ * paths to special libraries they need, and FORTRAN compiler and
+ * libraries.
+ */
+# ifdef MAKING_MAKEFILE
+
+/*
+ * Here you can select which individual ingest modules to build.  
+ */
+# define	BuildIngestScheduler	YES	/* Sun only	 	*/
+# define	BuildClassIngest	NO
+# define	BuildRadarIngest	NO	/* Sun only, see PCAP below*/
+# define	BuildSLGrabber		NO	/* Sun only		*/
+# define	BuildNowradIngest	NO
+# define	BuildProfsIngest	NO
+# define	BuildSatelliteIngest	NO
+# define	BuildP3Composite	NO
+# define	BuildTOGASlowTape	NO
+# define	BuildSlowTapeIngest	NO
+# define 	BuildTAOIngest		NO
+# define	Build_GMS_ISCCP		NO
+# define	Build_TRMM_SSMI		NO
+# define	Build_TRMM_Rain		NO	/* Sun only		*/
+# define	Build_TRMM_Sonde	NO
+# define	Build_FEST_P3_C130	NO
+# define	BuildPrecipIngest	NO
+# define	BuildDAPIngest		NO
+# define	BuildRAPDataServerIngest NO
+# define	BuildWetnetIngest	NO
+
+/*
+ * The PAM ingest programs (pam_ingest and daypam_ingest) require libraries
+ * which are compiled within the RDSS source tree.  Do not define this to YES
+ * unless your RDSS source tree still contains the compiled mda, pamutil, and
+ * unp libraries (i.e., it all built successfully and has not been cleaned).
+ */
+# define	BuildPamIngest		NO   /* Requires RDSS compiled tree */
+
+/*
+ * The data file utilities are the program for handling various file 
+ * and converting files to Zebra's netCDF conventions.  Some examples
+ * are gprotocdf and mudtocdf, for GENPRO and MUDRAS, respectively.
+ * Unless you know you will need them, you might as well wait and only
+ * compile the programs you need when you need them.
+ */
+# define	BuildDataUtilities		NO
+
+/* The following 2 libraries will probably need to be explicitly
+ * defined here, but ONLY if you need them for ingest modules.
+ *
+ * The SUDS library is only needed for the PAM and CLASS ingest modules.
+ *
+ *	SudsLibrary	$(RDSSDIR)/suds/libsuds.a
+ *
+ * The 'PAM configuration library', libunp.a, is only needed by
+ * the PAM ingest module.
+ *
+ *	PamCfgLibrary	-lunp
+ *
+ * If you don't need to ingest any PAM or CLASS data (which will be the
+ * case 99% of the time), you can safely ignore the definition of the
+ * two symbols above.
+ */
+
+/*
+ * This one works if the RDSS source tree is in /rdss and has not been cleaned
+ */
+/* # define PamCfgLibrary /rdss/pam/cfg/access/libunp.a */
+
+/*
+ * This one is for RDSS libraries installed into /usr/local/lib
+ */
+/* # define PamCfgLibrary /usr/local/lib/libunp.a */
+
+/*
+ * If you have a nonstandard Fortran compiler (i.e. your "make" won't figure
+ * it out automatically) set it here.  (This example for the GNU compiler).
+ * If you are compiling under Linux, the appropriate FORTRAN compiler and
+ * libraries are given defaults further down in this file, and you don't
+ * need to uncomment this definition.
+ */
+/* # define FortranCompiler g77 */
+
+/*
+ * FORTRAN libraries tend to be very system-specific.  Define this
+ * parameter to be the command-line options necessary to link with
+ * the FORTRAN libraries on your system.  If not using g77 or Suns,
+ * the defaults below are probably fine.  Otherwise, uncomment the
+ * g77 example definition below, or choose the correct definition
+ * from among the examples for Sun architectures.
+ *
+ * Fortran libraries are no longer needed to build the graphics process.
+ * They are only necessary if building the data utilities or certain
+ * ingestors, such as the satellite GOES and GVAR ingestors.
+ *
+ * Similar to FortranCompiler, FortranLibraries has a default for
+ * Linux defined below.
+ */
+
+/* G77 -- may also have to give -L if not linking with gcc */
+/* # define FortranLibraries -lf2c */
+
+#ifndef FortranLibraries
+#ifdef SunArchitecture
+/*
+ * Change the FortranLibraries definition below if you are not using the
+ * SC4.2 version of the Fortran compiler.
+ */
+#ifndef FortranLibraries
+# define FortranLibraries -L/opt/SUNWspro/SC4.2/lib -lF77 -lV77 -lM77 -lsunmath
+#endif
+
+#else
+#ifdef HPArchitecture /* HP-UX */
+
+# define FortranLibraries -lU77 -lf -lisamstub
+
+#else
+#ifdef SGIArchitecture /* IRIX 5.x */
+
+# define FortranLibraries -lF77 -lI77 -U77 -lisam
+
+#else
+#ifdef LinuxArchitecture
+
+# define FortranCompiler g77
+# define FortranLibraries -lf2c
+
+#else 
+#ifdef OSF1Architecture
+# define FortranCompiler f77
+# define FortranLibraries -lfor -lFutil -lUfor -lots
+ 
+#else
+#ifdef AIXArchitecture
+
+# define FortranCompiler xlf
+# define FortranLibraries -lxlf -lxlf90
+ 
+#endif /* AIX */
+#endif /* OSF */
+#endif /* Linux */
+#endif /* SGI */
+#endif /* !HP */
+#endif /* !Sun */
+#endif /* !FortranLibraries */
+
+# endif /* MAKING_MAKEFILE */
